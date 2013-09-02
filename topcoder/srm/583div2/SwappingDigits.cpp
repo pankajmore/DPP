f554c83321a662a369c0b661e96356178a33fbe6
Pankaj More <pankajmore@gmail.com>
2013-09-02 22:59:23 +0530

    tc srm 583div2 250:144

---
+++ topcoder/srm/583div2/SwappingDigits.cpp
@@ -0,0 +1,110 @@
+#line 2 "SwappingDigits.cpp"
+
+#include <algorithm>
+#include <complex>
+#include <exception>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <csetjmp>
+#include <fstream>
+#include <locale>
+#include <stdexcept>
+#include <cassert>
+#include <csignal>
+#include <functional>
+#include <map>
+#include <cctype>
+#include <cstdarg>
+#include <iomanip>
+#include <memory>
+#include <streambuf>
+#include <cerrno>
+#include <cstddef>
+#include <ios>
+#include <new>
+#include <string>
+#include <cfloat>
+#include <cstdio>
+#include <iosfwd>
+#include <numeric>
+#include <typeinfo>
+#include <ciso646>
+#include <cstdlib>
+#include <iostream>
+#include <ostream>
+#include <utility>
+#include <climits>
+#include <cstring>
+#include <istream>
+#include <queue>
+#include <valarray>
+#include <clocale>
+#include <ctime>
+#include <iterator>
+#include <set>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <limits>
+#include <sstream>
+
+typedef long long ll;
+typedef unsigned int ui;
+
+using namespace std;
+class SwappingDigits
+        {
+        public:
+        string minNumber(string num)
+            {
+		int i,j;
+		char min,max,curr;
+		int n = num.length();
+		min = num[n-1];
+		max = num[0];
+		int x,y;
+		for(i=0;i<n-1;i++) {
+		    curr = num[i];
+		    x = i;
+		    min = '9';
+		    for(j=i+1;j<n;j++) {
+			if(x ==0) {
+			    if(min >= num[j] && num[j]!='0') {min=num[j];y=j;}
+			} else
+			if(min >= num[j]) {min = num[j];y=j;}
+		    }
+		    if(curr > min) break;
+		}
+		char tmp;
+		if(curr > min) {
+		tmp = num[x];
+		num[x] = num[y];
+		num[y] = tmp;
+		}
+		return num;
+            }
+
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const string &Expected, const string &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arg0 = "596"; string Arg1 = "569"; verify_case(0, Arg1, minNumber(Arg0)); }
+	void test_case_1() { string Arg0 = "93561"; string Arg1 = "13569"; verify_case(1, Arg1, minNumber(Arg0)); }
+	void test_case_2() { string Arg0 = "5491727514"; string Arg1 = "1491727554"; verify_case(2, Arg1, minNumber(Arg0)); }
+	void test_case_3() { string Arg0 = "10234"; string Arg1 = "10234"; verify_case(3, Arg1, minNumber(Arg0)); }
+	void test_case_4() { string Arg0 = "93218910471211292416"; string Arg1 = "13218910471211292496"; verify_case(4, Arg1, minNumber(Arg0)); }
+
+// END CUT HERE
+
+        };
+
+    // BEGIN CUT HERE
+    int main()
+        {
+        SwappingDigits ___test;
+        ___test.run_test(-1);
+        }
+    // END CUT HERE
