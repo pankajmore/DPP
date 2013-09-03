3fc45151a04fbbe2173855734bcaeb7670cdefaa
Pankaj More <pankajmore@gmail.com>
2013-09-03 16:01:12 +0530

    tc srm 580div2 250:219

---
+++ topcoder/srm/580div2/ShoutterDiv2.cpp
@@ -0,0 +1,89 @@
+#line 2 "ShoutterDiv2.cpp"
+
+#include <algorithm>
+#include <complex>
+#include <exception>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <csetjmp>
+#include <fstream>
+#include <locale>
+#include <stdexcept>
+#include <cassert>
+#include <csignal>
+#include <functional>
+#include <map>
+#include <cctype>
+#include <cstdarg>
+#include <iomanip>
+#include <memory>
+#include <streambuf>
+#include <cerrno>
+#include <cstddef>
+#include <ios>
+#include <new>
+#include <string>
+#include <cfloat>
+#include <cstdio>
+#include <iosfwd>
+#include <numeric>
+#include <typeinfo>
+#include <ciso646>
+#include <cstdlib>
+#include <iostream>
+#include <ostream>
+#include <utility>
+#include <climits>
+#include <cstring>
+#include <istream>
+#include <queue>
+#include <valarray>
+#include <clocale>
+#include <ctime>
+#include <iterator>
+#include <set>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <limits>
+#include <sstream>
+
+using namespace std;
+
+class ShoutterDiv2
+        {
+        public:
+        int count(vector <int> s, vector <int> t)
+            {
+		int count;
+		for(int i=0;i<s.size();i++) {
+		    for(int j=i+1;j<s.size();j++) {
+			if(t[i] >= s[j])if(s[i] <= t[j]) count++;
+		    }
+		}
+		return count;
+            }
+
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {1, 2, 4}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {3, 4, 6}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 2; verify_case(0, Arg2, count(Arg0, Arg1)); }
+	void test_case_1() { int Arr0[] = {0}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {100}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 0; verify_case(1, Arg2, count(Arg0, Arg1)); }
+	void test_case_2() { int Arr0[] = {0,0,0}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {1,1,1}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 3; verify_case(2, Arg2, count(Arg0, Arg1)); }
+	void test_case_3() { int Arr0[] = {9,26,8,35,3,58,91,24,10,26,22,18,15,12,15,27,15,60,76,19,12,16,37,35,25,4,22,47,65,3,2,23,26,33,7,11,34,74,67,32,15,45,20,53,60,25,74,13,44,51}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arr1[] = {26,62,80,80,52,83,100,71,20,73,23,32,80,37,34,55,51,86,97,89,17,81,74,94,79,85,77,97,87,8,70,46,58,70,97,35,80,76,82,80,19,56,65,62,80,49,79,28,75,78}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arg2 = 830; verify_case(3, Arg2, count(Arg0, Arg1)); }
+
+// END CUT HERE
+
+        };
+
+    // BEGIN CUT HERE
+    int main()
+        {
+        ShoutterDiv2 ___test;
+        ___test.run_test(-1);
+        }
+    // END CUT HERE
