7041bcba657f7230a2a672f64de688b64219ce95
Pankaj More <pankajmore@gmail.com>
2013-09-14 23:15:06 +0530

    http://codeforces.com/contest/344

---
+++ codeforces/200div2/A.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <map>
+#include <string>
+#include <cstdio>
+#include <iostream>
+#include <ostream>
+#include <utility>
+#include <cstring>
+#include <istream>
+#include <queue>
+#include <set>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <limits>
+#include <sstream>
+
+using namespace std;
+
+typedef vector<int> VI;
+#define REP(k,a) for(int k=0; k < (a); ++k)
+#define SZ(x) ((int)((x).size()))
+#define ALL(c) (c).begin(), (c).end()
+#define PB push_back
+#define MP make_pair
+#define TR(c,i) for(typeof((c).begin() i = (c).begin(); i != (c).end(); i++)
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+
+int main(int argc, char *argv[])
+{
+    int c=0,n;
+    string old,cur;
+    scanf("%d",&n);
+    REP(i,n) {
+	old = cur;
+	cin>>cur;
+	if(old != cur) c++;
+    }
+    cout<<c<<endl;
+    return 0;
+}
