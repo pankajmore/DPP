cf163556f955a196ebdc73b9c96ed0c45ee1991a
Pankaj More <pankajmore@gmail.com>
2013-09-20 23:00:27 +0530

    http://codeforces.com/contest/347/problems B

---
+++ codeforces/201div2/B.cpp
@@ -0,0 +1,92 @@
+#include <algorithm>
+#include <complex>
+#include <exception>
+#include <list>
+#include <stack>
+#include <bitset>
+#include <csetjmp>
+#include <fstream>
+#include <locale>
+#include <stdexcept>
+#include <cassert>
+#include <csignal>
+#include <functional>
+#include <map>
+#include <cctype>
+#include <cstdarg>
+#include <iomanip>
+#include <memory>
+#include <streambuf>
+#include <cerrno>
+#include <cstddef>
+#include <ios>
+#include <new>
+#include <string>
+#include <cfloat>
+#include <cstdio>
+#include <iosfwd>
+#include <numeric>
+#include <typeinfo>
+#include <ciso646>
+#include <cstdlib>
+#include <iostream>
+#include <ostream>
+#include <utility>
+#include <climits>
+#include <cstring>
+#include <istream>
+#include <queue>
+#include <valarray>
+#include <clocale>
+#include <ctime>
+#include <iterator>
+#include <set>
+#include <vector>
+#include <cmath>
+#include <deque>
+#include <limits>
+#include <sstream>
+
+using namespace std;
+
+typedef long long LL;
+typedef long double LD;
+typedef vector<int> VI;
+typedef vector<VI> VII;
+typedef vector<string> VS;
+typedef pair<int,int> PII;
+
+#define FOR(k,a,b) for(typeof(a) k=(a); k <= (b); ++k)
+#define REP(k,a) for(int k=0; k < (a); ++k)
+#define SZ(x) ((int)((x).size()))
+#define ALL(c) (c).begin(), (c).end()
+#define PB push_back
+#define MP make_pair
+#define TR(c,i) for(typeof((c).begin() i = (c).begin(); i != (c).end(); i++)
+#define present(c,x) ((c).find(x) != (c).end())
+#define cpresent(c,x) (find(all(c),x) != (c).end())
+
+int main(int argc, char *argv[])
+{
+  int n;
+  cin>>n;
+  VI a(n);
+  REP(i,n) cin>>a[i];
+
+  int k=0;
+  map<int,int> m;
+  REP(i,n) {
+    if(a[i] == i) k++;
+    m.insert(PII(a[i],i));
+  }
+  //  cout<<k<<endl;
+  if(k<n) {
+    k++;
+  REP(i,n) {
+    if(a[i] == i) continue;
+    if(m.find(i)->second==a[i]) {k++;break;}
+    }
+  }
+  cout<<k<<endl;
+  return 0;
+}
