7233a47e55b079e9e40da7f120552d8455f6b063
Pankaj More <pankajmore@gmail.com>
2013-12-09 21:35:59 +0530

    https://www.hackerrank.com/challenges/merge-two-sorted-linked-lists

---
+++ hackerrank/challenges/merge-two-sorted-linked-lists.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include<cstdio>
+#include<cstdlib>
+using namespace std;
+struct Node
+{
+	int data;
+	Node *next;
+};/*
+  Merge two sorted lists A and B as one linked list
+  Node is defined as
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+Node* MergeLists(Node *headA, Node* headB)
+{
+  if(headA == NULL) return headB;
+  else if(headB == NULL) return headA;
+  else {
+      if(headA->data < headB->data) {
+          Node * restA = headA->next;
+          headA->next = MergeLists(restA,headB);
+          return headA;
+      }
+      else {
+          Node * restB = headB->next;
+          headB->next = MergeLists(headA,restB);
+          return headB;
+      }
+  }
+}void Print(Node *head)
+{
+	bool ok = false;
+	while(head != NULL)
+	{
+		if(ok)cout<<" ";
+		else ok = true;
+		cout<<head->data;
+		head = head->next;
+	}
+        cout<<"\n";
+}
+Node* Insert(Node *head,int x)
+{
+   Node *temp = new Node();
+   temp->data = x;
+   temp->next = NULL;
+   if(head == NULL)
+   {
+       return temp;
+   }
+   Node *temp1;
+   for(temp1 = head;temp1->next!=NULL;temp1= temp1->next);
+   temp1->next = temp;return head;
+}
+int main()
+{
+	int t;
+	cin>>t;
+	while(t-- >0)
+	{
+		Node *A = NULL;
+		Node *B = NULL;
+		int m;cin>>m;
+		while(m--){
+			int x; cin>>x;
+			A = Insert(A,x);}
+		int n; cin>>n;
+        while(n--){
+			int y;cin>>y;
+			B = Insert(B,y);
+		}
+		A = MergeLists(A,B);
+		Print(A);
+	}
+}
