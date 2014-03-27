1e91d71560f8e78d920050f4bdfcec7c2480d358
Pankaj More <pankajmore@gmail.com>
2014-03-27 23:52:39 +0530

    https://www.hackerrank.com/challenges/service-lane

---
+++ hackerrank/challenges/service-lane.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+    int i,r,j,N,T;
+    cin>>N>>T;
+    vector<int> A(N,0);
+    for(int i=0;i<N;i++) cin>>A[i];
+    while(T--){
+	cin>>i>>j;
+	r = 3;
+	for(int k=i;k<=j;k++){
+	    r = (r > A[k])?A[k]:r;
+	}
+	cout<<r<<endl;
+    }
+    return 0;
+}
