2bb49e37b9891cc841ceefde43e50bd87948214c
Pankaj More <pankajmore@gmail.com>
2015-05-31 23:49:27 +0530

    sample offline data

---
+++ EPI/src/OfflineSampling.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by pankajm on 5/30/15.
+ */
+// TODO: incomplete
+public class OfflineSampling {
+    public int[] sample(int[] A, int k)
+    {
+        int N = A.length;
+        Random r = new Random();
+        boolean reverse = false;
+        if( k > N/2) {
+            k = N - k;
+            reverse = true;
+        }
+        for(int j=0;j<k;j++) {
+            int i = r.nextInt(N - 1 - j);
+            swap(A[i], A[N - 1 - j]);
+        }
+        if(reverse)
+            return Arrays.copyOfRange(A, 0, N-k);
+        else
+            return Arrays.copyOfRange(A, N-k, N-1);
+    }
+
+    private void swap(int x, int y) {
+        x = x^y;
+        y = x^y;
+        x = x^y;
+    }
+
+}
--- EPI/src/ReservoirSampling.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by pankajm on 5/30/15.
- */
-public class ReservoirSampling {
-    //TODO: how to write a stream of numbers in java
-}
