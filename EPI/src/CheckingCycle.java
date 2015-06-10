ccd9764cb20ea72fdf5b01ce29d1cab5e51ada46
Pankaj More <pankajmore@gmail.com>
2015-06-10 11:47:09 +0530

    8.5 cycle in a linked list

---
+++ EPI/src/CheckingCycle.java
@@ -0,0 +1,34 @@
+import utils.Node;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/**
+ * Created by Pankaj on 6/10/15.
+ */
+public class CheckingCycle {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        PrintStream out = new PrintStream(System.out);
+
+        int N = in.nextInt();
+        int[] A = new int[N];
+        for(int i=0;i<N;i++) A[i] = in.nextInt();
+        Node head = new Node(A);
+
+        out.println("Before cycle : ");
+        out.println(head.toString());
+
+        assert(head.checkCycle() == null);
+
+        head.introduceCycle(N/2);
+
+        out.println("After cycle : ");
+        out.println(head.toString());
+
+        Node startOfCycle = head.checkCycle();
+        assert (startOfCycle != null);
+        out.println("Cycle at : " + startOfCycle.val);
+
+    }
+}
--- EPI/src/ReverseLinkedListIterative.java
+++ EPI/src/ReverseLinkedListIterative.java
@@ -1,63 +1,24 @@
 import java.io.PrintStream;
 import java.util.Scanner;
 
+import utils.Node;
+
 /**
  * Created by Pankaj on 6/9/15.
  */
 public class ReverseLinkedListIterative {
-    public static class Node{
-        public int val;
-        public Node next;
 
-        public String toString(){
-            StringBuilder sb = new StringBuilder();
-            sb.append(val);
-            Node curr = next;
-            while(curr != null){
-                sb.append(" -> ");
-                sb.append(curr.val);
-                curr = curr.next;
-            }
-            return sb.toString();
-        }
-    }
-
-    public static Node toLinkedList(int[] A){
-        if(A.length == 0)
-            return null;
-        Node head = new Node();
-        head.val = A[0];
-        Node curr = head;
-        for(int i=1;i<A.length;i++){
-            curr.next = new Node();
-            curr.next.val = A[i];
-            curr = curr.next;
-        }
-        return head;
-    }
-
-    public static Node iReverse(Node head){
-        Node temp, curr = head, rest = null;
-        while(curr != null){
-            temp = curr.next;
-            curr.next = rest;
-            rest = curr;
-            curr = temp;
-        }
-        return rest;
-    }
-
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
         PrintStream out = new PrintStream(System.out);
 
         int N = in.nextInt();
         int[] A = new int[N];
         for(int i=0;i<N;i++) A[i] = in.nextInt();
-        Node head = toLinkedList(A);
+        Node head = new Node(A);
         out.println(head.toString());
-        out.println(iReverse(head).toString());
+        out.println(head.iReverse().toString());
 
-        assert(iReverse(iReverse(head)).equals(head));
+        assert(head.iReverse().iReverse().equals(head));
     }
 }
