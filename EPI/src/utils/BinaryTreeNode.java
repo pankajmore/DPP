11bb4d40a4de9a7e8f880e374766368158d609cc
Pankaj More <pankajmore@gmail.com>
2015-06-19 23:21:29 +0530

    binary tree api

--- EPI/src/utils/BinaryTreeNode.java
+++ EPI/src/utils/BinaryTreeNode.java
@@ -1,26 +1,89 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by Pankaj on 6/12/15.
  */
 public class BinaryTreeNode {
-    private int val;
-    private BinaryTreeNode left;
-    private BinaryTreeNode right;
+    private int _val;
+    private BinaryTreeNode _left;
+    private BinaryTreeNode _right;
 
     public BinaryTreeNode(int val, BinaryTreeNode left, BinaryTreeNode right)
     {
-        this.val = val;
-        this.left = left;
-        this.right = right;
+        this._val = val;
+        this._left = left;
+        this._right = right;
     }
 
-    public BinaryTreeNode getLeft() {
-        return left;
+    public int val() { return _val; }
+
+    public BinaryTreeNode left() {
+        return _left;
     }
 
-    public BinaryTreeNode getRight() {
-        return right;
+    public BinaryTreeNode right() {
+        return _right;
+    }
+
+    public ArrayList<Integer> inOrder()
+    {
+        ArrayList<Integer> vals = new ArrayList<Integer>();
+        if(this.left() != null)
+            vals.addAll(this.left().inOrder());
+        vals.add(this.val());
+        if(this.right() != null)
+            vals.addAll(this.right().inOrder());
+        return vals;
+    }
+
+    public ArrayList<Integer> preOrder()
+    {
+        ArrayList<Integer> vals = new ArrayList<Integer>();
+        vals.add(this.val());
+        if(this.left() != null)
+            vals.addAll(this.left().preOrder());
+        if(this.right() != null)
+            vals.addAll(this.right().preOrder());
+        return vals;
+    }
+
+    public ArrayList<Integer> postOrder()
+    {
+        ArrayList<Integer> vals = new ArrayList<Integer>();
+        if(this.left() != null)
+            vals.addAll(this.left().postOrder());
+        if(this.right() != null)
+            vals.addAll(this.right().postOrder());
+        vals.add(this.val());
+        return vals;
+    }
+
+    public ArrayList<Integer> levelOrder()
+    {
+        ArrayList<Integer> vals = new ArrayList<Integer>();
+        Queue<BinaryTreeNode> q = new LinkedList<BinaryTreeNode>();
+        q.add(this);
+        while(!q.isEmpty())
+        {
+            BinaryTreeNode node = q.remove();
+            vals.add(node.val());
+            if(node.left() != null)
+                q.add(node.left());
+            if(node.right() != null)
+                q.add(node.right());
+        }
+        return vals;
+    }
+
+    public ArrayList<ArrayList<Integer>> levelOrderByDepth()
+    {
+        ArrayList<ArrayList<Integer>> ls = new ArrayList<ArrayList<Integer>>();
+        Queue<BinaryTreeNode> q = new LinkedList<BinaryTreeNode>();
     }
 }
 
