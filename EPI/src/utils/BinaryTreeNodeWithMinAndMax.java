2bd070803997389f8dee896e0324d5bf1e314a06
Pankaj More <pankajmore@gmail.com>
2015-08-02 22:09:54 +0530

    15.1 Check if a Binary Tree is BST O(n) time and O(n) space

--- EPI/src/utils/BinaryTreeNodeWithMinAndMax.java
+++ EPI/src/utils/BinaryTreeNodeWithMinAndMax.java
@@ -3,14 +3,17 @@ package utils;
 /**
  * Created by Pankaj on 7/30/15.
  */
-public class BinaryTreeNodeWithMinAndMax extends BinaryTreeNode {
+public class BinaryTreeNodeWithMinAndMax {
+    private int _val;
     private BinaryTreeNodeWithMinAndMax _left;
     private BinaryTreeNodeWithMinAndMax _right;
     private BinaryTreeNodeWithMinAndMax _min;
     private BinaryTreeNodeWithMinAndMax _max;
 
     public BinaryTreeNodeWithMinAndMax(int val, BinaryTreeNodeWithMinAndMax left, BinaryTreeNodeWithMinAndMax right) {
-        super(val, left, right);
+        this._val = val;
+        this._left = left;
+        this._right = right;
         _min = _max = this;
         if (left != null) {
             if (this.left().min().val() < _min.val())
@@ -24,6 +27,10 @@ public class BinaryTreeNodeWithMinAndMax extends BinaryTreeNode {
             if (this.right().max().val() > _max.val())
                 _max = this.right().max();
         }
+    }
+
+    private int val() {
+        return _val;
     }
 
     public BinaryTreeNodeWithMinAndMax min() {
@@ -40,5 +47,14 @@ public class BinaryTreeNodeWithMinAndMax extends BinaryTreeNode {
 
     public BinaryTreeNodeWithMinAndMax left() {
         return _left;
+    }
+
+    public boolean isBinarySearchTree() {
+        boolean ok = true;
+        if (this.left() != null)
+            ok &= this.left().max().val() <= this.val() && this.left().isBinarySearchTree();
+        if (this.right() != null)
+            ok &= this.right().min().val() >= this.val() && this.right().isBinarySearchTree();
+        return ok;
     }
 }
