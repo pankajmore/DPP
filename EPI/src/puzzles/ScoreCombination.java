2548ac46c8a5002278d83974007254192ac5e352
Pankaj More <pankajmore@gmail.com>
2015-08-10 00:00:24 +0530

    17.1 Score combination

---
+++ EPI/src/puzzles/ScoreCombination.java
@@ -0,0 +1,20 @@
+package puzzles;
+
+import java.util.List;
+
+/**
+ * Created by Pankaj on 8/9/15.
+ */
+public class ScoreCombination {
+    private static int countCombinations(int s, List<Integer> W) {
+        if (s == 0 || W.size() == 0) return 0;
+        if (W.size() == 1) return s % W.get(0) == 0 ? 1 : 0;
+        int x = W.get(W.size() - 1), c = 0;
+        List<Integer> tail = W.subList(1, W.size());
+        while (s >= 0) {
+            c += countCombinations(s, tail);
+            s -= x;
+        }
+        return c;
+    }
+}
