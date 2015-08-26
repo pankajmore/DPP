ce34c1d240ae91a7519e5dbc8bee8f2ce98c08eb
Pankaj More <pankajmore@gmail.com>
2015-08-26 22:42:13 +0530

    17.2 EditDistance with editCost

--- EPI/src/puzzles/LevenshteinDistance.java
+++ EPI/src/puzzles/LevenshteinDistance.java
@@ -4,6 +4,15 @@ package puzzles;
  * Created by Pankaj on 8/22/15.
  */
 public class LevenshteinDistance {
+
+    public static int editDistance(String s1, String s2) {
+        return editDistanceImpl(s1, s2, 1);
+    }
+
+    public static int editDistanceInsertAndDeleteOnly(String s1, String s2) {
+        return editDistanceImpl(s1, s2, 2);
+    }
+
     /**
      * Edit Distance between s1 of length m and s2 or length n
      * Time : O(m*n)
@@ -13,31 +22,26 @@ public class LevenshteinDistance {
      * @param s2
      * @return
      */
-    public static int editDistanceDP(String s1, String s2) {
-        if (s1.isEmpty()) return s2.length();
-        if (s2.isEmpty()) return s1.length();
+    private static int editDistanceImpl(String s1, String s2, int costEdit) {
         if (s1.length() < s2.length()) {
             String tmp = s1;
             s1 = s2;
             s2 = tmp;
         }
         int m = s1.length(), n = s2.length();
-        int A[] = new int[n];
-        A[0] = s1.charAt(0) == s2.charAt(0) ? 0 : 1;
-        for (int i = 1; i < n; i++) A[i] = A[i - 1] + 1;
-        for (int i = 1; i < m; i++) {
+        int A[] = new int[n + 1];
+        for (int i = 0; i <= n; i++) A[i] = i;
+        for (int i = 1; i <= m; i++) {
             int pp, prev = A[0];
-            A[0] += 1;
-            for (int j = 1; j < n; j++) {
+            A[0] = i;
+            for (int j = 1; j <= n; j++) {
                 pp = A[j];
-                A[j] = Math.min(Math.min(A[j - 1], A[j]) + 1,
-                        prev + (s1.charAt(i) == s2.charAt(j) ? 0 : 1));
+                A[j] = s1.charAt(i - 1) == s2.charAt(j - 1) ? prev : Math.min(1 + Math.min(A[j - 1], A[j]), prev + costEdit);
                 prev = pp;
             }
         }
-        return A[n - 1];
+        return A[n];
     }
-
     /**
      * Compute the number of inserts + deletes + edits
      * needed to transform s1 to s2
@@ -46,7 +50,7 @@ public class LevenshteinDistance {
      * @param s2
      * @return
      */
-    public static int editDistance(String s1, String s2) {
+    public static int editDistanceRecursive(String s1, String s2) {
         if (s1.isEmpty()) return s2.length();
         if (s2.isEmpty()) return s1.length();
 
