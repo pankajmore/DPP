f059fd2025168b0efa082ed4ee0a7d56cff2e834
Pankaj More <pankajmore@gmail.com>
2015-09-07 23:34:44 +0530

    17.3 Compute binomial coefficients (with test)

--- EPI/src/puzzles/BinomialCoefficients.java
+++ EPI/src/puzzles/BinomialCoefficients.java
@@ -1,22 +1,41 @@
 package puzzles;
 
+import static java.lang.Math.min;
+
 /**
  * Created by Pankaj on 9/5/15.
  */
 public class BinomialCoefficients {
+    /**
+     * Time Complexity : Exponential
+     * Space : O(n)
+     *
+     * @param n
+     * @param k
+     * @return n Choose k
+     */
     public static int nCkRecursive(int n, int k) {
+        if (n < k) return 0;
         if (n == k || k == 0) return 1;
+        if (k > n / 2) k = n - k;
         return nCkRecursive(n - 1, k) + nCkRecursive(n - 1, k - 1);
     }
 
+    /**
+     * Time Complexity : O(n*k)
+     * Space : O(k)
+     *
+     * @param n
+     * @param k
+     * @return n Choose k
+     */
     public static int nCk(int n, int k) {
-        int A[] = new int[n + 1];
-        for (int i = 0; i <= n; i++) A[i] = 1;
-        for (int j = 1; j <= k; j++) {
-            for (int i = 1; i <= n; i++) {
-                A[i] += A[i - 1];
-            }
-        }
-        return A[n];
+        if (n < k) return 0;
+        if (k > n / 2) k = n - k;
+        int A[] = new int[k + 1];
+        A[0] = 1;
+        for (int i = 1; i <= n; i++)
+            for (int j = min(i, k); j > 0; j--) A[j] += A[j - 1];
+        return A[k];
     }
 }
---
+++ EPI/test/puzzles/BinomialCoefficientsTest.java
@@ -0,0 +1,25 @@
+package puzzles;
+
+import org.junit.Test;
+import org.junit.contrib.theories.Theories;
+import org.junit.runner.RunWith;
+
+import static puzzles.BinomialCoefficients.nCk;
+import static puzzles.BinomialCoefficients.nCkRecursive;
+
+@RunWith(Theories.class)
+public class BinomialCoefficientsTest {
+
+    @Test
+    public void nCkTest() {
+        assert compareWithTrivial(0, 10);
+        assert compareWithTrivial(10, 0);
+        assert compareWithTrivial(4, 2);
+        assert compareWithTrivial(20, 10);
+        assert compareWithTrivial(20, 19);
+    }
+
+    private boolean compareWithTrivial(int n, int k) {
+        return nCk(n, k) == nCkRecursive(n, k);
+    }
+}
