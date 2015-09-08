56b11c6b5531509b93d68464b966b2057855d462
Pankaj More <pankajmore@gmail.com>
2015-09-08 23:59:50 +0530

    18.6 3-Sum Problem (trivial attempt with another test)

--- EPI/test/puzzles/ThreeSumTest.java
+++ EPI/test/puzzles/ThreeSumTest.java
@@ -5,7 +5,10 @@ import org.junit.contrib.theories.Theories;
 import org.junit.contrib.theories.Theory;
 import org.junit.runner.RunWith;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 @RunWith(Theories.class)
 public class ThreeSumTest {
@@ -16,5 +19,30 @@ public class ThreeSumTest {
         int k = A[new Random().nextInt(n)] + A[new Random().nextInt(n)]
                 + A[new Random().nextInt(n)];
         assert ThreeSum.slow(A, k);
+    }
+
+    @Theory
+    public void carefullyChosenMustNotExist(@ForAll Integer[] A) {
+        int k = notDefinitelyInA(A);
+        assert !ThreeSum.slow(A, k);
+    }
+
+    private int notDefinitelyInA(Integer[] A) {
+        Set<Integer> set = new HashSet<>(Arrays.asList(A));
+        while (true) {
+            int x = new Random().nextInt();
+            x = 3 * Math.abs(x);
+            if (set.contains(x / 3)) continue;
+            for (int d = 0; d <= x; d++) if (twoSumCheck(A, x - d)) continue;
+            return x;
+        }
+    }
+
+    private boolean twoSumCheck(Integer[] A, Integer k) {
+        Set<Integer> set = new HashSet<>(Arrays.asList(A));
+        for (Integer x : A) {
+            if (set.contains(k - x)) return true;
+        }
+        return false;
     }
 }
