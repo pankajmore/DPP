778866a4bcf405440090dd92a3a11bd70a6f4757
Pankaj More <pankajmore@gmail.com>
2015-09-11 00:01:23 +0530

    18.6.3  3-sum problem: Triple closest to K

--- EPI/src/puzzles/ThreeSum.java
+++ EPI/src/puzzles/ThreeSum.java
@@ -1,8 +1,6 @@
 package puzzles;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by Pankaj on 9/8/15.
@@ -27,4 +25,22 @@ public class ThreeSum {
 
     /* There is an O(nlogn) solution if numbers are non-negative and max(A) ~ O(n)
     * Using FFT*/
+
+    public static List<Integer> closest(Integer[] A, int k) {
+        Arrays.sort(A);
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (Integer x : A) {
+            for (Integer y : A) {
+                int t = Arrays.binarySearch(A, k - x - y);
+                if (t >= 0 && t < A.length) return Arrays.asList(x, y, k - x - y);
+                else if (t == A.length) map.put(k - x - y - A[A.length - 1], Arrays.asList(x, y, k - x - y));
+                else {
+                    if (A[-t + 1] - k > k - A[-t]) map.put(A[-t + 1] - k, Arrays.asList(x, y, A[-t + 1]));
+                    else map.put(k - A[-t], Arrays.asList(x, y, A[-t]));
+                }
+            }
+        }
+        int minKey = Collections.min(map.keySet());
+        return map.get(minKey);
+    }
 }
