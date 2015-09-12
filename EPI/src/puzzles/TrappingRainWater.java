a7cf7c7b7065c9c8b5cb8988cd5a1d64635d0bee
Pankaj More <pankajmore@gmail.com>
2015-09-12 21:05:06 +0530

    18.3 Trapping water (todo)

---
+++ EPI/src/puzzles/TrappingRainWater.java
@@ -0,0 +1,18 @@
+package puzzles;
+
+/**
+ * Created by Pankaj on 9/12/15.
+ */
+public class TrappingRainWater {
+    /**
+     * Time : O(n^2)
+     * Space : O(n)
+     *
+     * @param A
+     * @return
+     */
+    //TODO
+    public static int capacity(Integer[] A) {
+        return 0;
+    }
+}
