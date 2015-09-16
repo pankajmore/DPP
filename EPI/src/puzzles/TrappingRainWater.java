807405919cda94f05b55b6d15e1c61ed83ca7091
Pankaj More <pankajmore@gmail.com>
2015-09-16 11:25:13 +0530

    18.3 Trapping Water O(n) time and space

--- EPI/src/puzzles/TrappingRainWater.java
+++ EPI/src/puzzles/TrappingRainWater.java
@@ -5,14 +5,27 @@ package puzzles;
  */
 public class TrappingRainWater {
     /**
-     * Time : O(n^2)
+     * Time : O(n)
      * Space : O(n)
      *
-     * @param A
-     * @return
+     * @param A array of non-negative integers
+     * @return the capacity of the 1-D tank with unit width rectangle and height from A
      */
-    //TODO
-    public static int capacity(Integer[] A) {
-        return 0;
+    public static int capacity(int[] A) {
+        int N = A.length;
+        int[] L, R, H;
+        L = new int[N + 2];
+        H = new int[N + 2];
+        R = new int[N + 2];
+        for (int i = 1; i <= N; i++) H[i] = A[i - 1];
+        for (int i = 1; i <= N; i++) {
+            L[i] = Math.max(L[i - 1], H[i]);
+            R[N - i + 1] = Math.max(R[N - i + 2], H[N - i + 1]);
+        }
+        int c = 0;
+        for (int i = 1; i <= N; i++) {
+            c += Math.max(Math.min(L[i - 1], R[i + 1]) - H[i], 0);
+        }
+        return c;
     }
 }
---
+++ EPI/test/puzzles/TrappingRainWaterTest.java
@@ -0,0 +1,12 @@
+package puzzles;
+
+import org.junit.Test;
+
+public class TrappingRainWaterTest {
+
+    @Test
+    public void testCapacity() throws Exception {
+        int[] A = {1, 2, 1, 3, 4, 4, 5, 1, 2, 0, 3};
+        assert TrappingRainWater.capacity(A) == 7;
+    }
+}
