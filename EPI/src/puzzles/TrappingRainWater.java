5d4274210b9751978cd4a45d96d1a5521e77ae7a
Pankaj More <pankajmore@gmail.com>
2015-09-21 23:34:27 +0530

    18.3 Trapping Rain Water in O(1) space

--- EPI/src/puzzles/TrappingRainWater.java
+++ EPI/src/puzzles/TrappingRainWater.java
@@ -1,5 +1,7 @@
 package puzzles;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Created by Pankaj on 9/12/15.
  */
@@ -11,13 +13,13 @@ public class TrappingRainWater {
      * @param A array of non-negative integers
      * @return the capacity of the 1-D tank with unit width rectangle and height from A
      */
-    public static int capacity(int[] A) {
+    public static int capacity(@NotNull int[] A) {
         int N = A.length;
         int[] L, R, H;
         L = new int[N + 2];
         H = new int[N + 2];
         R = new int[N + 2];
-        for (int i = 1; i <= N; i++) H[i] = A[i - 1];
+        System.arraycopy(A, 0, H, 1, N);
         for (int i = 1; i <= N; i++) {
             L[i] = Math.max(L[i - 1], H[i]);
             R[N - i + 1] = Math.max(R[N - i + 2], H[N - i + 1]);
@@ -29,5 +31,38 @@ public class TrappingRainWater {
         return c;
     }
 
-    //TODO: implement O(1) space optimized solution
+
+    /**
+     * Time : O(n)
+     * Space : O(1)
+     *
+     * @param A array of non-negative integers denoting the height of each unit-width rectangle
+     * @return the maximum capacity of the container formed by A
+     */
+    public static int capacity1(@NotNull int[] A) {
+        int N = A.length;
+        int c = 0, maxSoFar = 0;
+        int m = maxIndex(A);
+        for (int i = 0; i < m; i++) {
+            c += Math.max(maxSoFar - A[i], 0);
+            maxSoFar = Math.max(maxSoFar, A[i]);
+        }
+        maxSoFar = 0;
+        for (int i = N - 1; i > m; i--) {
+            c += Math.max(maxSoFar - A[i], 0);
+            maxSoFar = Math.max(maxSoFar, A[i]);
+        }
+        return c;
+    }
+
+    private static int maxIndex(@NotNull int[] A) {
+        int mIndex = 0;
+        int mVal = A[0];
+        for (int i = 0; i < A.length; i++)
+            if (A[i] > mVal) {
+                mVal = A[i];
+                mIndex = i;
+            }
+        return mIndex;
+    }
 }
--- EPI/test/puzzles/TrappingRainWaterTest.java
+++ EPI/test/puzzles/TrappingRainWaterTest.java
@@ -8,5 +8,6 @@ public class TrappingRainWaterTest {
     public void testCapacity() throws Exception {
         int[] A = {1, 2, 1, 3, 4, 4, 5, 1, 2, 0, 3};
         assert TrappingRainWater.capacity(A) == 7;
+        assert TrappingRainWater.capacity1(A) == 7;
     }
 }
