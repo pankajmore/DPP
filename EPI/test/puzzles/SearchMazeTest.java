5397888e474e3f5837da291442cd48c6b1b72934
Pankaj More <pankajmore@gmail.com>
2015-09-24 23:20:29 +0530

    19.1 Search Maze O(n^2) time and space

---
+++ EPI/test/puzzles/SearchMazeTest.java
@@ -0,0 +1,38 @@
+package puzzles;
+
+import org.junit.Test;
+import puzzles.SearchMaze.Point;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static puzzles.SearchMaze.checkPath;
+import static puzzles.SearchMaze.searchMaze;
+
+public class SearchMazeTest {
+
+    @Test
+    public void testSearchMaze() throws Exception {
+        List<List<Integer>> maze = Arrays.asList(
+                Arrays.asList(1, 0, 0, 0, 0, 0, 1, 1, 0, 0),
+                Arrays.asList(0, 0, 1, 0, 0, 0, 0, 0, 0, 0),
+                Arrays.asList(1, 0, 1, 0, 0, 1, 1, 0, 1, 1),
+                Arrays.asList(0, 0, 0, 1, 1, 1, 0, 0, 1, 0),
+                Arrays.asList(0, 1, 1, 0, 0, 0, 0, 0, 0, 0),
+                Arrays.asList(0, 1, 1, 0, 0, 1, 0, 1, 1, 0),
+                Arrays.asList(0, 0, 0, 0, 1, 0, 0, 0, 0, 0),
+                Arrays.asList(1, 0, 1, 0, 1, 0, 1, 0, 0, 0),
+                Arrays.asList(1, 0, 1, 1, 0, 0, 0, 1, 1, 1),
+                Arrays.asList(0, 0, 0, 0, 0, 0, 0, 1, 1, 0)
+        );
+        List<List<Integer>> originalMaze = new ArrayList<>();
+        for (List<Integer> row : maze) originalMaze.add(new ArrayList<>(row));
+
+        Point start = new Point(9, 0), stop = new Point(0, 9);
+        List<Point> path = searchMaze(maze, start, stop);
+        System.out.println(path);
+        assert path != null && !path.isEmpty();
+        assert checkPath(path, originalMaze, start, stop);
+    }
+}
