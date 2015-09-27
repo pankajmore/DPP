60a137332150f8b2fdd0c5f70accb4415830f0a5
Pankaj More <pankajmore@gmail.com>
2015-09-27 22:46:56 +0530

    5.9 Spreadsheet Column Encoding

---
+++ EPI/src/puzzles/SpreadsheetEncoding.java
@@ -0,0 +1,12 @@
+package puzzles;
+
+/**
+ * Created by Pankaj on 9/27/15.
+ */
+public class SpreadsheetEncoding {
+    public static int ssDecodeColID(final String col) {
+        int ret = 0;
+        for (char c : col.toCharArray()) ret = ret * 26 + c - 'A' + 1;
+        return ret;
+    }
+}
