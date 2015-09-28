7b21a6500279155962bc16d0d27d01601b4c4eab
Pankaj More <pankajmore@gmail.com>
2015-09-28 21:50:48 +0530

    5.10 Reverse Digits

---
+++ EPI/test/puzzles/ReverseIntegerTest.java
@@ -0,0 +1,15 @@
+package puzzles;
+
+import com.pholser.junit.quickcheck.ForAll;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
+
+@RunWith(Theories.class)
+public class ReverseIntegerTest {
+    @Theory
+    public void testReverse(@ForAll int x) {
+        if (Math.abs(x) > Integer.MAX_VALUE / 10) return; // at boundaries reverse might overflow
+        assert ReverseInteger.reverse(x) == ReverseInteger.reverseUsingString(x);
+    }
+}
