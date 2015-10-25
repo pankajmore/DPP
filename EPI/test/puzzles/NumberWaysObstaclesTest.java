327e8ebb6daea8a24294df9d3197b14ca69711a0
Pankaj More <pankajmore@gmail.com>
2015-10-25 17:09:53 +0530

    17.4 Number of ways in a 2D array with obstacles Time : O(mn) Space : O(n)

---
+++ EPI/test/puzzles/NumberWaysObstaclesTest.java
@@ -0,0 +1,22 @@
+package puzzles;
+
+import org.junit.Test;
+
+import static puzzles.NumberWaysObstacles.numberOfWaysWithObstacles;
+
+public class NumberWaysObstaclesTest {
+
+    @Test
+    public void testNumberOfWaysWithObstacles() throws Exception {
+        boolean[][] B = {{false, false}, {false, false}};
+        assert numberOfWaysWithObstacles(B) == 2;
+
+        B = new boolean[][]{
+                {false, false, false},
+                {false, false, false},
+                {false, true, false},
+                {true, false, false},
+        };
+        assert numberOfWaysWithObstacles(B) == 3;
+    }
+}
