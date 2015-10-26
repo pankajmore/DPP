970f10f082796bd18c298516c60ba75a43b7ad17
Pankaj More <pankajmore@gmail.com>
2015-10-26 23:12:32 +0530

    17.4.2 Number of strictly monotonic decimals of length k

--- EPI/test/puzzles/MonotonicDecimalsTest.java
+++ EPI/test/puzzles/MonotonicDecimalsTest.java
@@ -2,8 +2,7 @@ package puzzles;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-import static puzzles.MonotonicDecimals.numberOfMonotonicDecimals;
+import static puzzles.MonotonicDecimals.*;
 
 public class MonotonicDecimalsTest {
 
@@ -12,5 +11,21 @@ public class MonotonicDecimalsTest {
         assert numberOfMonotonicDecimals(0) == 0;
         assert numberOfMonotonicDecimals(1) == 9;
         assert numberOfMonotonicDecimals(2) == 45;
+
+        for (int length = 1; length < 9; length++)
+            assert numberOfMonotonicDecimals(length) == monotonicDecimalsRecursive(length).size();
+    }
+
+    @Test
+    public void testNumberOfStrictlyMonotonicDecimals() throws Exception {
+        assert numberOfStrictlyMonotonicDecimals(0) == 0;
+        assert numberOfStrictlyMonotonicDecimals(1) == 9;
+        assert numberOfStrictlyMonotonicDecimals(2) == 36;
+
+        for (int length = 1; length < 9; length++) {
+            int actual = numberOfStrictlyMonotonicDecimals(length);
+            int expected = strictlyMonotonicDecimalsRecursive(length).size();
+            assert expected == actual;
+        }
     }
 }
