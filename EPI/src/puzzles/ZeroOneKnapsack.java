2a4d5542982b5e5a0dcebea8acf8e1fac7fd31a4
Pankaj More <pankajmore@gmail.com>
2015-10-29 22:30:21 +0530

    17.7 we can overwrite the same array

--- EPI/src/puzzles/ZeroOneKnapsack.java
+++ EPI/src/puzzles/ZeroOneKnapsack.java
@@ -8,12 +8,11 @@ import java.util.List;
 public class ZeroOneKnapsack {
     public static int knapsack(int capacity, List<Item> items) {
         int N = items.size();
-        int[][] V = new int[2][capacity + 1];
+        int[] V = new int[capacity + 1];
         for (int i = 1; i < N; i++)
-            for (int w = 0; w <= capacity; w++)
-                V[i % 2][w] = items.get(i).weight <= w ? Math.max(V[(i - 1) % 2][w], V[(i - 1) % 2][w - items.get(i).weight] + items.get(i).value)
-                        : V[(i - 1) % 2][w];
-        return V[(N - 1) % 2][capacity];
+            for (int w = capacity; w >= items.get(i).weight; w--)
+                V[w] = Math.max(V[w], V[w - items.get(i).weight] + items.get(i).value);
+        return V[capacity];
     }
 
     public static class Item {
