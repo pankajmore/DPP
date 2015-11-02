0f1e20b5eb04751c5e9dc0730021c3d54c5da1eb
Pankaj More <pankajmore@gmail.com>
2015-11-02 23:24:53 +0530

    18.9 Find majority element if it exists. Time : O(n) Space : O(1) + a test

--- EPI/src/puzzles/SearchMajority.java
+++ EPI/src/puzzles/SearchMajority.java
@@ -1,7 +1,35 @@
 package puzzles;
 
+import java.io.InputStream;
+import java.util.Scanner;
+
 /**
  * Created by Pankaj on 11/1/15.
  */
 public class SearchMajority {
+    public static String majoritySearch(InputStream sin) {
+        String candidate = "";
+        Scanner in = new Scanner(sin);
+        int count = 0;
+        while (in.hasNextLine()) {
+            if (count == 0) {
+                candidate = in.nextLine();
+                count++;
+            } else {
+                if (candidate.equals(in.nextLine())) count++;
+                else count--;
+            }
+        }
+        return candidate;
+    }
+
+    public static boolean checkIfMajority(InputStream sin, String ans) {
+        int count = 0, N = 0;
+        Scanner in = new Scanner(sin);
+        while (in.hasNextLine()) {
+            if (in.nextLine().equals(ans)) count++;
+            N++;
+        }
+        return count > N / 2;
+    }
 }
--- EPI/test/puzzles/SearchMajorityTest.java
+++ EPI/test/puzzles/SearchMajorityTest.java
@@ -1,7 +1,34 @@
 package puzzles;
 
-import static org.junit.Assert.*;
+import com.pholser.junit.quickcheck.ForAll;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
 
+import java.io.*;
+import java.util.Collections;
+import java.util.List;
+
+import static puzzles.SearchMajority.checkIfMajority;
+import static puzzles.SearchMajority.majoritySearch;
+
+@RunWith(Theories.class)
 public class SearchMajorityTest {
 
+    @Theory
+    public void testMajoritySearch(@ForAll List<String> ls) throws IOException {
+        int N = ls.size();
+        if (N == 0) return;
+        for (int i = 0; i <= N; i++) ls.add(ls.get(0));
+        Collections.shuffle(ls);
+        InputStream sin;
+        ByteArrayOutputStream sout = new ByteArrayOutputStream();
+        OutputStreamWriter osw = new OutputStreamWriter(sout);
+        for (String s : ls) osw.write(s + "\n");
+        osw.close();
+        sin = new ByteArrayInputStream(sout.toByteArray());
+        String expected = majoritySearch(sin);
+        sin.reset();
+        assert checkIfMajority(sin, expected);
+    }
 }
