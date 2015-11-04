c9b891e39fdc2dac394528beff876ed6624816a0
Pankaj More <pankajmore@gmail.com>
2015-11-04 10:50:37 +0530

    18.11 O(n) solution with randomized testing

--- EPI/src/puzzles/ContainerWithMostWater.java
+++ EPI/src/puzzles/ContainerWithMostWater.java
@@ -7,6 +7,35 @@ import java.util.List;
  */
 public class ContainerWithMostWater {
     /**
+     * Time : O(n)
+     * Space : O(1)
+     * <p>
+     * Invariant Property :
+     * If A[0] < A[n-1], for all x area(0,x) <= area(0,n-1),
+     * we can discard A[0].
+     * If A[0] > A[n-1], for all x area(x,n-1) <= area(0,n-1),
+     * we can discard A[n-1]
+     * If A[0] == A[n-1], we can discard both
+     *
+     * @param heights the list of heights of vertical lines
+     * @return the maximum amount of water that can be filled between
+     * and two vertical lines
+     */
+    public static int getMaxArea(List<Integer> heights) {
+        int N = heights.size(), i = 0, j = N - 1, maxArea = 0;
+        while (i < j) {
+            maxArea = Math.max(maxArea, Math.min(heights.get(i), heights.get(j)) * (j - i));
+            if (heights.get(i) < heights.get(j)) ++i;
+            else if (heights.get(i) > heights.get(j)) --j;
+            else {
+                ++i;
+                --j;
+            }
+        }
+        return maxArea;
+    }
+
+    /**
      * Time : O(n^2)
      * Space : O(1)
      *
--- EPI/test/puzzles/ContainerWithMostWaterTest.java
+++ EPI/test/puzzles/ContainerWithMostWaterTest.java
@@ -1,18 +1,31 @@
 package puzzles;
 
+import com.pholser.junit.quickcheck.ForAll;
 import org.junit.Test;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static puzzles.ContainerWithMostWater.getMaxArea;
 import static puzzles.ContainerWithMostWater.getMaxAreaBruteForce;
 
+@RunWith(Theories.class)
 public class ContainerWithMostWaterTest {
 
+    @Theory
+    public void greedyAndBruteForceShouldMatch(@ForAll List<Integer> heights) {
+        for (int i = 0; i < heights.size(); i++) heights.set(i, Math.abs(heights.get(i)) / heights.size());
+        assert getMaxArea(heights) == getMaxAreaBruteForce(heights);
+    }
+
     @Test
-    public void testGetMaxAreaBruteForce() throws Exception {
+    public void testBookExample() {
         List<Integer> A
                 = Arrays.asList(1, 2, 1, 3, 4, 4, 5, 6, 2, 1, 3, 1, 3, 2, 1, 2, 4, 1);
         assert (48 == getMaxAreaBruteForce(A));
+        assert (48 == getMaxArea(A));
     }
 }
