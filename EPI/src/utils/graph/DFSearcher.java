ae014c5278c7a3a97ca4f489b84e26603b1b0d33
Pankaj More <pankajmore@gmail.com>
2015-11-06 23:51:17 +0530

    Algorithm to find if a vertex v is reachable from vertex u in a directed graph (forest).

---
+++ EPI/src/utils/graph/DFSearcher.java
@@ -0,0 +1,24 @@
+package utils.graph;
+
+/**
+ * Created by Pankaj on 11/6/15.
+ */
+public class DFSearcher {
+    boolean[] visited;
+
+    public DFSearcher(DirectedGraph g, int s) {
+        visited = new boolean[g.size()];
+        dfs(g, s);
+    }
+
+    private void dfs(DirectedGraph g, int u) {
+        visited[u] = true;
+        for (Integer v : g.adj(u)) {
+            if (!visited[v]) dfs(g, v);
+        }
+    }
+
+    public boolean reachable(int v) {
+        return visited[v];
+    }
+}
