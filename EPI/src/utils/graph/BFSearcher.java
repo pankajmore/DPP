4f8f4bfc9a341822ee6663e3f78ef2c7852389ad
Pankaj More <pankajmore@gmail.com>
2015-11-07 22:59:00 +0530

    BFS Search

---
+++ EPI/src/utils/graph/BFSearcher.java
@@ -0,0 +1,26 @@
+package utils.graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by Pankaj on 11/7/15.
+ */
+public class BFSearcher {
+    boolean[] visited;
+
+    public BFSearcher(DirectedGraph g, int s) {
+        visited = new boolean[g.size()];
+        bfs(g, s);
+    }
+
+    private void bfs(DirectedGraph g, int s) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(s);
+        while (!q.isEmpty()) {
+            Integer u = q.poll();
+            visited[u] = true;
+            for (Integer v : g.adj(u)) if (!visited[v]) q.add(v);
+        }
+    }
+}
