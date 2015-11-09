632ecfc0dad46a5b5dbdaa9fc0e57fd3b2702b9e
Pankaj More <pankajmore@gmail.com>
2015-11-09 23:35:45 +0530

    detect if a directed graph is a DAG

--- EPI/src/utils/graph/CycleDetector.java
+++ EPI/src/utils/graph/CycleDetector.java
@@ -4,4 +4,29 @@ package utils.graph;
  * Created by Pankaj on 11/8/15.
  */
 public class CycleDetector {
+    private boolean[] visited;
+    private boolean[] done;
+    private boolean dagflag;
+
+    public CycleDetector(DirectedGraph g) {
+        visited = new boolean[g.size()];
+        done = new boolean[g.size()];
+        for (int v = 0; v < g.size(); v++)
+            if (!visited[v])
+                dagflag &= search(g, v);
+    }
+
+    private boolean search(DirectedGraph g, int u) {
+        visited[u] = true;
+        for (Integer v : g.adj(u)) {
+            if (!visited[v]) search(g, v);
+            else if (!done[v]) return false;
+        }
+        done[u] = true;
+        return true;
+    }
+
+    public boolean isDAG() {
+        return dagflag;
+    }
 }
