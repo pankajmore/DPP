82165cf463160d86bc5eb7c5c5268523f2e604bb
Pankaj More <pankajmore@gmail.com>
2015-11-13 00:41:49 +0530

    Test for topological sort

---
+++ EPI/test/utils/graph/TopologicalSorterTest.java
@@ -0,0 +1,16 @@
+package utils.graph;
+
+import examples.graph.DirectedGraphs;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class TopologicalSorterTest {
+
+    @Test
+    public void testTopologicalOrder() throws Exception {
+        int[] expected = {0, 2, 1, 3};
+        int[] actual = new TopologicalSorter(DirectedGraphs.diamond).topologicalOrder();
+        assert Arrays.equals(actual, expected);
+    }
+}
