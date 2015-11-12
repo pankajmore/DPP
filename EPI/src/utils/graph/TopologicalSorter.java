d6c58a14cff0c4bbccf4be4c71cacf05d76cdc36
Pankaj More <pankajmore@gmail.com>
2015-11-13 00:41:25 +0530

    TopologicalSorter

---
+++ EPI/src/utils/graph/TopologicalSorter.java
@@ -0,0 +1,31 @@
+package utils.graph;
+
+/**
+ * Created by Pankaj on 11/13/15.
+ */
+public class TopologicalSorter {
+    private int count;
+    private boolean[] visited;
+    private int[] ts;
+
+    public TopologicalSorter(DirectedGraph g) {
+        visited = new boolean[g.size()];
+        ts = new int[g.size()];
+        count = g.size();
+        for (int v = 0; v < g.size(); v++) {
+            if (!visited[v]) tsort(g, v);
+        }
+    }
+
+    public int[] topologicalOrder() {
+        return ts;
+    }
+
+    private void tsort(DirectedGraph g, int u) {
+        visited[u] = true;
+        for (Integer v : g.adj(u)) {
+            if (!visited[v]) tsort(g, v);
+        }
+        ts[--count] = u;
+    }
+}
