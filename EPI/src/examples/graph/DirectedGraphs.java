ff05ba1905effab5ce8990efabc24d85cd2b984c
Pankaj More <pankajmore@gmail.com>
2015-11-17 21:19:31 +0530

    example graph from https://en.wikipedia.org/wiki/Strongly_connected_component

--- EPI/src/examples/graph/DirectedGraphs.java
+++ EPI/src/examples/graph/DirectedGraphs.java
@@ -8,4 +8,7 @@ import utils.graph.DirectedGraph;
 public class DirectedGraphs {
     public static DirectedGraph twoNodeCycle = new DirectedGraph(2).addEdge(0, 1).addEdge(1, 0);
     public static DirectedGraph diamond = new DirectedGraph(4).addEdge(0, 1).addEdge(0, 2).addEdge(1, 3).addEdge(2, 3);
+    public static DirectedGraph wikiExample = new DirectedGraph(8).addEdge(0, 1).addEdge(1, 2).addEdge(1, 4).addEdge(1, 5)
+            .addEdge(2, 3).addEdge(2, 6).addEdge(3, 2).addEdge(3, 7).addEdge(4, 0).addEdge(4, 5).addEdge(5, 6).addEdge(6, 5)
+            .addEdge(7, 3).addEdge(7, 6);
 }
