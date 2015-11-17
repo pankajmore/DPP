2b4bcf179efed158cbe17b55411b40b9980e0352
Pankaj More <pankajmore@gmail.com>
2015-11-17 21:16:21 +0530

    a non-trivial test case and boilerplate to compare to nested sets

--- EPI/test/utils/graph/StronglyConnectedComponentsTest.java
+++ EPI/test/utils/graph/StronglyConnectedComponentsTest.java
@@ -7,8 +7,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.Assert.*;
-
 public class StronglyConnectedComponentsTest {
 
     @Test
@@ -16,6 +14,22 @@ public class StronglyConnectedComponentsTest {
         Set<Set<Integer>> actual = new StronglyConnectedComponents(DirectedGraphs.twoNodeCycle).sccs();
         Set<Set<Integer>> expected = new HashSet<>();
         expected.add(new HashSet<>(Arrays.asList(0, 1)));
-        assert actual.iterator().next().equals(expected.iterator().next());
+        assert compare(actual, expected);
+
+        actual = new StronglyConnectedComponents(DirectedGraphs.wikiExample).sccs();
+        expected.clear();
+        expected.add(new HashSet<>(Arrays.asList(0, 1, 4)));
+        expected.add(new HashSet<>(Arrays.asList(2, 3, 7)));
+        expected.add(new HashSet<>(Arrays.asList(5, 6)));
+        assert compare(actual, expected);
+    }
+
+    private boolean compare(Set<Set<Integer>> s1, Set<Set<Integer>> s2) {
+        if (s1.size() != s2.size()) return false;
+        for (Set<Integer> aS1 : s1) {
+            if (!s2.contains(aS1))
+                return false;
+        }
+        return true;
     }
 }
