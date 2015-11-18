0afa51bdb40242b82ddd27ca5ae36b3cbc942845
Pankaj More <pankajmore@gmail.com>
2015-11-18 23:53:54 +0530

    Timer class which supports only 1 task

---
+++ EPI/src/puzzles/Timer.java
@@ -0,0 +1,39 @@
+package puzzles;
+
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Created by Pankaj on 11/18/15.
+ */
+public class Timer {
+    private final TimerThread timerThread = new TimerThread();
+    private final TimerTask task;
+    private int delay;
+    private AtomicBoolean cancelled = new AtomicBoolean(false);
+
+    Timer(TimerTask t) {
+        task = t;
+    }
+
+    public void schedule(int delay) {
+        this.delay = delay;
+        timerThread.start();
+    }
+
+    public void cancel() {
+        cancelled.set(true);
+    }
+
+    class TimerThread extends Thread {
+        public void run() {
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            // do we need to synchronize here?
+            if (!cancelled.get()) new Thread(task).start();
+        }
+    }
+}
