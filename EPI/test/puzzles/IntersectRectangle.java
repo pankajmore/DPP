68b43fcda8657ab598eeba4b863ddc4291b10268
Pankaj More <pankajmore@gmail.com>
2015-11-20 12:13:14 +0530

    5.13 Interect Rectangle

---
+++ EPI/test/puzzles/IntersectRectangle.java
@@ -0,0 +1,32 @@
+package puzzles;
+
+/**
+ * Created by Pankaj on 11/20/15.
+ */
+public class IntersectRectangle {
+    public static Rectangle intersectRectangle(Rectangle R1, Rectangle R2) {
+        if (!isIntersect(R1, R2)) return new Rectangle(0, 0, -1, -1);
+        return new Rectangle(
+                Math.max(R1.x, R2.x),
+                Math.max(R1.y, R2.y),
+                Math.min(R1.x + R1.width, R2.x + R2.width) - Math.max(R1.x, R2.x),
+                Math.min(R1.y + R1.height, R2.y + R2.height) - Math.max(R1.y, R2.y)
+        );
+    }
+
+    private static boolean isIntersect(Rectangle R1, Rectangle R2) {
+        return R1.x <= R2.x + R2.width && R1.x + R1.width >= R2.x
+                && R1.y <= R2.y + R2.height && R1.y + R1.height >= R2.y;
+    }
+
+    public static class Rectangle {
+        public final int x, y, width, height;
+
+        public Rectangle(int x, int y, int width, int height) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
---
+++ EPI/test/puzzles/IntersectRectangleTest.java
@@ -0,0 +1,29 @@
+package puzzles;
+
+import org.junit.Test;
+import puzzles.IntersectRectangle.Rectangle;
+
+import static puzzles.IntersectRectangle.intersectRectangle;
+
+public class IntersectRectangleTest {
+
+    @Test
+    public void testIntersectRectangle() throws Exception {
+        Rectangle R1, R2;
+        R1 = new Rectangle(0, 0, 2, 2);
+        R2 = new Rectangle(1, 1, 3, 3);
+        Rectangle result = intersectRectangle(R1, R2);
+        assert (result.x == 1 && result.y == 1 && result.width == 1
+                && result.height == 1);
+        R1 = new Rectangle(0, 0, 1, 1);
+        R2 = new Rectangle(1, 1, 3, 3);
+        result = intersectRectangle(R1, R2);
+        assert (result.x == 1 && result.y == 1 && result.width == 0
+                && result.height == 0);
+        R1 = new Rectangle(0, 0, 1, 1);
+        R2 = new Rectangle(2, 2, 3, 3);
+        result = intersectRectangle(R1, R2);
+        assert (result.x == 0 && result.y == 0 && result.width == -1
+                && result.height == -1);
+    }
+}
