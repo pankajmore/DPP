4bed18e8604b2f28b02abd0d33357f7609639c6f
Pankaj More <pankajmore@gmail.com>
2015-11-22 21:10:01 +0530

    7.6 The Look and Say problem

---
+++ EPI/test/puzzles/LookAndSay.java
@@ -0,0 +1,41 @@
+package puzzles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Pankaj on 11/22/15.
+ */
+public class LookAndSay {
+    public static String lookAndSay(int n) {
+        String s = "1";
+        for (int i = 2; i <= n; i++) s = next(s);
+        return s;
+    }
+
+    public static List<String> lookAndSayList(int n) {
+        List<String> l = new ArrayList<>();
+        String s = "1";
+        l.add(s);
+        for (int i = 2; i <= n; i++) {
+            s = next(s);
+            l.add(s);
+        }
+        return l;
+    }
+
+    private static String next(String s) {
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) != s.charAt(i - 1)) {
+                sb.append(count);
+                sb.append(s.charAt(i - 1));
+                count = 1;
+            } else count++;
+        }
+        sb.append(count);
+        sb.append(s.charAt(s.length() - 1));
+        return sb.toString();
+    }
+}
---
+++ EPI/test/puzzles/LookAndSayTest.java
@@ -0,0 +1,16 @@
+package puzzles;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static puzzles.LookAndSay.lookAndSayList;
+
+public class LookAndSayTest {
+
+    @Test
+    public void testLookAndSayList() throws Exception {
+        assert Arrays.asList("1", "11", "21", "1211", "111221", "312211", "13112221", "1113213211")
+                .equals(lookAndSayList(8));
+    }
+}
