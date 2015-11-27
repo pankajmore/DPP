bdc3fa0040d4ecd86e6fea011b0edc2a76134048
Pankaj More <pankajmore@gmail.com>
2015-11-27 21:49:39 +0530

    10.9 InOrder traversal of a binary tree in O(1) space using parent pointers

--- EPI/src/utils/BinaryTreeNodeWithParent.java
+++ EPI/src/utils/BinaryTreeNodeWithParent.java
@@ -1,38 +1,79 @@
 package utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Integer.max;
 import static java.lang.Math.abs;
 
 /**
  * Created by Pankaj on 6/28/15.
  */
-public class BinaryTreeNodeWithParent extends BinaryTreeNode {
-    /**
-     * Pointer to the parent node of this
-     */
+public class BinaryTreeNodeWithParent {
+
+    private int _val;
     private BinaryTreeNodeWithParent _parent;
+    private BinaryTreeNodeWithParent _left;
+    private BinaryTreeNodeWithParent _right;
+
+    public BinaryTreeNodeWithParent(BinaryTreeNode t) {
+        if (t.left() != null) {
+            this._left = new BinaryTreeNodeWithParent(t.left());
+            this._left._parent = this;
+        }
+        if (t.right() != null) {
+            this._right = new BinaryTreeNodeWithParent(t.right());
+            this._right._parent = this;
+        }
+        this._val = t.val();
+    }
 
     public BinaryTreeNodeWithParent(int val, BinaryTreeNodeWithParent left, BinaryTreeNodeWithParent right) {
-        super(val, left, right);
+        this._val = val;
+        this._left = left;
+        this._right = right;
         this._parent = null;
-        left._parent = this;
-        right._parent = this;
+        _left._parent = this;
+        _right._parent = this;
     }
 
-    @Override
-    public List<BinaryTreeNode> inOrder() {
-        return super.inOrder(); // TODO: make it use O(1) space
+    public BinaryTreeNodeWithParent leftMost() {
+        BinaryTreeNodeWithParent n = this;
+        while (n._left != null) n = n._left;
+        return n;
     }
 
-    @Override
-    public List<Integer> preOrder() {
-        return super.preOrder();
+    public Boolean isLeftChild() {
+        return this._parent != null && this._parent._left != null && this._parent._left.equals(this);
     }
 
-    @Override
-    public List<Integer> postOrder() {
-        return super.postOrder();
+    public Boolean isRightChild() {
+        return !isLeftChild();
+    }
+
+    public BinaryTreeNodeWithParent successor() {
+        BinaryTreeNodeWithParent n = this;
+        if (n._right != null) return n._right.leftMost();
+        if (n.isLeftChild()) return n._parent;
+        else {
+            while (n.isRightChild() && n._parent != null) n = n._parent;
+            return n._parent;
+        }
+    }
+
+    //TODO: Implement preOrder and postOrder in O(1) space
+    public List<BinaryTreeNodeWithParent> inOrder() {
+        List<BinaryTreeNodeWithParent> ls = new ArrayList<>();
+        BinaryTreeNodeWithParent n = this.leftMost();
+        while (n != null) {
+            ls.add(n);
+            n = n.successor();
+        }
+        return ls;
+    }
+
+    public int val() {
+        return _val;
     }
 
     public BinaryTreeNodeWithParent leastCommonAncestor(BinaryTreeNodeWithParent n1, BinaryTreeNodeWithParent n2) {
@@ -46,5 +87,11 @@ public class BinaryTreeNodeWithParent extends BinaryTreeNode {
             n2 = n2._parent;
         }
         return n1;
+    }
+
+    public int height() {
+        if (_left == null && _right == null) return 0;
+        else if (_left == null) return 1 + _right.height();
+        else return 1 + max(_left.height(), _right.height());
     }
 }
--- EPI/test/utils/BinaryTreeNodeWithParentTest.java
+++ EPI/test/utils/BinaryTreeNodeWithParentTest.java
@@ -1,26 +1,18 @@
 package utils;
 
+import examples.BinaryTreeExample;
 import org.junit.Test;
 
+import java.util.Arrays;
+
+//TODO: Generate and test on random trees
 public class BinaryTreeNodeWithParentTest {
 
     @Test
     public void testInOrder() throws Exception {
-
-    }
-
-    @Test
-    public void testPreOrder() throws Exception {
-
-    }
-
-    @Test
-    public void testPostOrder() throws Exception {
-
-    }
-
-    @Test
-    public void testLeastCommonAncestor() throws Exception {
-
+        BinaryTreeNodeWithParent bookExample = new BinaryTreeNodeWithParent(BinaryTreeExample.constructBookExample());
+        int[] actual = bookExample.inOrder().stream().mapToInt(BinaryTreeNodeWithParent::val).toArray();
+        int[] expected = BinaryTreeExample.constructBookExample().inOrder().stream().mapToInt(BinaryTreeNode::val).toArray();
+        assert Arrays.equals(actual, expected);
     }
 }
