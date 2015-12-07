5cfeff89fbec59b40efc56309635876bcdf4df41
Pankaj More <pankajmore@gmail.com>
2015-12-07 18:54:36 +0530

    15.6 Build a BST given its preOrder sequence Time : O(n) Space : O(n)

--- EPI/src/puzzles/RebuildBSTPreOrder.java
+++ EPI/src/puzzles/RebuildBSTPreOrder.java
@@ -8,6 +8,7 @@ import java.util.List;
  * Created by Pankaj on 12/6/15.
  */
 public class RebuildBSTPreOrder {
+    static int i;
     /**
      * Builds a BST given its preOrder traversal.
      * Assumes that the keys in the BST are unique.
@@ -24,5 +25,26 @@ public class RebuildBSTPreOrder {
         for (i = 0; i < preOrderSequence.size(); i++) if (preOrderSequence.get(i) > preOrderSequence.get(0)) break;
         return new BinaryTreeNode(preOrderSequence.get(0), rebuildBSTFromPreOrder(preOrderSequence.subList(1, i)),
                 rebuildBSTFromPreOrder(preOrderSequence.subList(i, preOrderSequence.size())));
+    }
+
+    /**
+     * Time : O(n)
+     * Space : O(n)
+     *
+     * @param preOrderSequence list of keys in preOrder traversal of a BST
+     * @return the BST
+     */
+    public static BinaryTreeNode rebuildBSTFromPreOrderFast(List<Integer> preOrderSequence) {
+        i = 0;
+        return rebuildBSTFromPreOrderFastHelper(preOrderSequence, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private static BinaryTreeNode rebuildBSTFromPreOrderFastHelper(List<Integer> preOrderSequence, int minValue, int maxValue) {
+        if (i == preOrderSequence.size()) return null;
+        int val = preOrderSequence.get(i);
+        if (val < minValue || val > maxValue) return null;
+        ++i;
+        return new BinaryTreeNode(val, rebuildBSTFromPreOrderFastHelper(preOrderSequence, minValue, val),
+                rebuildBSTFromPreOrderFastHelper(preOrderSequence, val, maxValue));
     }
 }
--- EPI/test/puzzles/RebuildBSTPreOrderTest.java
+++ EPI/test/puzzles/RebuildBSTPreOrderTest.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static puzzles.RebuildBSTPreOrder.rebuildBSTFromPreOrder;
+import static puzzles.RebuildBSTPreOrder.rebuildBSTFromPreOrderFast;
 
 public class RebuildBSTPreOrderTest {
 
@@ -18,7 +19,10 @@ public class RebuildBSTPreOrderTest {
                 19, 7, 3, 2, 5, 11, 17, 13, 43, 23, 37, 29, 31, 41, 47, 53
         );
         final BinaryTreeNode expected = BinaryTreeExample.constructBSTExample();
-        final BinaryTreeNode actual = rebuildBSTFromPreOrder(preOrderSequence);
-        assert actual.equals(expected);
+        final BinaryTreeNode actual1 = rebuildBSTFromPreOrder(preOrderSequence);
+        assert actual1.equals(expected);
+
+        final BinaryTreeNode actual2 = rebuildBSTFromPreOrderFast(preOrderSequence);
+        assert actual2.equals(expected);
     }
 }
