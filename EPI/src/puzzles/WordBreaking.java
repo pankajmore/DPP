1bd9d35f983b80b2659beb9e3f8e5ef6147d896f
Pankaj More <pankajmore@gmail.com>
2015-12-15 21:06:48 +0530

    17.12 use a substring lookup cache to reduce time to O(n^2) but space becomes O(n^2) as well

--- EPI/src/puzzles/WordBreaking.java
+++ EPI/src/puzzles/WordBreaking.java
@@ -1,6 +1,7 @@
 package puzzles;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -9,8 +10,8 @@ import java.util.Set;
  */
 public class WordBreaking {
     /**
-     * Time: O(s.length()^3)
-     * Space: O(n)
+     * Time: O(s.length()^2)
+     * Space: O(s.length()^2)
      *
      * @param s    the string to break
      * @param dict the dictionary of words
@@ -18,17 +19,39 @@ public class WordBreaking {
      * else null
      */
     public static List<String> wordBreaking(String s, Set<String> dict) {
-        boolean[] solutions = buildSolutionDP(s, dict);
-        return backtrack(s, dict, solutions);
+        boolean[][] subStringCache = cacheSubStringLookup(s, dict);
+        boolean[] solutions = buildSolutionDP(s, subStringCache);
+        return backtrack(s, subStringCache, solutions);
     }
 
-    private static List<String> backtrack(String s, Set<String> dict, boolean[] solutions) {
+    private static boolean[][] cacheSubStringLookup(String s, Set<String> dict) {
+        Set<List<Character>> dictOfChars = new HashSet<>();
+        for (String d : dict) {
+            List<Character> chars = new ArrayList<>();
+            for (Character c : d.toCharArray()) {
+                chars.add(c);
+            }
+            dictOfChars.add(chars);
+        }
+        int N = s.length();
+        boolean[][] cache = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            List<Character> chars = new ArrayList<>();
+            for (int j = i; j < N; j++) {
+                chars.add(s.charAt(j));
+                cache[i][j] = dictOfChars.contains(chars);
+            }
+        }
+        return cache;
+    }
+
+    private static List<String> backtrack(String s, boolean[][] subStringCache, boolean[] solutions) {
         int N = s.length();
         if (!solutions[N]) return null;
         List<String> words = new ArrayList<>();
         int start = 0, end = 0;
         while (++end <= N) {
-            if (solutions[end] && dict.contains(s.substring(start, end))) {
+            if (solutions[end] && subStringCache[start][end - 1]) {
                 words.add(s.substring(start, end));
                 start = end;
             }
@@ -36,13 +59,13 @@ public class WordBreaking {
         return words;
     }
 
-    private static boolean[] buildSolutionDP(String s, Set<String> dict) {
+    private static boolean[] buildSolutionDP(String s, boolean[][] subStringCache) {
         int N = s.length();
         boolean[] dp = new boolean[N + 1];
         dp[0] = true;
         for (int i = 1; i <= N; i++) {
             for (int k = 0; k < i; k++) {
-                if (dp[k] && dict.contains(s.substring(k, i))) {
+                if (dp[k] && subStringCache[k][i - 1]) {
                     dp[i] = true;
                     break;
                 }
