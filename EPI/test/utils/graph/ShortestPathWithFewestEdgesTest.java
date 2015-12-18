352db9374f4f8cf464d5a25fc389801fbe8327aa
Pankaj More <pankajmore@gmail.com>
2015-12-18 20:10:16 +0530

    19.10 Find the shortest path with least number of edges

---
+++ EPI/test/utils/graph/ShortestPathWithFewestEdgesTest.java
@@ -0,0 +1,18 @@
+package utils.graph;
+
+import examples.graph.DirectedWeightedGraphs;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class ShortestPathWithFewestEdgesTest {
+
+    @Test
+    public void testShortestPath() throws Exception {
+        final Dijkstra dijkstra = new Dijkstra(DirectedWeightedGraphs.bookExample, 0);
+        assert dijkstra.shortestPath(7) == 22;
+        assert Arrays.asList(0, 2, 4, 3, 7).equals(dijkstra.shortestPathSequence(7));
+    }
+}
