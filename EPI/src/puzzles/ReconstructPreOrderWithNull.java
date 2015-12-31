12eefccbd30362ac309bbf80eadb4af5e949610e
Pankaj More <pankajmore@gmail.com>
2015-12-31 22:00:47 +0530

    Reconstruct a binary tree given its preorder sequence with markers

---
+++ EPI/src/puzzles/ReconstructPreOrderWithNull.java
@@ -0,0 +1,27 @@
+package puzzles;
+
+import utils.BinaryTreeNode;
+
+import java.util.List;
+
+/**
+ * Created by Pankaj on 12/31/15.
+ */
+public class ReconstructPreOrderWithNull {
+    private static Integer idx;
+
+    public static BinaryTreeNode reconstructPreOrder(List<Integer> preorder) {
+        idx = 0;
+        return reconstructPreOrderHelper(preorder);
+    }
+
+    private static BinaryTreeNode reconstructPreOrderHelper(List<Integer> preorder) {
+        Integer rootVal = preorder.get(idx++);
+        if (rootVal == null) {
+            return null;
+        }
+        BinaryTreeNode left = reconstructPreOrder(preorder);
+        BinaryTreeNode right = reconstructPreOrder(preorder);
+        return new BinaryTreeNode(rootVal, left, right);
+    }
+}
