1f612d1f1d4ca0ca3461e593d16b5cc6f30ad953
Pankaj More <pankajmore@gmail.com>
2016-01-01 21:53:33 +0530

    10.16 Populate next pointers in a perfect binary tree in O(1) space

--- EPI/src/puzzles/PopulatingNextRightPointers.java
+++ EPI/src/puzzles/PopulatingNextRightPointers.java
@@ -4,10 +4,36 @@ package puzzles;
  * Created by Pankaj on 1/1/16.
  */
 public class PopulatingNextRightPointers {
+    /**
+     * Updates the next field for every node in a perfect binary tree
+     * Time : O(n)
+     * Space : O(1)
+     *
+     * @param root the root of the given tree
+     */
     public static void populateNextPointer(BinaryTreeNode<Integer> root) {
+        while (root != null) {
+            BinaryTreeNode<Integer> parent = root;
+            while (parent != null && parent.left != null) {
+                parent.left.next = parent.right;
+                if (parent.next != null) parent.right.next = parent.next.left;
+                parent = parent.next;
+            }
+            root = root.left;
+        }
+    }
+
+    /**
+     * Updates the next field for every node in a perfect binary tree
+     * Time : O(n)
+     * Space : O(logn)
+     *
+     * @param root the root of the given tree
+     */
+    public static void populateNextPointer1(BinaryTreeNode<Integer> root) {
         if (root == null) return;
-        populateNextPointer(root.left);
-        populateNextPointer(root.right);
+        populateNextPointer1(root.left);
+        populateNextPointer1(root.right);
         BinaryTreeNode<Integer> left = root.left;
         BinaryTreeNode<Integer> right = root.right;
         while (left != null && right != null) {
--- EPI/test/puzzles/PopulatingNextRightPointersTest.java
+++ EPI/test/puzzles/PopulatingNextRightPointersTest.java
@@ -4,11 +4,11 @@ import org.junit.Test;
 import puzzles.PopulatingNextRightPointers.BinaryTreeNode;
 
 import static puzzles.PopulatingNextRightPointers.populateNextPointer;
+import static puzzles.PopulatingNextRightPointers.populateNextPointer1;
 
 public class PopulatingNextRightPointersTest {
-
     @Test
-    public void testPopulateNextRightPointer1() throws Exception {
+    public void testPopulateNextRightPointer() throws Exception {
         //     3
         //  2     5
         BinaryTreeNode<Integer> root = new BinaryTreeNode<>(3);
@@ -18,14 +18,11 @@ public class PopulatingNextRightPointersTest {
         assert (root.next == null);
         assert (root.left.next == root.right);
         assert (root.right.next == null);
-    }
 
-    @Test
-    public void testPopulateNextRightPointer2() throws Exception {
         //     3
         //  2     5
         // 1 7   4 6
-        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(3);
+        root = new BinaryTreeNode<>(3);
         root.left = new BinaryTreeNode<>(2);
         root.left.right = new BinaryTreeNode<>(7);
         root.left.left = new BinaryTreeNode<>(1);
@@ -33,6 +30,36 @@ public class PopulatingNextRightPointersTest {
         root.right.left = new BinaryTreeNode<>(4);
         root.right.right = new BinaryTreeNode<>(6);
         populateNextPointer(root);
+        assert (root.next == null);
+        assert (root.left.next == root.right);
+        assert (root.left.left.next == root.left.right);
+        assert (root.left.right.next == root.right.left);
+        assert (root.right.left.next == root.right.right);
+    }
+
+    @Test
+    public void testPopulateNextRightPointer1() throws Exception {
+        //     3
+        //  2     5
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(3);
+        root.left = new BinaryTreeNode<>(2);
+        root.right = new BinaryTreeNode<>(5);
+        populateNextPointer1(root);
+        assert (root.next == null);
+        assert (root.left.next == root.right);
+        assert (root.right.next == null);
+
+        //     3
+        //  2     5
+        // 1 7   4 6
+        root = new BinaryTreeNode<>(3);
+        root.left = new BinaryTreeNode<>(2);
+        root.left.right = new BinaryTreeNode<>(7);
+        root.left.left = new BinaryTreeNode<>(1);
+        root.right = new BinaryTreeNode<>(5);
+        root.right.left = new BinaryTreeNode<>(4);
+        root.right.right = new BinaryTreeNode<>(6);
+        populateNextPointer1(root);
         assert (root.next == null);
         assert (root.left.next == root.right);
         assert (root.left.left.next == root.left.right);
