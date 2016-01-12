4bb85cb79c726a750f6fde6f1e2ea8eecd90bc16
Pankaj More <pankajmore@gmail.com>
2016-01-12 21:54:51 +0530

    Build the powerSet in lexicographic order

--- EPI/test/puzzles/PowerSet.java
+++ EPI/test/puzzles/PowerSet.java
@@ -9,22 +9,17 @@ import java.util.List;
 public class PowerSet {
     public static List<List<Integer>> generatePowerSet(List<Integer> A) {
         List<List<Integer>> powerSet = new ArrayList<>();
-        if (A.size() == 1) {
+        if (A.size() == 0) {
             powerSet.add(new ArrayList<>());
-            List<Integer> singleton = new ArrayList<>();
-            singleton.add(A.get(0));
-            powerSet.add(singleton);
             return powerSet;
         }
-        for (List<Integer> set : generatePowerSet(A.subList(0, A.size() - 1))) {
-            List<Integer> s1 = new ArrayList<>(), s2 = new ArrayList<>();
-            for (Integer x : set) {
-                s1.add(x);
-                s2.add(x);
-            }
-            s2.add(A.get(A.size() - 1));
-            powerSet.add(s1);
-            powerSet.add(s2);
+        List<List<Integer>> prevSubsets = generatePowerSet(A.subList(0, A.size() - 1));
+        powerSet.addAll(prevSubsets);
+        for (List<Integer> subset : prevSubsets) {
+            List<Integer> newSubset = new ArrayList<>();
+            newSubset.addAll(subset);
+            newSubset.add(A.get(A.size() - 1));
+            powerSet.add(newSubset);
         }
         return powerSet;
     }
