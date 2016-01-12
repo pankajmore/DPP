605401ae487936b147cbb10620d46010c025273d
Pankaj More <pankajmore@gmail.com>
2016-01-12 22:37:04 +0530

    Print the powerSet using O(N) space

--- EPI/test/puzzles/PowerSet.java
+++ EPI/test/puzzles/PowerSet.java
@@ -5,22 +5,56 @@ import java.util.List;
 
 /**
  * Created by Pankaj on 1/11/16.
+ * Various ways to compute the powerSet of a set
  */
 public class PowerSet {
+    /**
+     * Time : O(N*2^N)
+     * Space : O(N*2^N)
+     *
+     * @param A a list of size N
+     * @return all the subsets of A
+     */
     public static List<List<Integer>> generatePowerSet(List<Integer> A) {
+        return generatePowerSetHelper(A, A.size() - 1);
+    }
+
+    private static List<List<Integer>> generatePowerSetHelper(List<Integer> A, int index) {
         List<List<Integer>> powerSet = new ArrayList<>();
-        if (A.size() == 0) {
+        if (index == -1) {
             powerSet.add(new ArrayList<>());
             return powerSet;
         }
-        List<List<Integer>> prevSubsets = generatePowerSet(A.subList(0, A.size() - 1));
+        List<List<Integer>> prevSubsets = generatePowerSetHelper(A, index - 1);
         powerSet.addAll(prevSubsets);
         for (List<Integer> subset : prevSubsets) {
             List<Integer> newSubset = new ArrayList<>();
             newSubset.addAll(subset);
-            newSubset.add(A.get(A.size() - 1));
+            newSubset.add(A.get(index));
             powerSet.add(newSubset);
         }
         return powerSet;
+    }
+
+    /**
+     * Prints all the subsets of A
+     * Time : O(N*2^N)
+     * Space : O(N)
+     *
+     * @param A a list of size N
+     */
+    public static void generatePowerSet1(List<Integer> A) {
+        generatePowerSetHelper(A, 0, new ArrayList<>());
+    }
+
+    private static void generatePowerSetHelper(List<Integer> A, int index, List<Integer> subset) {
+        if (index == A.size()) {
+            System.out.println(subset);
+            return;
+        }
+        generatePowerSetHelper(A, index + 1, subset);
+        subset.add(A.get(index));
+        generatePowerSetHelper(A, index + 1, subset);
+        subset.remove(subset.size() - 1);
     }
 }
---
+++ EPI/test/puzzles/PowerSetTest.java
@@ -0,0 +1,35 @@
+package puzzles;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static puzzles.PowerSet.generatePowerSet;
+import static puzzles.PowerSet.generatePowerSet1;
+
+/**
+ * Created by pankaj on 1/12/16.
+ */
+public class PowerSetTest {
+
+    @Test
+    public void testGeneratePowerSet() throws Exception {
+        assert Arrays.asList(
+                Collections.emptyList(),
+                Collections.singletonList(1),
+                Collections.singletonList(2),
+                Arrays.asList(1, 2),
+                Collections.singletonList(3),
+                Arrays.asList(1, 3),
+                Arrays.asList(2, 3),
+                Arrays.asList(1, 2, 3)
+
+        ).equals(generatePowerSet(Arrays.asList(1, 2, 3)));
+    }
+
+    @Test
+    public void testGeneratePowerSet1() throws Exception {
+        generatePowerSet1(Arrays.asList(1, 2, 3));
+    }
+}
