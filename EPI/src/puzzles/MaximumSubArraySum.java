08fea7f51c1d7e00d13e3e2ed96708d559782c0a
Pankaj More <pankajmore@gmail.com>
2016-01-17 23:51:23 +0530

    FInd the maximum sub-array sum in a circular list

--- EPI/src/puzzles/MaximumSubArraySum.java
+++ EPI/src/puzzles/MaximumSubArraySum.java
@@ -31,4 +31,62 @@ public class MaximumSubArraySum {
         }
         return bestSubArray;
     }
+
+    /**
+     * Time : O(N)
+     * Space : O(1)
+     *
+     * @param A a list of N integers
+     * @return the maximum sub array sum
+     */
+    public static int findMaximumSubArraySum(List<Integer> A) {
+        int best_sum = 0, sum = 0, min_sum = 0;
+        for (Integer x : A) {
+            sum += x;
+            if (sum < min_sum) min_sum = sum;
+            best_sum = Math.max(best_sum, sum - min_sum);
+        }
+        return best_sum;
+    }
+
+    /**
+     * Time : O(N)
+     * Space : O(N)
+     *
+     * @param A a *circular* list of N integers
+     * @return the maximum sub-array sum, the sub-array may wrap around
+     */
+    public static int findMaximumSumSubArrayCircular(List<Integer> A) {
+        return Math.max(findMaximumSubArraySum(A), findCircularMaxSubArraySum(A));
+    }
+
+    /**
+     * Time : O(N)
+     * Space : O(1)
+     *
+     * @param A a *circular* list of N integers
+     * @return the maximum sub-array sum that *must* wrap around
+     */
+    public static int findCircularMaxSubArraySum(List<Integer> A) {
+        if (A.size() == 0) return 0;
+        int N = A.size();
+        int[] maxBegin = new int[N], maxEnd = new int[N];
+        maxBegin[0] = A.get(0);
+        int sum = A.get(0);
+        for (int i = 1; i < N; i++) {
+            sum += A.get(i);
+            maxBegin[i] = Math.max(maxBegin[i - 1], sum);
+        }
+        maxEnd[N - 1] = 0;
+        sum = 0;
+        for (int i = N - 2; i >= 0; i--) {
+            sum += A.get(i + 1);
+            maxEnd[i] = Math.max(maxEnd[i + 1], sum);
+        }
+        int circularSum = 0;
+        for (int i = 0; i < N; i++) {
+            circularSum = Math.max(circularSum, maxBegin[i] + maxEnd[i]);
+        }
+        return circularSum;
+    }
 }
