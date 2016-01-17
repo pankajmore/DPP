68abbb568dd0f489e7d4155436d792a87bccd5bd
Pankaj More <pankajmore@gmail.com>
2016-01-17 23:51:55 +0530

    Test for max sub-array sum in a circular list

--- EPI/test/puzzles/MaximumSubArraySumTest.java
+++ EPI/test/puzzles/MaximumSubArraySumTest.java
@@ -4,8 +4,10 @@ import org.junit.Test;
 import puzzles.SubseqCover.SubArray;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import static puzzles.MaximumSubArraySum.findMaximumSumSubArray;
+import static puzzles.MaximumSubArraySum.findMaximumSumSubArrayCircular;
 
 /**
  * Created by pankaj on 1/16/16.
@@ -15,10 +17,19 @@ public class MaximumSubArraySumTest {
     @Test
     public void testFindMaximumSumSubArray() throws Exception {
 
-        assert findMaximumSumSubArray(Arrays.asList()).equals(new SubArray(0, 0));
+        assert findMaximumSumSubArray(Collections.emptyList()).equals(new SubArray(0, 0));
         assert findMaximumSumSubArray(Arrays.asList(0, 1, 2)).equals(new SubArray(0, 3));
         assert findMaximumSumSubArray(Arrays.asList(-1, -3, -5)).equals(new SubArray(0, 0));
         assert findMaximumSumSubArray(Arrays.asList(904, 40, 523, 12, -355, -385, -124, 481, -31)).equals(new SubArray(0, 4));
         assert findMaximumSumSubArray(Arrays.asList(1, -2, 1, 3, -4, 2)).equals(new SubArray(2, 4));
+    }
+
+    @Test
+    public void testFindMaximumSumSubArrayCircular() throws Exception {
+        assert findMaximumSumSubArrayCircular(Collections.emptyList()) == 0;
+        assert findMaximumSumSubArrayCircular(Arrays.asList(0, 1, 2)) == 3;
+        assert findMaximumSumSubArrayCircular(Arrays.asList(-1, -3, -5)) == 0;
+        assert findMaximumSumSubArrayCircular(Arrays.asList(904, 40, 523, 12, -355, -385, -124, 481, -31)) == 1929;
+        assert findMaximumSumSubArrayCircular(Arrays.asList(1, -2, 1, 3, -4, 2)) == 5;
     }
 }
