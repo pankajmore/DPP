5d5af9eb907162193e9eb1848bd73accc6731de7
Pankaj More <pankajmore@gmail.com>
2016-01-24 18:45:28 +0530

    Tests for Jump Game

--- EPI/test/puzzles/ArrayEndReachableTest.java
+++ EPI/test/puzzles/ArrayEndReachableTest.java
@@ -15,5 +15,10 @@ public class ArrayEndReachableTest {
     public void testIsArrayEndReachable() throws Exception {
         assert isArrayEndReachable(Arrays.asList(3, 3, 1, 0, 2, 0, 1));
         assert !isArrayEndReachable(Arrays.asList(3, 2, 0, 0, 2, 0, 1));
+        assert isArrayEndReachable(Arrays.asList(2, 3, 1, 1, 4));
+        assert !isArrayEndReachable(Arrays.asList(3, 2, 1, 0, 4));
+        assert !isArrayEndReachable(Arrays.asList(3, 2, 1, -10, 4));
+        assert isArrayEndReachable(Arrays.asList(2, 3, -1, -1, 4));
+        assert !isArrayEndReachable(Arrays.asList(2, 2, -1, -1, 100));
     }
 }
