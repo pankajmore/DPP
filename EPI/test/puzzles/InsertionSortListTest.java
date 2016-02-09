dcd1f6dd3c7f1daf1ee56d46e5155a011437d737
Pankaj More <pankajmore@gmail.com>
2016-02-09 21:39:27 +0530

    8.18 Test for insertion sort on list

---
+++ EPI/test/puzzles/InsertionSortListTest.java
@@ -0,0 +1,38 @@
+package puzzles;
+
+import com.pholser.junit.quickcheck.ForAll;
+import org.junit.Test;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
+import utils.Node;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static puzzles.InsertionSortList.insertionSortList;
+
+/**
+ * Created by pankaj on 2/9/16.
+ */
+@RunWith(Theories.class)
+public class InsertionSortListTest {
+    @Theory
+    public void compareWithJDKSort(@ForAll List<Integer> l) throws Exception {
+        if (l.isEmpty()) return;
+        int[] A = new int[l.size()];
+        for (int i = 0; i < l.size(); i++) A[i] = l.get(i);
+        Node head = new Node(A);
+        Node sortedHead = insertionSortList(head);
+        Collections.sort(l);
+        assert sortedHead.toList().equals(l);
+    }
+
+    @Test
+    public void testInsertionSortList() throws Exception {
+        Node head = new Node(new int[]{4, 3, 2, 2, 5});
+        Node sortedHead = insertionSortList(head);
+        assert sortedHead.toList().equals(Arrays.asList(2, 2, 3, 4, 5));
+    }
+}
