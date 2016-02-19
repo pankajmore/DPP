3df01817fd7a1124ed64b6bb91ee97762f30d042
Pankaj More <pankajmore@gmail.com>
2016-02-19 23:48:17 +0530

    17.6.1 Test for Word Search

--- EPI/test/puzzles/StringInMatrixTest.java
+++ EPI/test/puzzles/StringInMatrixTest.java
@@ -5,6 +5,8 @@ import utils.Pair;
 
 import java.util.Arrays;
 
+import static puzzles.StringInMatrix.exist;
+
 public class StringInMatrixTest {
 
     @Test
@@ -17,5 +19,17 @@ public class StringInMatrixTest {
         int[] S = {1, 3, 4, 6};
         assert StringInMatrix.match(A, S).equals(
                 Arrays.asList(new Pair<>(0, 0), new Pair<>(1, 0), new Pair<>(1, 1), new Pair<>(2, 1)));
+    }
+
+    @Test
+    public void testExist() throws Exception {
+        char[][] board = {
+                {'A', 'B', 'C', 'E'},
+                {'S', 'F', 'C', 'S'},
+                {'A', 'D', 'E', 'E'},
+        };
+        assert exist(board, "ABCCED");
+        assert exist(board, "SEE");
+        assert !exist(board, "ABCB");
     }
 }
