adcfa9d3be980b032e16fd20aad27fb7bb0b0e85
Pankaj More <pankajmore@gmail.com>
2016-02-23 23:26:02 +0530

    10.15 Compute the exterior of a binary tree

---
+++ EPI/src/puzzles/ExteriorBinaryTree.java
@@ -0,0 +1,41 @@
+package puzzles;
+
+import utils.BinaryTreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by pankaj on 2/22/16.
+ */
+public class ExteriorBinaryTree {
+    public static List<BinaryTreeNode> exterior(BinaryTreeNode root) {
+        List<BinaryTreeNode> list = new ArrayList<>();
+        list.add(root);
+        leftExterior(root.left, list);
+        bottomExterior(root, list);
+        rightExterior(root.right, list);
+        return list;
+    }
+
+    private static void leftExterior(BinaryTreeNode root, List<BinaryTreeNode> list) {
+        if(root == null) return;
+        if(root.left == null && root.right == null) return;
+        list.add(root);
+        leftExterior(root.left, list);
+    }
+
+    private static void bottomExterior(BinaryTreeNode root, List<BinaryTreeNode> list) {
+        if (root == null) return;
+        if (root.left == null && root.right == null) list.add(root);
+        bottomExterior(root.left, list);
+        bottomExterior(root.right, list);
+    }
+
+    private static void rightExterior(BinaryTreeNode root, List<BinaryTreeNode> list) {
+        if(root == null) return;
+        if(root.left == null && root.right == null) return;
+        rightExterior(root.right, list);
+        list.add(root);
+    }
+}
