7098557d51023604bb58e742c13252bdf9a67805
Pankaj More <pankajmore@gmail.com>
2016-02-23 23:26:34 +0530

    Test for exterior of a binary tree

---
+++ EPI/test/puzzles/ExteriorBinaryTreeTest.java
@@ -0,0 +1,24 @@
+package puzzles;
+
+import examples.BinaryTreeExample;
+import org.junit.Test;
+import utils.BinaryTreeNode;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static puzzles.ExteriorBinaryTree.exterior;
+
+/**
+ * Created by pankaj on 2/22/16.
+ */
+public class ExteriorBinaryTreeTest {
+
+    @Test
+    public void testExterior() throws Exception {
+        List<Integer> expected = Arrays.asList(314, 6, 271, 28, 0, 17, 641, 257, 28, 271, 6);
+        List<Integer> actual = exterior(BinaryTreeExample.constructBookExample()).stream().map(BinaryTreeNode::val).collect(Collectors.toList());
+        assert expected.equals(actual);
+    }
+}
