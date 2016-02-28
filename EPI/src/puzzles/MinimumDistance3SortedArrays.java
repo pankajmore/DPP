f3f5b418cbb0661028c155905d1e40cd31bf8dfe
Pankaj More <pankajmore@gmail.com>
2016-02-28 22:46:25 +0530

    15.7 Compute the closest entries in k sorted arrays

--- EPI/src/puzzles/MinimumDistance3SortedArrays.java
+++ EPI/src/puzzles/MinimumDistance3SortedArrays.java
@@ -1,6 +1,6 @@
 package puzzles;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by pankaj on 2/25/16.
@@ -15,7 +15,7 @@ public class MinimumDistance3SortedArrays {
      * @param A list of size N_A
      * @param B list of size N_B
      * @param C list of size N_C
-     * @return
+     * @return the minDistance as defined above
      */
     public static int minimumDistance(List<Integer> A, List<Integer> B, List<Integer> C) {
         int i = 0, j = 0, k = 0;
@@ -27,6 +27,37 @@ public class MinimumDistance3SortedArrays {
             if (i < A.size() && min == A.get(i)) i++;
             else if (j < B.size() && min == B.get(j)) j++;
             else k++;
+        }
+        return minDistance;
+    }
+
+    /**
+     * Time : O(NlogK)
+     * Space: O(k)
+     *
+     * @param A list of K lists of integers. total number of elements = N
+     * @return the minimumDistance between any k elements, one from each list
+     */
+    public static int minimumDistance(List<List<Integer>> A) {
+        int minDistance = Integer.MAX_VALUE;
+        SortedMap<Integer, Queue<Iterator<Integer>>> map = new TreeMap<>();
+        for (int i = 0; i < A.size(); i++) {
+            Iterator<Integer> it = A.get(i).iterator();
+            if (!it.hasNext()) return minDistance;
+            Integer k = it.next();
+            if (!map.containsKey(k)) map.put(k, new LinkedList<>());
+            map.get(k).add(it);
+        }
+        while (!map.isEmpty()) {
+            minDistance = Math.min(minDistance, map.lastKey() - map.firstKey());
+            Integer min = map.firstKey();
+            Iterator<Integer> it = map.get(min).remove();
+            if (map.get(min).isEmpty()) map.remove(min);
+            if (!it.hasNext()) return minDistance;
+            Integer k = it.next();
+            if (!map.containsKey(k)) map.put(k, new LinkedList<>());
+            map.get(k).add(it);
+
         }
         return minDistance;
     }
