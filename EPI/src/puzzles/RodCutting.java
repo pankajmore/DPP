b0929cff5c06bfff14c0372d581182052c0ec5dc
Pankaj More <pankajmore@gmail.com>
2016-03-01 23:50:23 +0530

    Rod cutting problem

---
+++ EPI/src/puzzles/RodCutting.java
@@ -0,0 +1,19 @@
+package puzzles;
+
+/**
+ * Created by pankaj on 2/26/16.
+ */
+public class RodCutting {
+    public static int maxRodCut(int[] prices) {
+        int N = prices.length;
+        int[] dp = new int[N + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= N; i++) {
+            dp[i] = prices[i - 1];
+            for (int j = 0; j < i; j++) {
+                dp[i] = Math.max(dp[i], dp[j] + prices[i - j - 1]);
+            }
+        }
+        return dp[N];
+    }
+}
---
+++ EPI/test/puzzles/RodCuttingTest.java
@@ -0,0 +1,16 @@
+package puzzles;
+
+import org.junit.Test;
+
+import static puzzles.RodCutting.maxRodCut;
+
+/**
+ * Created by pankaj on 2/26/16.
+ */
+public class RodCuttingTest {
+
+    @Test
+    public void testMaxRodCut() throws Exception {
+        assert maxRodCut(new int[]{1, 5, 8, 9, 10, 17, 17, 20}) == 22;
+    }
+}
