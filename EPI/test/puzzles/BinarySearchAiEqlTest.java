74a066e1618396fd55d28845b903d6ecc2a675de
Pankaj More <pankajmore@gmail.com>
2016-03-06 19:28:48 +0530

    12.3 Search a sorted array for A[i] = i

---
+++ EPI/test/puzzles/BinarySearchAiEqlTest.java
@@ -0,0 +1,19 @@
+package puzzles;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+import static puzzles.BinarySearchAiEql.searchEntryEqualToItsIndex;
+
+/**
+ * Created by pankaj on 3/6/16.
+ */
+public class BinarySearchAiEqlTest {
+
+    @Test
+    public void testSearchEntryEqualToItsIndex() throws Exception {
+        assert searchEntryEqualToItsIndex(Arrays.asList(-2, 0, 2, 3, 6, 7, 9)) == 3;
+    }
+}
