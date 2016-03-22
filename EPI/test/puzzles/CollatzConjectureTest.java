8a44a4055955bd3a7c7cb5ea22de313912c9a22f
Pankaj More <pankajmore@gmail.com>
2016-03-22 10:50:22 +0530

    13.16 Test the Collatz Conjecture

---
+++ EPI/test/puzzles/CollatzConjectureTest.java
@@ -0,0 +1,16 @@
+package puzzles;
+
+import org.junit.Test;
+
+import static puzzles.CollatzConjecture.testCollatzConjecture;
+
+/**
+ * Created by pankaj on 3/22/16.
+ */
+public class CollatzConjectureTest {
+
+    @Test
+    public void testTestCollatzConjecture() throws Exception {
+        assert testCollatzConjecture(1 << 20);
+    }
+}
