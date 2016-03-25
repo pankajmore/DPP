7d5b4a29d86328fd1fc428f83c13b815cda851b6
Pankaj More <pankajmore@gmail.com>
2016-03-25 13:31:13 +0530

    8.15 Test whether a singly linked list is palindromic (constant space)

--- EPI/src/puzzles/PalindromeLinkedList.java
+++ EPI/src/puzzles/PalindromeLinkedList.java
@@ -1,5 +1,6 @@
 package puzzles;
 
+import org.jetbrains.annotations.NotNull;
 import utils.Node;
 
 /**
@@ -10,10 +11,61 @@ public class PalindromeLinkedList {
      * Time : O(N)
      * Space: O(N)
      *
-     * @param head linked list of size N
+     * @param head singly linked list of size N
      * @return true if list is a palindrome
      */
     public static boolean isPalindrome(Node head) {
         return head.toList().equals(head.reverse().toList());
     }
+
+    /**
+     * Time : O(N)
+     * Space: O(1)
+     * <p>
+     * Mutates the list intermittently
+     *
+     * @param head singly linked list of size N
+     * @return true if list is a palindrome
+     */
+    public static boolean isPalindrome1(@NotNull Node head) {
+        if (head.next == null) return true;
+        Node x = head, mid = reverse(breakAtMid(head)), y = mid;
+        boolean res = true;
+        while (x != null && y != null) {
+            if (x.val != y.val) {
+                res = false;
+                break;
+            }
+            x = x.next;
+            y = y.next;
+        }
+        mid = reverse(mid);
+        Node curr = head;
+        while (curr.next != null) curr = curr.next;
+        curr.next = mid;
+        return res;
+    }
+
+    private static Node reverse(Node head) {
+        Node prev = null, curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    private static Node breakAtMid(Node head) {
+        Node slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        Node res = slow.next;
+        slow.next = null;
+        return res;
+    }
+
 }
--- EPI/test/puzzles/PalindromeLinkedListTest.java
+++ EPI/test/puzzles/PalindromeLinkedListTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import utils.Node;
 
 import static puzzles.PalindromeLinkedList.isPalindrome;
+import static puzzles.PalindromeLinkedList.isPalindrome1;
 
 /**
  * Created by pankaj on 3/24/16.
@@ -15,7 +16,18 @@ public class PalindromeLinkedListTest {
         assert isPalindrome(new Node(new int[]{1}));
         assert isPalindrome(new Node(new int[]{1, 1}));
         assert isPalindrome(new Node(new int[]{1, 2, 1}));
+        assert !isPalindrome(new Node(new int[]{1, 2, 3}));
         assert isPalindrome(new Node(new int[]{1, 3, 3, 1}));
         assert isPalindrome(new Node(new int[]{1, 4, 6, 4, 1}));
+    }
+
+    @Test
+    public void testIsPalindrome1() throws Exception {
+        assert isPalindrome1(new Node(new int[]{1}));
+        assert isPalindrome1(new Node(new int[]{1, 1}));
+        assert isPalindrome1(new Node(new int[]{1, 2, 1}));
+        assert !isPalindrome1(new Node(new int[]{1, 2, 3}));
+        assert isPalindrome1(new Node(new int[]{1, 3, 3, 1}));
+        assert isPalindrome1(new Node(new int[]{1, 4, 6, 4, 1}));
     }
 }
