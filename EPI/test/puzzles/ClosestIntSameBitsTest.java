b17864f83c33351a249c58e55af3386de1daf0b6
Pankaj More <pankajmore@gmail.com>
2016-03-26 16:53:00 +0530

    5.4 Find a closest integer with the same weight

---
+++ EPI/test/puzzles/ClosestIntSameBitsTest.java
@@ -0,0 +1,35 @@
+package puzzles;
+
+import com.pholser.junit.quickcheck.ForAll;
+import com.pholser.junit.quickcheck.generator.InRange;
+import org.junit.Test;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
+
+import static puzzles.ClosestIntSameBits.closestIntSameBits;
+import static puzzles.ClosestIntSameBits.closestIntSameBits1;
+import static puzzles.ClosestIntSameBits.closestIntSameBitsBruteForce;
+
+/**
+ * Created by pankaj on 3/26/16.
+ */
+@RunWith(Theories.class)
+public class ClosestIntSameBitsTest {
+
+    @Test
+    public void testClosestIntSameBits() throws Exception {
+        assert closestIntSameBits(1) == 2;
+        assert closestIntSameBits(2) == 1;
+        assert closestIntSameBits(3) == 5;
+        assert closestIntSameBits(4) == 2;
+        assert closestIntSameBits(5) == 6;
+        assert closestIntSameBits(6) == 5;
+    }
+
+    @Theory
+    public void testCheckWithBruteForce(@ForAll @InRange(minInt = 1, maxInt = Integer.MAX_VALUE - 1) int x) throws Exception {
+        assert closestIntSameBits1(x) == closestIntSameBitsBruteForce(x);
+        assert closestIntSameBits(x) == closestIntSameBitsBruteForce(x);
+    }
+}
