0ff1dcf398e126b7cbcb71a8c9cc82704c3a224c
Pankaj More <pankajmore@gmail.com>
2016-03-29 10:34:30 +0530

    Build a Cartesian Tree from a list

---
+++ EPI/test/utils/CartesianTreeTest.java
@@ -0,0 +1,21 @@
+package utils;
+
+import com.pholser.junit.quickcheck.ForAll;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+/**
+ * Created by pankaj on 3/29/16.
+ */
+@RunWith(Theories.class)
+public class CartesianTreeTest {
+
+    @Theory
+    public void testSequence(@ForAll List<Integer> l) throws Exception {
+        List<Integer> expected = new CartesianTree(l).sequence();
+        assert expected.equals(l);
+    }
+}
