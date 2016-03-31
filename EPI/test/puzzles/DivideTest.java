8450f8e76e1f5dd43bf5bfe7b5db6a96a35a2a62
Pankaj More <pankajmore@gmail.com>
2016-03-31 20:56:36 +0530

    5.6 Compute x / y

---
+++ EPI/test/puzzles/DivideTest.java
@@ -0,0 +1,33 @@
+package puzzles;
+
+import com.pholser.junit.quickcheck.ForAll;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by pankaj on 3/31/16.
+ */
+@RunWith(Theories.class)
+public class DivideTest {
+
+    @Theory
+    public void divide(@ForAll int dividend, @ForAll int divisor) throws Exception {
+        try {
+            int q = Divide.divide(dividend, divisor);
+            assert q == dividend / divisor;
+        } catch (ArithmeticException e) {
+            assert divisor == 0;
+        }
+    }
+
+    @Theory
+    public void divide1(@ForAll int dividend, @ForAll int divisor) throws Exception {
+        try {
+            int q = Divide.divide1(dividend, divisor);
+            assert q == dividend / divisor;
+        } catch (ArithmeticException e) {
+            assert divisor == 0;
+        }
+    }
+}
