33c1dfafeac8ebe2f0c544dc1a12e7d72eccfe95
Pankaj More <pankajmore@gmail.com>
2016-04-03 12:34:08 +0530

    6.4 Check if a board game is winnable (greedy approach)

--- EPI/src/puzzles/ArrayEndReachable.java
+++ EPI/src/puzzles/ArrayEndReachable.java
@@ -21,4 +21,19 @@ public class ArrayEndReachable {
         }
         return lastReachableIndex == 0;
     }
+
+    /**
+     * Time : O(N)
+     * Space: O(1)
+     *
+     * @param A A_i indicates how far we can jump from i
+     * @return true if we can reach the last index from start
+     */
+    public static boolean isArrayEndReachableGreedy(List<Integer> A) {
+        int N = A.size(), furthest = 0;
+        for (int i = 0; i <= furthest && furthest < N; i++) {
+            furthest = Math.max(furthest, i + A.get(i));
+        }
+        return furthest >= N - 1;
+    }
 }
--- EPI/test/puzzles/ArrayEndReachableTest.java
+++ EPI/test/puzzles/ArrayEndReachableTest.java
@@ -5,12 +5,12 @@ import org.junit.Test;
 import java.util.Arrays;
 
 import static puzzles.ArrayEndReachable.isArrayEndReachable;
+import static puzzles.ArrayEndReachable.isArrayEndReachableGreedy;
 
 /**
  * Created by pankaj on 1/23/16.
  */
 public class ArrayEndReachableTest {
-
     @Test
     public void testIsArrayEndReachable() throws Exception {
         assert isArrayEndReachable(Arrays.asList(3, 3, 1, 0, 2, 0, 1));
@@ -20,5 +20,16 @@ public class ArrayEndReachableTest {
         assert !isArrayEndReachable(Arrays.asList(3, 2, 1, -10, 4));
         assert isArrayEndReachable(Arrays.asList(2, 3, -1, -1, 4));
         assert !isArrayEndReachable(Arrays.asList(2, 2, -1, -1, 100));
+    }
+
+    @Test
+    public void testIsArrayEndReachableGreedy() throws Exception {
+        assert isArrayEndReachableGreedy(Arrays.asList(3, 3, 1, 0, 2, 0, 1));
+        assert !isArrayEndReachableGreedy(Arrays.asList(3, 2, 0, 0, 2, 0, 1));
+        assert isArrayEndReachableGreedy(Arrays.asList(2, 3, 1, 1, 4));
+        assert !isArrayEndReachableGreedy(Arrays.asList(3, 2, 1, 0, 4));
+        assert !isArrayEndReachableGreedy(Arrays.asList(3, 2, 1, -10, 4));
+        assert isArrayEndReachableGreedy(Arrays.asList(2, 3, -1, -1, 4));
+        assert !isArrayEndReachableGreedy(Arrays.asList(2, 2, -1, -1, 100));
     }
 }
