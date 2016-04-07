ebf6d75ef2604b20821bceadf0fea556b1f51580
Pankaj More <pankajmore@gmail.com>
2016-04-07 20:20:14 +0530

    6.9 Generalizations of max difference (1)

---
+++ EPI/src/puzzles/BuyAndSellStockTwice.java
@@ -0,0 +1,54 @@
+package puzzles;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+/**
+ * Created by pankaj on 4/7/16.
+ */
+public class BuyAndSellStockTwice {
+    public static double buyAndSellStockTwice(@NotNull List<Double> prices) {
+        int N = prices.size();
+        double maxProfit = 0, minSoFar = Double.MAX_VALUE;
+        double[] L = new double[N], R = new double[N];
+        for (int i = 0; i < N; i++) {
+            maxProfit = Math.max(maxProfit, prices.get(i) - minSoFar);
+            minSoFar = Math.min(minSoFar, prices.get(i));
+            L[i] = maxProfit;
+        }
+        double maxSoFar = Double.MIN_VALUE;
+        maxProfit = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            maxProfit = Math.max(maxProfit, maxSoFar - prices.get(i));
+            maxSoFar = Math.max(maxSoFar, prices.get(i));
+            R[i] = maxProfit;
+        }
+        maxProfit = 0;
+        for (int i = 0; i < N; i++) {
+            maxProfit = Math.max(maxProfit, L[i] + R[i]);
+        }
+        return maxProfit;
+    }
+
+    public static double buyAndSellStockTwice1(List<Double> prices) {
+        int N = prices.size();
+        double maxProfit = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                maxProfit = Math.max(maxProfit, prices.get(j) - prices.get(i));
+            }
+        }
+        for (int a = 0; a < N; a++) {
+            for (int b = a + 1; b < N; b++) {
+                double temp = prices.get(b) - prices.get(a);
+                for (int c = b + 1; c < N; c++) {
+                    for (int d = c + 1; d < N; d++) {
+                        maxProfit = Math.max(maxProfit, prices.get(d) - prices.get(c) + temp);
+                    }
+                }
+            }
+        }
+        return maxProfit;
+    }
+}
---
+++ EPI/test/puzzles/BuyAndSellStockTwiceTest.java
@@ -0,0 +1,24 @@
+package puzzles;
+
+import com.pholser.junit.quickcheck.ForAll;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static puzzles.BuyAndSellStockTwice.buyAndSellStockTwice;
+import static puzzles.BuyAndSellStockTwice.buyAndSellStockTwice1;
+
+/**
+ * Created by pankaj on 4/7/16.
+ */
+@RunWith(Theories.class)
+public class BuyAndSellStockTwiceTest {
+    @Theory
+    public void compareWithBruteForce(@ForAll List<Double> prices) {
+        final double expected = buyAndSellStockTwice1(prices);
+        final double actual = buyAndSellStockTwice(prices);
+        assert actual == expected;
+    }
+}
