4a877ebc892e0605b3dd5d1e2c2b78bc1659e413
Pankaj More <pankajmore@gmail.com>
2016-04-13 10:45:37 +0530

    6.10.2 Compute the max of all possible triplets

---
+++ EPI/src/puzzles/MaxProductTriplets.java
@@ -0,0 +1,50 @@
+package puzzles;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by pankaj on 4/13/16.
+ */
+public class MaxProductTriplets {
+    /**
+     * Time : O(N)
+     * Space: O(1)
+     *
+     * @param A list of N integers
+     * @return return the max product of all possible triplets, INT_MIN if N < 3
+     */
+    public static int maxProductTriplets(List<Integer> A) {
+        int N = A.size();
+        if (N < 3) return Integer.MIN_VALUE;
+        int[] max = new int[3], min = new int[2];
+        Arrays.fill(max, Integer.MIN_VALUE);
+        Arrays.fill(min, Integer.MAX_VALUE);
+        for (int x : A) {
+            if (x > max[0]) max[0] = x;
+            Arrays.sort(max);
+            if (x < min[1]) min[1] = x;
+            Arrays.sort(min);
+        }
+        return Math.max(max[0] * max[1] * max[2], max[2] * min[0] * min[1]);
+    }
+
+    /**
+     * Time : O(N^3)
+     * Space: O(1)
+     *
+     * @param A list of N integers
+     * @return return the max product of all possible triplets, INT_MIN if N < 3
+     */
+    public static int maxProductTriplets1(List<Integer> A) {
+        int maxProduct = Integer.MIN_VALUE, N = A.size();
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    maxProduct = Math.max(maxProduct, A.get(i) * A.get(j) * A.get(k));
+                }
+            }
+        }
+        return maxProduct;
+    }
+}
---
+++ EPI/test/puzzles/MaxProductTripletsTest.java
@@ -0,0 +1,25 @@
+package puzzles;
+
+import com.pholser.junit.quickcheck.ForAll;
+import com.pholser.junit.quickcheck.generator.InRange;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static puzzles.MaxProductTriplets.maxProductTriplets;
+import static puzzles.MaxProductTriplets.maxProductTriplets1;
+
+/**
+ * Created by pankaj on 4/13/16.
+ */
+@RunWith(Theories.class)
+public class MaxProductTripletsTest {
+    @Theory
+    public void compareWithBruteForce(@ForAll @InRange(minInt = -1000, maxInt = 1000) List<Integer> A) {
+        int expected = maxProductTriplets1(A);
+        int actual = maxProductTriplets(A);
+        assert actual == expected;
+    }
+}
