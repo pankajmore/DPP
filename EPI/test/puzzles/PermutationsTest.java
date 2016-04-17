9272a9b999f95c806ad7540cda2388205ef27689
Pankaj More <pankajmore@gmail.com>
2016-04-17 21:35:56 +0530

    6.14 Tests for previous permutation and that next.prev = id

--- EPI/test/puzzles/PermutationsTest.java
+++ EPI/test/puzzles/PermutationsTest.java
@@ -1,12 +1,26 @@
 package puzzles;
 
+import com.pholser.junit.quickcheck.ForAll;
 import org.junit.Test;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static puzzles.Permutations.*;
 
+@RunWith(Theories.class)
 public class PermutationsTest {
+
+    @Theory
+    public void compareNextAndPrevPermutation(@ForAll List<Integer> A) {
+        List<Integer> prev = prevPermutation(A);
+        if (prev == null) return;
+        List<Integer> nextOfPrev = nextPermutation(prev);
+        assert nextOfPrev.equals(A);
+    }
 
     @Test
     public void testAllPermutations() throws Exception {
@@ -35,6 +49,16 @@ public class PermutationsTest {
         assert nextPermutation(Arrays.asList(2, 3, 1)).equals(Arrays.asList(3, 1, 2));
         assert nextPermutation(Arrays.asList(3, 1, 2)).equals(Arrays.asList(3, 2, 1));
         assert nextPermutation(Arrays.asList(3, 2, 1)) == null;
+    }
+
+    @Test
+    public void testPrevPermutation() throws Exception {
+        assert prevPermutation(Arrays.asList(1, 2, 3)) == null;
+        assert prevPermutation(Arrays.asList(1, 3, 2)).equals(Arrays.asList(1, 2, 3));
+        assert prevPermutation(Arrays.asList(2, 1, 3)).equals(Arrays.asList(1, 3, 2));
+        assert prevPermutation(Arrays.asList(2, 3, 1)).equals(Arrays.asList(2, 1, 3));
+        assert prevPermutation(Arrays.asList(3, 1, 2)).equals(Arrays.asList(2, 3, 1));
+        assert prevPermutation(Arrays.asList(3, 2, 1)).equals(Arrays.asList(3, 1, 2));
     }
 
     @Test
