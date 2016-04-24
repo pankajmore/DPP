eab29186f24d7cac3cb0df11f447086fc678454b
Pankaj More <pankajmore@gmail.com>
2016-04-24 22:45:56 +0530

    Swap even and odd consecutive bits

---
+++ EPI/src/puzzles/SwapEvenOddBits.java
@@ -0,0 +1,17 @@
+package puzzles;
+
+/**
+ * Created by pankaj on 4/22/16.
+ */
+public class SwapEvenOddBits {
+    public static int swapEvenOddBits(int x) {
+        return (x & 0xAAAAAAAA) >>> 1 | (x & 0x55555555) << 1;
+    }
+
+    public static int swapEvenOddBits1(int n) {
+        for (int i = 0; i < Integer.SIZE; i += 2)
+            if ((n >> i & 1) != ((n >> i + 1) & 1))
+                n ^= 0b11 << i;
+        return n;
+    }
+}
