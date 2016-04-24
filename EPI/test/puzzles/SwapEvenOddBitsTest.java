b84936c509b079af7cb1e4975cc375e56d971fa4
Pankaj More <pankajmore@gmail.com>
2016-04-24 22:46:25 +0530

    Test for swapping even and odd consecutive bits

---
+++ EPI/test/puzzles/SwapEvenOddBitsTest.java
@@ -0,0 +1,23 @@
+package puzzles;
+
+import com.pholser.junit.quickcheck.ForAll;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
+
+import static puzzles.SwapEvenOddBits.swapEvenOddBits;
+import static puzzles.SwapEvenOddBits.swapEvenOddBits1;
+
+/**
+ * Created by pankaj on 4/22/16.
+ */
+@RunWith(Theories.class)
+public class SwapEvenOddBitsTest {
+    @Theory
+    public void testSwapEvenOddBits(@ForAll int n) throws Exception {
+        assert swapEvenOddBits(n) == swapEvenOddBits1(n);
+        assert swapEvenOddBits(n) == swapEvenOddBits(n);
+        assert swapEvenOddBits1(swapEvenOddBits1(n)) == n;
+    }
+
+}
