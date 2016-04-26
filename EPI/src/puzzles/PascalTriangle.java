e965c678dee1926cc9c7529744d3e88c0dd7089e
Pankaj More <pankajmore@gmail.com>
2016-04-26 11:23:08 +0530

    6.24.1 Compute the Nth row of Pascal's Triangle

--- EPI/src/puzzles/PascalTriangle.java
+++ EPI/src/puzzles/PascalTriangle.java
@@ -21,4 +21,48 @@ public class PascalTriangle {
         }
         return rows;
     }
+
+    /**
+     * Time : O(NxN)
+     * Space: O(N)
+     *
+     * @param N positive integer
+     * @return the Nth row of Pascal's Triangle
+     */
+    public static List<Integer> getNthRow(int N) {
+        List<Integer> A = new ArrayList<>();
+        if (N <= 0) return A;
+        for (int i = 0; i < N; i++) A.add(0);
+        A.set(0, 1);
+        for (int i = 1; i < N; i++) {
+            int temp = A.get(0);
+            for (int j = 1; j < i; j++) {
+                int next = A.get(j);
+                A.set(j, temp + next);
+                temp = next;
+            }
+            A.set(i, temp);
+        }
+        return A;
+    }
+
+    /**
+     * Time : O(N)
+     * Space: O(N)
+     *
+     * @param N positive integer
+     * @return the Nth row of Pascal's Triangle
+     */
+    public static List<Integer> getNthRow1(int N) {
+        List<Integer> A = new ArrayList<>();
+        if (N <= 0) return A;
+        int[] fact = new int[N];
+        fact[0] = 1;
+        for (int i = 1; i < N; i++) fact[i] = i * fact[i - 1];
+
+        for (int i = 0; i < N; i++) {
+            A.add(fact[N - 1] / (fact[i] * fact[N - 1 - i]));
+        }
+        return A;
+    }
 }
