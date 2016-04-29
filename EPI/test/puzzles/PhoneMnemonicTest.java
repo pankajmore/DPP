e996279022076bd665e91b930cd7d2213cbc871e
Pankaj More <pankajmore@gmail.com>
2016-04-29 11:35:00 +0530

    7.5 unit tests

---
+++ EPI/test/puzzles/PhoneMnemonicTest.java
@@ -0,0 +1,18 @@
+package puzzles;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static puzzles.PhoneMnemonic.phoneMnemonic;
+
+/**
+ * Created by pankaj on 4/29/16.
+ */
+public class PhoneMnemonicTest {
+    @Test
+    public void testPhoneMnemonic() throws Exception {
+        assert phoneMnemonic("2").equals(Arrays.asList("A", "B", "C"));
+        assert phoneMnemonic("23").equals(Arrays.asList("AD", "AE", "AF", "BD", "BE", "BF", "CD", "CE", "CF"));
+    }
+}
