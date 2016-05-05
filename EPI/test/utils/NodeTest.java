e10f9a50248819ab43186514032a1ca85a5c13c5
Pankaj More <pankajmore@gmail.com>
2016-05-05 19:13:45 +0530

    8.4 Tests for reverse sub-lists k at a time

--- EPI/test/utils/NodeTest.java
+++ EPI/test/utils/NodeTest.java
@@ -43,4 +43,14 @@ public class NodeTest {
         assert new Node(new int[]{1, 2, 3}).reverse(0, 1).toList().equals(Arrays.asList(2, 1, 3));
         assert new Node(new int[]{1, 2, 3}).reverse(1, 2).toList().equals(Arrays.asList(1, 3, 2));
     }
+
+    @Test
+    public void testReverseKSubList() throws Exception {
+        assert new Node(new int[]{1, 2, 3}).reverse(3).toList().equals(Arrays.asList(3, 2, 1));
+        assert new Node(new int[]{1}).reverse(3).toList().equals(Arrays.asList(1));
+        assert new Node(new int[]{1, 2}).reverse(3).toList().equals(Arrays.asList(1, 2));
+        assert new Node(new int[]{1, 2, 3, 4, 5, 6, 7, 8}).reverse(3).toList().equals(Arrays.asList(3, 2, 1, 6, 5, 4, 7, 8));
+        assert new Node(new int[]{1, 2, 3, 4, 5, 6}).reverse(3).toList().equals(Arrays.asList(3, 2, 1, 6, 5, 4));
+        assert new Node(new int[]{1, 2, 3, 4, 5}).reverse(2).toList().equals(Arrays.asList(2, 1, 4, 3, 5));
+    }
 }
