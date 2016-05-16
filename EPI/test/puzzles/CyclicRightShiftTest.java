e66fb056ceabc42b13d0545c1e3c2645ce6b6c53
Pankaj More <pankajmore@gmail.com>
2016-05-16 21:04:39 +0530

    8.11 Quick check property for testing cyclic right shift on linked lists

--- EPI/test/puzzles/CyclicRightShiftTest.java
+++ EPI/test/puzzles/CyclicRightShiftTest.java
@@ -1,15 +1,24 @@
 package puzzles;
 
+import com.pholser.junit.quickcheck.ForAll;
+import com.pholser.junit.quickcheck.generator.InRange;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
 import utils.Node;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static puzzles.CyclicRightShift.cyclicallyRightShift;
+import static puzzles.RotateArray.rotateArray;
 
 /**
  * Created by pankaj on 3/21/16.
  */
+@RunWith(Theories.class)
 public class CyclicRightShiftTest {
 
     @Test
@@ -17,5 +26,16 @@ public class CyclicRightShiftTest {
         Node original = new Node(new int[]{1, 2, 3, 4, 5});
         Node shifted = cyclicallyRightShift(original, 3);
         assert shifted.toList().equals(Arrays.asList(3, 4, 5, 1, 2));
+    }
+
+    @Theory
+    public void compareWithArrayRotate(@ForAll int[] A, @ForAll @InRange(minInt = 0) int k) throws Exception {
+        if(A.length == 0) return;
+        Node l = new Node(A);
+        int[] actual = new int[A.length];
+        List<Integer> actualList = cyclicallyRightShift(l, k).toList();
+        for(int i = 0; i < A.length; i++) actual[i] = actualList.get(i);
+        rotateArray(A,k);
+        Assert.assertArrayEquals(A, actual);
     }
 }
