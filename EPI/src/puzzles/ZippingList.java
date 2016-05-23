e40adea5c86742428a69a01312cff9aba8353df7
Pankaj More <pankajmore@gmail.com>
2016-05-23 23:51:38 +0530

    improve ZippingList merge step

--- EPI/src/puzzles/ZippingList.java
+++ EPI/src/puzzles/ZippingList.java
@@ -2,6 +2,8 @@ package puzzles;
 
 import utils.Node;
 
+import java.util.List;
+
 /**
  * Created by Pankaj on 12/28/15.
  */
@@ -11,27 +13,33 @@ public class ZippingList {
         return merge(l, reverse(breakAtMid(l)));
     }
 
+    public static Node zip1(Node l) throws Exception {
+        if (l == null || l.next == null) return l;
+        List<Integer> A = l.toList();
+        int[] B = new int[A.size()];
+        int idx = 0, i = 0, j = A.size() - 1;
+        while (i < j) {
+            B[idx++] = A.get(i);
+            B[idx++] = A.get(j);
+            i++;
+            j--;
+        }
+        if (i == j) B[idx] = A.get(i);
+        return new Node(B);
+    }
+
     private static Node merge(Node a, Node b) {
-        Node head = a, nextA, nextB, prevA = null;
-        while (a != null && b != null) {
-            nextA = a.next;
-            nextB = b.next;
-            a.next = b;
-            b.next = nextA;
-            prevA = a;
-            a = nextA;
-            b = nextB;
+        Node dummyHead = new Node(), curr = dummyHead;
+        while (b != null) {
+            curr.next = a;
+            curr = a;
+            a = a.next;
+            curr.next = b;
+            curr = b;
+            b = b.next;
         }
-        if (a != null) {
-            a = prevA; //shift a to the last node in a
-            while (a != null && a.next != null) a = a.next;
-            assert a != null;
-            a.next = b;
-        } else if (b != null) {
-            assert prevA != null;
-            prevA.next = b;
-        }
-        return head;
+        curr.next = a;
+        return dummyHead.next;
     }
 
     private static Node reverse(Node l) {
--- EPI/test/puzzles/ZippingListTest.java
+++ EPI/test/puzzles/ZippingListTest.java
@@ -1,22 +1,34 @@
 package puzzles;
 
+import com.pholser.junit.quickcheck.ForAll;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
 import utils.Node;
 
 import java.util.Arrays;
 
 import static puzzles.ZippingList.zip;
+import static puzzles.ZippingList.zip1;
 
+@RunWith(Theories.class)
 public class ZippingListTest {
 
     @Test
     public void testZip() throws Exception {
         /* odd case */
         Node l = new Node(new int[]{0, 1, 2, 3, 4});
-        assert zip(l).toList().equals(Arrays.asList(0, 4, 1, 3, 2));
+        Assert.assertEquals(zip(l).toList(), Arrays.asList(0, 4, 1, 3, 2));
         /* even case */
         l = new Node(new int[]{0, 1, 2, 3});
-        assert zip(l).toList().equals(Arrays.asList(0, 3, 1, 2));
+        Assert.assertEquals(zip(l).toList(), Arrays.asList(0, 3, 1, 2));
+    }
 
+    @Theory
+    public void compare(@ForAll int[] A) throws Exception {
+        if (A.length == 0) return;
+        Assert.assertEquals(zip(new Node(A)), zip1(new Node(A)));
     }
 }
