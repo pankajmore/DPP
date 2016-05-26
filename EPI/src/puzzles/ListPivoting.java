144b61eb962f36ee08d0bba23582991c87f0d6ae
Pankaj More <pankajmore@gmail.com>
2016-05-26 21:41:18 +0530

    8.17 Implement List Pivoting

---
+++ EPI/src/puzzles/ListPivoting.java
@@ -0,0 +1,31 @@
+package puzzles;
+
+import utils.Node;
+
+/**
+ * Created by pankaj on 5/26/16.
+ */
+public class ListPivoting {
+    public static Node listPivoting(Node l, int x) {
+        Node left = new Node(), leftFakeHead = left;
+        Node mid = new Node(), midFakeHead = mid;
+        Node right = new Node(), rightFakeHead = right;
+        while (l != null) {
+            if (l.val < x) {
+                left.next = l;
+                left = left.next;
+            } else if (l.val == x) {
+                mid.next = l;
+                mid = mid.next;
+            } else {
+                right.next = l;
+                right = right.next;
+            }
+            l = l.next;
+        }
+        right.next = null;
+        mid.next = rightFakeHead.next;
+        left.next = midFakeHead.next;
+        return leftFakeHead.next;
+    }
+}
---
+++ EPI/test/puzzles/ListPivotingTest.java
@@ -0,0 +1,25 @@
+package puzzles;
+
+import org.junit.Assert;
+import org.junit.Test;
+import utils.Node;
+
+import static puzzles.ListPivoting.listPivoting;
+
+/**
+ * Created by pankaj on 5/26/16.
+ */
+public class ListPivotingTest {
+    @Test
+    public void testListPivoting() throws Exception {
+        Assert.assertEquals(new Node(new int[]{1}), listPivoting(new Node(new int[]{1}), 3));
+        Assert.assertEquals(new Node(new int[]{3}), listPivoting(new Node(new int[]{3}), 3));
+        Assert.assertEquals(new Node(new int[]{5}), listPivoting(new Node(new int[]{5}), 3));
+        Assert.assertEquals(new Node(new int[]{1, 3}), listPivoting(new Node(new int[]{1, 3}), 3));
+        Assert.assertEquals(new Node(new int[]{3, 5}), listPivoting(new Node(new int[]{3, 5}), 3));
+        Assert.assertEquals(new Node(new int[]{1, 5}), listPivoting(new Node(new int[]{5, 1}), 3));
+        Assert.assertEquals(new Node(new int[]{1, 3, 5}), listPivoting(new Node(new int[]{5, 1, 3}), 3));
+        Assert.assertEquals(new Node(new int[]{1, 3, 3, 5}), listPivoting(new Node(new int[]{5, 3, 1, 3}), 3));
+    }
+
+}
