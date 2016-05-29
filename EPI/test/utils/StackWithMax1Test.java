5d9f0ce5f17713c5bb756f0f6d2abe3b0a2d235a
Pankaj More <pankajmore@gmail.com>
2016-05-29 22:35:43 +0530

    9.1 Test for equal entries

---
+++ EPI/test/utils/StackWithMax1Test.java
@@ -0,0 +1,32 @@
+package utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by pankaj on 5/29/16.
+ */
+public class StackWithMax1Test {
+    @Test
+    public void testMax() throws Exception {
+        StackWithMax1<Integer> s = new StackWithMax1<>();
+        s.push(new Integer(1));
+        assertEquals(new Integer(1), s.max());
+        s.push(new Integer(2));
+        assertEquals(new Integer(2), s.max());
+        s.push(new Integer(2));
+        s.push(new Integer(1));
+        s.push(new Integer(2));
+        assertEquals(new Integer(2), s.max());
+        assertEquals(2, s.maxSpace());
+        s.pop();
+        s.pop();
+        assertEquals(new Integer(2), s.max());
+        s.pop();
+        assertEquals(new Integer(2), s.max());
+        s.pop();
+        assertEquals(new Integer(1), s.max());
+    }
+
+}
