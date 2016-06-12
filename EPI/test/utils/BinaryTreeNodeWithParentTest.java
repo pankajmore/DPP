5bf66cbe791c6f6da7f5769431596ab8b181f2fa
Pankaj More <pankajmore@gmail.com>
2016-06-12 22:19:18 +0530

    Use JUnit Assert

--- EPI/test/utils/BinaryTreeNodeWithParentTest.java
+++ EPI/test/utils/BinaryTreeNodeWithParentTest.java
@@ -4,16 +4,14 @@ import examples.BinaryTreeExample;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-
 //TODO: Generate and test on random trees
 public class BinaryTreeNodeWithParentTest {
     @Test
     public void testDepth() {
         BinaryTreeNodeWithParent bookExample = new BinaryTreeNodeWithParent(BinaryTreeExample.constructBookExample());
-        assert bookExample.depth() == 0;
-        assert bookExample.left().left().right().depth() == 3;
-        assert bookExample.right().left().right().left().depth() == 4;
+        Assert.assertEquals(0, bookExample.depth());
+        Assert.assertEquals(3, bookExample.left().left().right().depth());
+        Assert.assertEquals(4, bookExample.right().left().right().left().depth());
     }
 
     @Test
@@ -21,7 +19,7 @@ public class BinaryTreeNodeWithParentTest {
         BinaryTreeNodeWithParent bookExample = new BinaryTreeNodeWithParent(BinaryTreeExample.constructBookExample());
         int[] actual = bookExample.inOrder().stream().mapToInt(BinaryTreeNodeWithParent::val).toArray();
         int[] expected = BinaryTreeExample.constructBookExample().inOrder().stream().mapToInt(BinaryTreeNode::val).toArray();
-        assert Arrays.equals(actual, expected);
+        Assert.assertArrayEquals(expected, actual);
     }
 
     @Test
@@ -46,7 +44,7 @@ public class BinaryTreeNodeWithParentTest {
         BinaryTreeNodeWithParent x = root.left().left();
         BinaryTreeNodeWithParent y = root.left().right().right();
         BinaryTreeNodeWithParent l = root.left();
-        assert l.equals(root.leastCommonAncestor(x, y));
-        assert l.equals(root.leastCommonAncestorHashTable(x, y));
+        Assert.assertEquals(l, root.leastCommonAncestor(x, y));
+        Assert.assertEquals(l, root.leastCommonAncestorHashTable(x, y));
     }
 }
