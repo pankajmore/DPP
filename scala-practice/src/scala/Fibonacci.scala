759945ef7a394106eb3589eb363b7b01768f8856
Pankaj More <pankajmore@gmail.com>
2016-06-22 22:08:18 +0530

    Tail-recursive and Closed Form solutions to Fibonacci series

--- scala-practice/src/scala/Fibonacci.scala
+++ scala-practice/src/scala/Fibonacci.scala
@@ -1,5 +1,7 @@
 package scala
 
+import scala.annotation.tailrec
+
 
 /**
   * Created by pankaj on 6/20/16.
@@ -12,17 +14,49 @@ object RecursiveFibonacci {
   }
 }
 
+object TailRecursiveFibonacci {
+  def apply(n: Int): BigInt = {
+    require(n >= 0)
+    if (n < 2) return n
+    apply(n, 0, 1)
+  }
+
+  @tailrec
+  private def apply(n: Int, a: BigInt, b: BigInt): BigInt = {
+    if (n == 1) return b
+    apply(n - 1, b, a + b)
+  }
+}
+
 object IterativeFibonacci {
   def apply(n: Int): BigInt = {
     require(n >= 0)
     if (n < 2) return n
-    var a = 0
-    var b = 1
-    for (i <- 3 to n) {
+    var a: BigInt = 0
+    var b: BigInt = 1
+    for (i <- 2 to n) {
       val tmp = b
       b = a + b
       a = tmp
     }
     b
   }
+
+  object ClosedFormFibonacci1 {
+    val phi: BigDecimal = (1 + Math.sqrt(5)) / 2
+    val psi: BigDecimal = 1 - phi
+
+    def apply(n: Int): BigInt = {
+      ((phi.pow(n) - psi.pow(n)) / Math.sqrt(5)).rounded.toBigInt()
+    }
+  }
+
+  object ClosedFormFibonacci2 {
+    val phi: BigDecimal = ClosedFormFibonacci1.phi
+
+    def apply(n: Int): BigInt = {
+      (phi.pow(n) / Math.sqrt(5)).rounded.toBigInt()
+    }
+  }
+
 }
--- scala-practice/test/scala/FibonacciSpec.scala
+++ scala-practice/test/scala/FibonacciSpec.scala
@@ -1,18 +1,35 @@
 package scala
 
+import org.scalacheck.Gen
 import org.scalatest.FlatSpec
 import org.scalatest.prop.GeneratorDrivenPropertyChecks._
+
+import scala.IterativeFibonacci.{ClosedFormFibonacci1, ClosedFormFibonacci2}
 
 /**
   * Created by pankaj on 6/21/16.
   */
 class FibonacciSpec extends FlatSpec {
+  val gen = Gen.choose[Int](2, 100000)
   "Sum of F(n) + F(n + 1)" should "equal F(n + 2)" in {
-    forAll { (n: Int) =>
-      whenever(n > 1) {
-        RecursiveFibonacci(n) == RecursiveFibonacci(n - 1) + RecursiveFibonacci(n - 2)
-        IterativeFibonacci(n) == IterativeFibonacci(n - 1) + IterativeFibonacci(n - 2)
+    forAll(gen) { (n) =>
+      assertResult(TailRecursiveFibonacci(n)) {
+        TailRecursiveFibonacci(n - 1) + TailRecursiveFibonacci(n - 2)
       }
+      assertResult(IterativeFibonacci(n)) {
+        IterativeFibonacci(n - 1) + IterativeFibonacci(n - 2)
+      }
+      //      assert(ClosedFormFibonacci1(n) - ClosedFormFibonacci1(n - 1) - ClosedFormFibonacci1(n - 2) <= ???)
+      //      assert(ClosedFormFibonacci2(n) - ClosedFormFibonacci2(n - 1) - ClosedFormFibonacci2(n - 2) <= ???)
+    }
+  }
+
+  "Nth Fibonacci number" should "match for all implementation" in {
+    forAll(gen) { (n) =>
+      val expected = IterativeFibonacci(n)
+      assert(expected === TailRecursiveFibonacci(n))
+      assert(expected - ClosedFormFibonacci1(n) <= 1)
+      assert(expected - ClosedFormFibonacci2(n) <= 1)
     }
   }
 }
