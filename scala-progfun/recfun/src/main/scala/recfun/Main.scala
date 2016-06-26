fb719b3ade56b5e9a2e39d89a6227f1341578aa4
Pankaj More <pankajmore@gmail.com>
2016-06-26 22:58:00 +0530

    #scala #progfun recfun 1 pascal triangle

---
+++ scala-progfun/recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,32 @@
+package recfun
+
+import scala.annotation.tailrec
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || r == 0 || c == r) return 1
+    pascal(c, r - 1) + pascal(c - 1, r - 1)
+  }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = ???
+
+  /**x9
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
