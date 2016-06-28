bc757b4ea8d013407e5b664784b2feeec57a28ce
Pankaj More <pankajmore@gmail.com>
2016-06-28 22:20:03 +0530

    remove redundant condition

--- scala-progfun/recfun/src/main/scala/recfun/Main.scala
+++ scala-progfun/recfun/src/main/scala/recfun/Main.scala
@@ -16,7 +16,7 @@ object Main {
     * Exercise 1
     */
   def pascal(c: Int, r: Int): Int = {
-    if (c == 0 || r == 0 || c == r) 1
+    if (c == 0 || c == r) 1
     else pascal(c, r - 1) + pascal(c - 1, r - 1)
   }
 
