1c38e8561a4ba1ddd390566da1e5cdd2f719bbd1
Pankaj More <pankajmore@gmail.com>
2016-08-12 22:28:24 +0530

    Algorithmic Toolbox 1

---
+++ algorithmic_toolbox/src/APlusB.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Created by pankaj on 8/12/16.
+ */
+public class APlusB {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(a + b);
+    }
+}
