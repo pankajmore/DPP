70bb109eb65fa8c85341ead3d6bb6178e1a4f4a3
Pankaj More <pankajmore@gmail.com>
2016-09-05 13:59:25 +0530

    #leetcode #392

---
+++ leetcode/src/IsSubsequence.java
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode.com/problems/is-subsequence/
+ * Created by pankaj on 9/5/16.
+ */
+public class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() > t.length()) return false;
+        int j = 0;
+        for (int i = 0; j < s.length() && i < t.length(); i++) {
+            if (s.charAt(j) == t.charAt(i)) j++;
+        }
+        return j == s.length();
+    }
+}
