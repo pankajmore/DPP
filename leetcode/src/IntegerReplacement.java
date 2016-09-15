673e8b1607ef7d054f6c7990a864078f8e270732
Pankaj More <pankajmore@gmail.com>
2016-09-15 10:29:04 +0530

    #leetcode #397 alternative

--- leetcode/src/IntegerReplacement.java
+++ leetcode/src/IntegerReplacement.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/integer-replacement/
  * Created by pankaj on 9/14/16.
@@ -14,5 +17,23 @@ public class IntegerReplacement {
             res++;
         }
         return res;
+    }
+
+    public int integerReplacement1(int n_) {
+        long n = (long) n_;
+        Map<Long, Integer> map = new HashMap<>();
+        map.put(1L, 0);
+        return helper(n, map);
+    }
+
+    private int helper(long n, Map<Long, Integer> map) {
+        if (!map.containsKey(n)) {
+            if (n % 2 == 0) {
+                map.put(n, 1 + helper(n / 2, map));
+            } else {
+                map.put(n, 1 + Math.min(helper(n - 1, map), helper(n + 1, map)));
+            }
+        }
+        return map.get(n);
     }
 }
