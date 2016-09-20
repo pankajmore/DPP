cc0b371b27090261b5551f99d6eee5602c02b9bc
Pankaj More <pankajmore@gmail.com>
2016-09-20 20:13:39 +0530

    #jeffe #dp #q3 longest common subsequence

---
+++ jeffe/src/Sequences.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+/**
+ * http://jeffe.cs.illinois.edu/teaching/algorithms/notes/05-dynprog.pdf
+ * Page - 19, Q3
+ * Created by pankaj on 9/20/16.
+ */
+public class Sequences {
+    public static <T> int longestCommonSubsequence(List<T> A, List<T> B) {
+        int M = A.size();
+        int N = B.size();
+        int[][] dp = new int[M + 1][N + 1];
+        for (int i = 1; i <= M; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (A.get(i - 1).equals(B.get(j - 1))) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+        return dp[M][N];
+    }
+}
