0b44431bfded679f70c829651a78931de21e6e39
Pankaj More <pankajmore@gmail.com>
2016-09-23 20:06:04 +0530

    #leetcode #401

---
+++ leetcode/src/BinaryWatch.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/binary-watch/
+ * Created by pankaj on 9/23/16.
+ */
+public class BinaryWatch {
+    public List<String> readBinaryWatch(int num) {
+        List<String> l = new ArrayList<>();
+        for (int h = 0; h < 12; h++) {
+            for (int m = 0; m < 60; m++) {
+                if (Integer.bitCount(h) + Integer.bitCount(m) == num) {
+                    l.add(String.format("%d:%02d", h, m));
+                }
+            }
+        }
+        return l;
+    }
+}
