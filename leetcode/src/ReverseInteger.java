b2298c968b8f78937b7059f9938d9a655ebf6b0e
Pankaj More <pankajmore@gmail.com>
2016-09-23 23:40:46 +0530

    #leetcode #7

---
+++ leetcode/src/ReverseInteger.java
@@ -0,0 +1,15 @@
+/**
+ * Created by pankaj on 9/23/16.
+ */
+public class ReverseInteger {
+    public int reverse(int x) {
+        int r = 0;
+        while (x != 0) {
+            int temp = (r * 10) + (x % 10);
+            if (temp / 10 != r) return 0; // handle overflow
+            r = temp;
+            x /= 10;
+        }
+        return r;
+    }
+}
