b75ee3aa5ee66962dd533dd1d9a7e58b4bbbad2c
Pankaj More <pankajmore@gmail.com>
2016-09-26 23:53:17 +0530

    #leetcode #70

---
+++ leetcode/src/ClimbingStairs.java
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/climbing-stairs/
+ * Created by pankaj on 9/26/16.
+ */
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        if (n < 2) return n;
+        int a = 1, b = 2;
+        for (int i = 3; i <= n; i++) {
+            int tmp = b;
+            b = a + b;
+            a = tmp;
+        }
+        return b;
+    }
+}
