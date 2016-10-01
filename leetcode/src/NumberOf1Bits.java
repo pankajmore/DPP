15e3e164a1ed78a4c02fcc3becb4998b147056ea
Pankaj More <pankajmore@gmail.com>
2016-10-01 15:56:47 +0530

    #leetcode #191 tree method to calculate Hamming Weight

--- leetcode/src/NumberOf1Bits.java
+++ leetcode/src/NumberOf1Bits.java
@@ -12,4 +12,14 @@ public class NumberOf1Bits {
         }
         return c;
     }
+
+    // you need to treat n as an unsigned value
+    public int popCount(int n) {
+        n = (n & 0x55555555) + (n >> 1 & 0x55555555);
+        n = (n & 0x33333333) + (n >> 2 & 0x33333333);
+        n = (n & 0x0F0F0F0F) + (n >> 4 & 0x0F0F0F0F);
+        n = (n & 0x00FF00FF) + (n >> 8 & 0x00FF00FF);
+        n = (n & 0x0000FFFF) + (n >> 16 & 0x0000FFF);
+        return n;
+    }
 }
