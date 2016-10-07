a52abc6f90c62db449deba88c85243a58e93f0c1
Pankaj More <pankajmore@gmail.com>
2016-10-07 21:08:01 +0530

    #leetcode #226 invert tree using stack

--- leetcode/src/InvertTree.java
+++ leetcode/src/InvertTree.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /**
  * https://leetcode.com/problems/invert-binary-tree/
  * Created by pankaj on 10/6/16.
@@ -8,6 +10,20 @@ public class InvertTree {
         TreeNode tmp = invertTree(root.right);
         root.right = invertTree(root.left);
         root.left = tmp;
+        return root;
+    }
+
+    public TreeNode invertTreeUsingStack(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        if (root != null) stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            TreeNode left = curr.left, right = curr.right;
+            curr.left = right;
+            curr.right = left;
+            if (curr.left != null) stack.push(curr.left);
+            if (curr.right != null) stack.push(curr.right);
+        }
         return root;
     }
 
