ab8788606768aa0f1e5b83f50610036c6f0a00a9
Pankaj More <pankajmore@gmail.com>
2016-10-09 12:47:43 +0530

    #leetcode #416 reduce space to O(sum)

--- leetcode/src/PartitionEqualSubsetSum.java
+++ leetcode/src/PartitionEqualSubsetSum.java
@@ -8,14 +8,13 @@ public class PartitionEqualSubsetSum {
         for (int num : nums) sum += num;
         if (N == 1 || sum % 2 == 1) return false;
         sum >>>= 1;
-        boolean[][] dp = new boolean[N + 1][sum + 1];
-        dp[0][0] = true;
-        for (int i = 1; i <= N; i++) {
-            dp[i][0] = true;
-            for (int j = 1; j <= sum; j++) {
-                dp[i][j] = dp[i - 1][j] || (j - nums[i - 1] >= 0 && dp[i - 1][j - nums[i - 1]]);
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true;
+        for (int num : nums) {
+            for (int j = sum; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
             }
         }
-        return dp[N][sum];
+        return dp[sum];
     }
 }
