4b22d81a5a6420291f6738fa9d76fccaf61d3884
Pankaj More <pankajmore@gmail.com>
2016-10-11 10:06:09 +0530

    #leetcode #409

---
+++ leetcode/src/LongestPalindrome.java
@@ -0,0 +1,21 @@
+/**
+ * https://leetcode.com/problems/longest-palindrome/
+ * Created by pankaj on 10/11/16.
+ */
+public class LongestPalindrome {
+    public int longestPalindrome(String s) {
+        int[] counts = new int[256];
+        for (char c : s.toCharArray()) counts[c] += 1;
+        int sum = 0;
+        boolean odd = false;
+        for (int cnt : counts) {
+            if (cnt % 2 == 0) {
+                sum += cnt;
+            } else {
+                sum += cnt - 1;
+                odd = true;
+            }
+        }
+        return sum + (odd ? 1 : 0);
+    }
+}
