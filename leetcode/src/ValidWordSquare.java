3fd203d86275471ce34f02c8765c557822e28883
Pankaj More <pankajmore@gmail.com>
2016-10-16 14:10:29 +0530

    #leetcode #422

---
+++ leetcode/src/ValidWordSquare.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/valid-word-square
+ * Created by pankaj on 10/16/16.
+ */
+public class ValidWordSquare {
+    public boolean validWordSquare(List<String> words) {
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words.get(i).length(); j++) {
+                if (j >= words.size() || i >= words.get(i).length() || words.get(i).charAt(j) != words.get(j).charAt(i))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
