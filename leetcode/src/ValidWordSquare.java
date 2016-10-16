0aeadb399070117ed763f5dd1224a7ed38c3c51d
Pankaj More <pankajmore@gmail.com>
2016-10-16 14:14:09 +0530

    fix index

--- leetcode/src/ValidWordSquare.java
+++ leetcode/src/ValidWordSquare.java
@@ -8,7 +8,7 @@ public class ValidWordSquare {
     public boolean validWordSquare(List<String> words) {
         for (int i = 0; i < words.size(); i++) {
             for (int j = 0; j < words.get(i).length(); j++) {
-                if (j >= words.size() || i >= words.get(i).length() || words.get(i).charAt(j) != words.get(j).charAt(i))
+                if (j >= words.size() || i >= words.get(j).length() || words.get(i).charAt(j) != words.get(j).charAt(i))
                     return false;
             }
         }
