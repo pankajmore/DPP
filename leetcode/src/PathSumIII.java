39edcd0a59a91779c6aaa5b6043ff19f96032b9e
Pankaj More <pankajmore@gmail.com>
2016-10-25 22:49:56 +0530

    better method names

--- leetcode/src/PathSumIII.java
+++ leetcode/src/PathSumIII.java
@@ -5,12 +5,12 @@
 public class PathSumIII {
     public int pathSum(TreeNode root, int sum) {
         if (root == null) return 0;
-        return pathSum1(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+        return fromRootSum(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
     }
 
-    public int pathSum1(TreeNode root, int sum) {
+    public int fromRootSum(TreeNode root, int sum) {
         if (root == null) return 0;
-        return (root.val == sum ? 1 : 0) + pathSum1(root.left, sum - root.val) + pathSum1(root.right, sum - root.val);
+        return (root.val == sum ? 1 : 0) + fromRootSum(root.left, sum - root.val) + fromRootSum(root.right, sum - root.val);
     }
 
     public class TreeNode {
