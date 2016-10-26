63ab6d65e0dea2cd2ccb97305715766b2671a6d0
Pankaj More <pankajmore@gmail.com>
2016-10-26 22:35:54 +0530

    #leetcode #283

---
+++ leetcode/src/MoveZeroes.java
@@ -0,0 +1,15 @@
+/**
+ * https://leetcode.com/problems/move-zeroes/
+ * Created by pankaj on 10/26/16.
+ */
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int i, idx, N = nums.length;
+        for(i = 0, idx = 0; i < N; i++) {
+            if(nums[i] != 0) {
+                nums[idx++] = nums[i];
+            }
+        }
+        for(; idx < N; idx++) nums[idx] = 0;
+    }
+}
