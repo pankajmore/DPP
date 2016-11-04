521d0e677a97a9a5ed74c8720443a4e43b4baeb5
Pankaj More <pankajmore@gmail.com>
2016-11-04 09:46:26 +0530

    #jeffe #dp #q4 number of times a given string occurs as a sub-sequence in another string

---
+++ jeffe/src/NumberOfSubsequences.java
@@ -0,0 +1,35 @@
+import com.sun.istack.internal.NotNull;
+
+import java.util.List;
+
+/**
+ * http://jeffe.cs.illinois.edu/teaching/algorithms/notes/05-dynprog.pdf
+ * Page - 20, Q4
+ * Created by pankaj on 11/4/16.
+ */
+public class NumberOfSubsequences {
+    public static int numberOfSubsequences(@NotNull List<Integer> A, @NotNull List<Integer> B) {
+        int N = A.size(), K = B.size();
+        int[] dp = new int[K + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= N; i++) {
+            int prev = dp[0];
+            for (int j = 1; j <= K; j++) {
+                int temp = dp[j];
+                dp[j] += A.get(i - 1).equals(B.get(j - 1)) ? prev : 0;
+                prev = temp;
+            }
+        }
+        return dp[K];
+    }
+
+    public static int numberOfSubsequencesSlow(@NotNull List<Integer> A, @NotNull List<Integer> B) {
+        int cnt = 0, N = A.size();
+        for (int i = 0; i < 1 << N; i++) {
+            if (Integer.bitCount(i) != B.size()) continue;
+            List<Integer> subSeq = Sequences.subset(A, i);
+            if (subSeq.equals(B)) cnt++;
+        }
+        return cnt;
+    }
+}
---
+++ jeffe/test/NumberOfSubsequencesTest.java
@@ -0,0 +1,37 @@
+import com.pholser.junit.quickcheck.ForAll;
+import com.pholser.junit.quickcheck.generator.InRange;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by pankaj on 11/4/16.
+ */
+@RunWith(Theories.class)
+public class NumberOfSubsequencesTest {
+    @Test
+    public void numberOfSubsequences() throws Exception {
+        Assert.assertEquals(1, NumberOfSubsequences.numberOfSubsequences(Arrays.asList(), Arrays.asList()));
+        Assert.assertEquals(1, NumberOfSubsequences.numberOfSubsequences(Arrays.asList(1), Arrays.asList()));
+        Assert.assertEquals(0, NumberOfSubsequences.numberOfSubsequences(Arrays.asList(), Arrays.asList(1)));
+
+        Assert.assertEquals(1, NumberOfSubsequences.numberOfSubsequences(Arrays.asList(1), Arrays.asList(1)));
+        Assert.assertEquals(3, NumberOfSubsequences.numberOfSubsequences(Arrays.asList(1, 1, 1), Arrays.asList(1)));
+        Assert.assertEquals(6, NumberOfSubsequences.numberOfSubsequences(Arrays.asList(1, 1, 1, 1), Arrays.asList(1, 1)));
+    }
+
+    @Theory
+    public void compareNumberOfSubsequences(@ForAll @InRange(minInt = 1, maxInt = 2) List<Integer> A,
+                                            @ForAll @InRange(minInt = 1, maxInt = 2) List<Integer> B) {
+        if (A.size() >= 20 || B.size() > A.size()) return;
+        int expected = NumberOfSubsequences.numberOfSubsequencesSlow(A, B);
+        int actual = NumberOfSubsequences.numberOfSubsequences(A, B);
+        Assert.assertEquals(expected, actual);
+    }
+
+}
