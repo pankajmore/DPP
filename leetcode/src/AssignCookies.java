9f316178cafe6fca6bd2833ad0575d3ae172a289
Pankaj More <pankajmore@gmail.com>
2016-11-14 10:11:23 +0530

    #leetcode #455

---
+++ leetcode/src/AssignCookies.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+/**
+ * Created by pankaj on 14/11/16.
+ */
+public class AssignCookies {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        for (int j = 0; j < s.length; j++) {
+            if (g[i] <= s[j]) {
+                i++;
+            }
+        }
+        return i;
+    }
+}
