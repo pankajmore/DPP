33c3d2378435637fa3dfa72c517e677ddc555346
Pankaj More <pankajmore@gmail.com>
2016-11-16 08:26:09 +0530

    #jeffe #dp #q10 (a,b)

---
+++ jeffe/src/Q10.java
@@ -0,0 +1,43 @@
+import com.sun.istack.internal.NotNull;
+
+import java.util.*;
+
+/**
+ * http://jeffe.cs.illinois.edu/teaching/algorithms/notes/05-dynprog.pdf
+ * Page - 22, Q10
+ * Created by pankaj on 15/11/16.
+ */
+public class Q10 {
+    public static int largestNonIntersectingSubset(@NotNull List<Integer> A, @NotNull List<Integer> B) {
+        assert A.size() == B.size();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < A.size(); i++) {
+            map.put(B.get(i), A.get(i));
+        }
+        Collections.sort(B, Comparator.comparing(map::get));
+        return Sequences.longestMonotonicSubsequence(B, true);
+    }
+
+    public static int largestIntersectingSubset(@NotNull List<Integer> A, @NotNull List<Integer> B) {
+        assert A.size() == B.size();
+        if (A.size() == 0) return 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < A.size(); i++) {
+            map.put(B.get(i), A.get(i));
+        }
+        Collections.sort(B, Comparator.comparing(map::get));
+        int maxCnt = 1, cnt = 1, minY = B.get(0);
+        for (int i = 1; i < B.size(); i++) {
+            if (B.get(i) < minY) {
+                cnt++;
+                minY = Math.min(minY, B.get(i));
+            } else {
+                maxCnt = Math.max(maxCnt, cnt);
+                minY = B.get(i);
+                cnt = 1;
+            }
+        }
+        maxCnt = Math.max(maxCnt, cnt);
+        return maxCnt;
+    }
+}
---
+++ jeffe/test/Q10Test.java
@@ -0,0 +1,30 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Created by pankaj on 15/11/16.
+ */
+public class Q10Test {
+    @Test
+    public void largestNonIntersectingSubset() throws Exception {
+        Assert.assertEquals(0, Q10.largestNonIntersectingSubset(Collections.emptyList(), Collections.emptyList()));
+        Assert.assertEquals(1, Q10.largestNonIntersectingSubset(Collections.singletonList(0), Collections.singletonList(1)));
+        Assert.assertEquals(2, Q10.largestNonIntersectingSubset(Arrays.asList(0, 1), Arrays.asList(1, 2)));
+        Assert.assertEquals(1, Q10.largestNonIntersectingSubset(Arrays.asList(0, 1), Arrays.asList(1, 0)));
+        Assert.assertEquals(2, Q10.largestNonIntersectingSubset(Arrays.asList(0, 1, 2), Arrays.asList(1, 0, 3)));
+    }
+
+    @Test
+    public void largestIntersectingSubset() throws Exception {
+        Assert.assertEquals(0, Q10.largestIntersectingSubset(Collections.emptyList(), Collections.emptyList()));
+        Assert.assertEquals(1, Q10.largestIntersectingSubset(Collections.singletonList(0), Collections.singletonList(1)));
+        Assert.assertEquals(1, Q10.largestIntersectingSubset(Arrays.asList(0, 1), Arrays.asList(1, 2)));
+        Assert.assertEquals(2, Q10.largestIntersectingSubset(Arrays.asList(0, 1), Arrays.asList(1, 0)));
+        Assert.assertEquals(3, Q10.largestIntersectingSubset(Arrays.asList(0, 1, 2), Arrays.asList(2, 1, 0)));
+        Assert.assertEquals(2, Q10.largestIntersectingSubset(Arrays.asList(0, 1, 2), Arrays.asList(-1, 2, 1)));
+    }
+
+}
