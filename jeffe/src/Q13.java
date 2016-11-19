8a856ddaea0be08bf9efe82afa29099534fe05c8
Pankaj More <pankajmore@gmail.com>
2016-11-19 11:39:42 +0530

    #jeffe #dp #q13

---
+++ jeffe/src/Q13.java
@@ -0,0 +1,35 @@
+import com.sun.istack.internal.NotNull;
+
+/**
+ * http://jeffe.cs.illinois.edu/teaching/algorithms/notes/05-dynprog.pdf
+ * Page - 23, Q13
+ * Created by pankaj on 19/11/16.
+ */
+public class Q13 {
+    public static int lcsNonOverlapFB(@NotNull String s) {
+        int N = s.length();
+        int[][] dp = new int[N][N];
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; i + j < N - 1; j++) {
+                dp[i][j] = s.charAt(i) != s.charAt(N - 1 - j) ? 0 : (i == 0 || j == 0 ? 0 : dp[i - 1][j - 1]) + 1;
+                max = Math.max(max, dp[i][j]);
+            }
+        }
+        return max;
+    }
+
+    public static int lcsNonOverlapFBSlow(@NotNull String s) {
+        int N = s.length();
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j < N; j++) {
+                String sub = s.substring(i, j + 1);
+                String r = new StringBuilder(sub).reverse().toString();
+                String rem = s.substring(j + 1);
+                if (rem.contains(r)) max = Math.max(max, j - i + 1);
+            }
+        }
+        return max;
+    }
+}
---
+++ jeffe/test/Q13Test.java
@@ -0,0 +1,40 @@
+import com.pholser.junit.quickcheck.ForAll;
+import com.pholser.junit.quickcheck.generator.InRange;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+/**
+ * Created by pankaj on 19/11/16.
+ */
+@RunWith(Theories.class)
+public class Q13Test {
+    @Test
+    public void lcsNonOverlapFB() throws Exception {
+        Assert.assertEquals(0, Q13.lcsNonOverlapFB(""));
+        Assert.assertEquals(0, Q13.lcsNonOverlapFB("a"));
+        Assert.assertEquals(0, Q13.lcsNonOverlapFB("ab"));
+        Assert.assertEquals(1, Q13.lcsNonOverlapFB("aa"));
+        Assert.assertEquals(1, Q13.lcsNonOverlapFB("aba"));
+        Assert.assertEquals(2, Q13.lcsNonOverlapFB("abba"));
+
+        Assert.assertEquals(0, Q13.lcsNonOverlapFB("ALGORITHM"));
+        Assert.assertEquals(1, Q13.lcsNonOverlapFB("RECURSION"));
+        Assert.assertEquals(3, Q13.lcsNonOverlapFB("REDIVIDE"));
+        Assert.assertEquals(4, Q13.lcsNonOverlapFB("DYNAMICPROGRAMMINGMANYTIMES"));
+    }
+
+    @Theory
+    public void compare(@ForAll @InRange(minInt = 0, maxInt = 1) List<Integer> A) {
+        StringBuilder sb = new StringBuilder();
+        A.forEach(sb::append);
+        String s = sb.toString();
+        int expected = Q13.lcsNonOverlapFBSlow(s);
+        int actual = Q13.lcsNonOverlapFB(s);
+        Assert.assertEquals(expected, actual);
+    }
+}
