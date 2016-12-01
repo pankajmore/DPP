9782f3a4e88ed0c4ce9e05f8887720298be849eb
Pankaj More <pankajmore@gmail.com>
2016-12-01 20:32:18 +0530

    #leetcode #459 alternative

--- leetcode/src/RepeatedSubstringPattern.java
+++ leetcode/src/RepeatedSubstringPattern.java
@@ -18,4 +18,8 @@ public class RepeatedSubstringPattern {
         }
         return true;
     }
+
+    public boolean repeatedSubstringPattern1(String str) {
+        return (str + str).substring(1, 2 * str.length() - 1).contains(str);
+    }
 }
