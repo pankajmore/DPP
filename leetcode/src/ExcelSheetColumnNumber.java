2e42a2800adbe99f8cd7314371bbbbbd2eee1994
Pankaj More <pankajmore@gmail.com>
2016-12-14 11:58:04 +0530

    #leetcode #171

---
+++ leetcode/src/ExcelSheetColumnNumber.java
@@ -0,0 +1,11 @@
+/**
+ * https://leetcode.com/problems/excel-sheet-column-number/
+ * Created by pankaj on 14/12/16.
+ */
+public class ExcelSheetColumnNumber {
+    public int titleToNumber(String s) {
+        int x = 0;
+        for (char c : s.toCharArray()) x = x * 26 + (c - 'A' + 1);
+        return x;
+    }
+}
