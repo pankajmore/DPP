787dbec644e653699702960505729bf9fd239fd0
Pankaj More <pankajmore@gmail.com>
2016-12-20 22:03:17 +0530

    #leetcode #326

---
+++ leetcode/src/PowerOfThree.java
@@ -0,0 +1,9 @@
+/**
+ * https://leetcode.com/problems/power-of-three/
+ * Created by pankaj on 20/12/16.
+ */
+public class PowerOfThree {
+    public boolean isPowerOfThree(int n) {
+        return n > 0 && 1162261467 % n == 0;
+    }
+}
