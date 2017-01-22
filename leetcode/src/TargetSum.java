ccc14657c6d739dba9d06b6b6a3e9b07401ef89d
Pankaj More <pankajmore@gmail.com>
2017-01-22 15:09:20 +0530

    #leetcode #494 alternatives

--- leetcode/src/TargetSum.java
+++ leetcode/src/TargetSum.java
@@ -1,5 +1,5 @@
 /**
- * https://leetcode.com/contest/leetcode-weekly-contest-16a/problems/target-sum/
+ * https://leetcode.com/problems/target-sum/
  * Created by pankaj on 21/01/17.
  */
 public class TargetSum {
@@ -18,5 +18,39 @@ public class TargetSum {
             }
         }
         return dp[N - 1][S + max];
+    }
+
+    public int findTargetSumWays1(int[] nums, int S) {
+        return waysHelper(nums, 0, S, 0);
+    }
+
+    private int waysHelper(int[] nums, int i, int S, int curr) {
+        if (i == nums.length) {
+            return curr == S ? 1 : 0;
+        }
+        return waysHelper(nums, i + 1, S, curr - nums[i])
+                + waysHelper(nums, i + 1, S, curr + nums[i]);
+    }
+
+    public int findTargetSumWays2(int[] nums, int S) {
+        int N = nums.length, sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += nums[i];
+            nums[i] += nums[i];
+        }
+        // reduce to a subset sum problem by magic!
+        return sum < S ? 0 : subsetSum(nums, sum + S);
+    }
+
+    private int subsetSum(int[] nums, int s) {
+        int N = nums.length;
+        int[][] dp = new int[2][s + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j <= s; j++) {
+                dp[i % 2][j] = dp[(i - 1) % 2][j] + (j < nums[i - 1] ? 0 : dp[(i - 1) % 2][j - nums[i - 1]]);
+            }
+        }
+        return dp[N % 2][s];
     }
 }
