3e1b77bf21fb82533564afb79585d5a1f629494b
Pankaj More <pankajmore@gmail.com>
2017-01-26 23:24:46 +0530

    #leetcode #367

---
+++ leetcode/src/ValidPerfectSquare.java
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/valid-perfect-square/
+ * Created by pankaj on 26/01/17.
+ */
+public class ValidPerfectSquare {
+    public boolean isPerfectSquare(int num) {
+        int lo = 1, hi = num;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            long p = (long) mid * (long) mid;
+            if (p == num) return true;
+            else if (p > num) hi = mid - 1;
+            else lo = mid + 1;
+        }
+        return false;
+    }
+}
