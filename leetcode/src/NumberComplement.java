8156d31717a65927e72fdb4483df32b6a8f5f657
Pankaj More <pankajmore@gmail.com>
2017-01-27 23:12:10 +0530

    #leetcode #476

---
+++ leetcode/src/NumberComplement.java
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode.com/problems/number-complement/
+ * Created by pankaj on 27/01/17.
+ */
+public class NumberComplement {
+    public int findComplement(int num) {
+        int mask = 0, n = num;
+        while (n > 0) {
+            n >>>= 1;
+            mask = (mask << 1) + 1;
+        }
+        return ~num & mask;
+    }
+}
