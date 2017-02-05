9b177e236669488dfeb1b96e1128651b2ee9b5cc
Pankaj More <pankajmore@gmail.com>
2017-02-05 14:49:39 +0530

    #leetcode #506

---
+++ leetcode/src/RelativeRanks.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/contest/leetcode-weekly-contest-18b/problems/relative-ranks/
+ * Created by pankaj on 05/02/17.
+ */
+public class RelativeRanks {
+    public String[] findRelativeRanks(int[] nums) {
+        int N = nums.length;
+        int[] A = nums.clone();
+        String[] res = new String[N];
+        Arrays.sort(A);
+        Map<Integer, Integer> rank = new HashMap<>();
+        for (int i = 0; i < N; i++) rank.put(A[i], N - i);
+        for (int i = 0; i < N; i++) {
+            int r = rank.get(nums[i]);
+            res[i] = r == 1 ? "Gold Medal" : r == 2 ? "Silver Medal" : r == 3 ? "Bronze Medal" : String.valueOf(r);
+        }
+        return res;
+    }
+}
