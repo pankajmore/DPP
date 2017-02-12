1f3136378b3f4f0656b7f25339c207efbd7e2edf
Pankaj More <pankajmore@gmail.com>
2017-02-13 00:01:15 +0530

    #leetcode #513

---
+++ leetcode/src/FindLeftMostElement.java
@@ -0,0 +1,29 @@
+/**
+ * https://leetcode.com/contest/leetcode-weekly-contest-19/problems/find-left-most-element/
+ * Created by pankaj on 13/02/17.
+ */
+public class FindLeftMostElement {
+    public int findLeftMostNode(TreeNode root) {
+        return find(root, 0)[0];
+    }
+
+    public int[] find(TreeNode root, int depth) {
+        if (root == null) return new int[]{-1, -1};
+        int[] left = find(root.left, depth + 1);
+        int[] right = find(root.right, depth + 1);
+        if (left[1] == -1 && right[1] == -1) {
+            return new int[]{root.val, depth};
+        } else if (left[1] == -1) {
+            return right;
+        } else if (right[1] == -1) {
+            return left;
+        } else {
+            return left[1] >= right[1] ? left : right;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left, right;
+    }
+}
