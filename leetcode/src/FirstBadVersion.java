cdc9acab4456ab6c10f148369ebf196918a87b56
Pankaj More <pankajmore@gmail.com>
2017-02-17 22:12:30 +0530

    #leetcode #278

---
+++ leetcode/src/FirstBadVersion.java
@@ -0,0 +1,25 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * https://leetcode.com/problems/first-bad-version/
+ * Created by pankaj on 17/02/17.
+ */
+public class FirstBadVersion {
+    class Solution extends VersionControl {
+        public int firstBadVersion(int n) {
+            int lo = 1, hi = n;
+            while (lo < hi) {
+                int mid = lo + (hi - lo) / 2;
+                if (!isBadVersion(mid)) lo = mid + 1;
+                else hi = mid;
+            }
+            return lo;
+        }
+    }
+
+    class VersionControl {
+        boolean isBadVersion(int version) {
+            throw new NotImplementedException();
+        }
+    }
+}
