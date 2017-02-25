a76087de4df63bdf3b01069c3563d83765e304a8
Pankaj More <pankajmore@gmail.com>
2017-02-25 13:28:02 +0530

    #leetcode #484 cleaner

--- leetcode/src/ProductOfArrayExceptSelf.java
+++ leetcode/src/ProductOfArrayExceptSelf.java
@@ -6,12 +6,13 @@ public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
         int N = nums.length;
         int[] res = new int[N];
-        res[N - 1] = 1;
-        for (int i = N - 2; i >= 0; i--) res[i] = (i == N - 1 ? 1 : res[i + 1]) * nums[i + 1];
-        int left = 1;
-        for (int i = 0; i < N; i++) {
-            res[i] *= left;
+        for (int i = 0, left = 1; i < N; i++) {
+            res[i] = left;
             left *= nums[i];
+        }
+        for (int i = N - 1, right = 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
         }
         return res;
     }
