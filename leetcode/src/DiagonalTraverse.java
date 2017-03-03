233aa0f72ae7396524a43c88d0f61421a4531667
Pankaj More <pankajmore@gmail.com>
2017-03-03 22:46:53 +0530

    #leetcode #498

--- leetcode/src/DiagonalTraverse.java
+++ leetcode/src/DiagonalTraverse.java
@@ -15,24 +15,10 @@ public class DiagonalTraverse {
             row -= d;
             col += d;
 
-            if (row >= m) {
-                row = m - 1;
-                col += 2;
-                d = -d;
-            }
-            if (col >= n) {
-                col = n - 1;
-                row += 2;
-                d = -d;
-            }
-            if (row < 0) {
-                row = 0;
-                d = -d;
-            }
-            if (col < 0) {
-                col = 0;
-                d = -d;
-            }
+            if (row >= m) { row = m - 1; col += 2; d = -d;}
+            if (col >= n) { col = n - 1; row += 2; d = -d;}
+            if (row < 0)  { row = 0; d = -d;}
+            if (col < 0)  { col = 0; d = -d;}
         }
 
         return result;
