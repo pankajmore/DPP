9971aa9d60b274ecbd4808a45bb22b355c4513a1
Pankaj More <pankajmore@gmail.com>
2017-03-08 22:18:41 +0530

    #interviewbit https://www.interviewbit.com/problems/largest-rectangle-in-histogram/

---
+++ InterviewBit/src/stacksAndQueues/LargestRectangleInHistogram.java
@@ -0,0 +1,20 @@
+package stacksAndQueues;
+
+/**
+ * https://www.interviewbit.com/problems/largest-rectangle-in-histogram/ Created by pankaj on
+ * 08/03/17.
+ */
+public class LargestRectangleInHistogram {
+  public int largestRectangleArea(ArrayList<Integer> A) {
+    int N = A.size(), maxArea = 0;
+    Stack<Integer> s = new Stack<>();
+    for (int i = 0; i <= N; i++) {
+      while (!s.isEmpty() && (i == A.size() || A.get(i) < A.get(s.peek()))) {
+        int h = A.get(s.pop());
+        maxArea = Math.max(maxArea, h * (i - (s.isEmpty() ? 0 : s.peek() + 1)));
+      }
+      s.push(i);
+    }
+    return maxArea;
+  }
+}
