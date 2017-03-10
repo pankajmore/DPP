276f8bb5afcd6d0c671179032346563e9f264390
Pankaj More <pankajmore@gmail.com>
2017-03-10 11:21:45 +0530

    #leetcode #84

---
+++ leetcode/src/LargestRectangleInHistogram.java
@@ -0,0 +1,26 @@
+/** https://leetcode.com/problems/largest-rectangle-in-histogram/ Created by pankaj on 10/03/17. */
+public class LargestRectangleInHistogram {
+  public int largestRectangleArea(int[] heights) {
+    int N = heights.length, maxArea = 0;
+    int[] right = new int[N];
+    Stack<Integer> s = new Stack<>();
+    for (int i = N - 1; i >= 0; i--) {
+      while (!s.isEmpty() && heights[i] <= heights[s.peek()]) {
+        s.pop();
+      }
+      right[i] = s.isEmpty() ? N - 1 : s.peek() - 1;
+      s.push(i);
+    }
+    int left = 0;
+    s.clear();
+    for (int i = 0; i < N; i++) {
+      while (!s.isEmpty() && heights[i] <= heights[s.peek()]) {
+        s.pop();
+      }
+      left = s.isEmpty() ? 0 : s.peek() + 1;
+      s.push(i);
+      maxArea = Math.max(maxArea, (right[i] - left + 1) * heights[i]);
+    }
+    return maxArea;
+  }
+}
