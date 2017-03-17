1a4937a0ef7fcb809fb2f9eadf7bfae0770cd3f9
Pankaj More <pankajmore@gmail.com>
2017-03-17 22:56:57 +0530

    #leetcode #260 bitwise trick to get last set bit

--- leetcode/src/SingleNumberIII.java
+++ leetcode/src/SingleNumberIII.java
@@ -3,18 +3,12 @@ public class SingleNumberIII {
   public int[] singleNumber(int[] nums) {
     int x = 0;
     for (int n : nums) x ^= n;
-    int mask = findHighestBit(x);
+    int mask = x & -x; // get the last bit set in x
     int a = 0, b = 0;
     for (int n : nums) {
       if ((n & mask) != 0) a ^= n;
       else b ^= n;
     }
     return new int[] {a, b};
-  }
-
-  private int findHighestBit(int x) {
-    int mask = 1;
-    while ((mask & x) == 0) mask <<= 1;
-    return mask;
   }
 }
