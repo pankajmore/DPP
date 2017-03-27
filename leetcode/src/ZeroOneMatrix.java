a6272c363f924ccb4866c65670bcb5e6265e0698
Pankaj More <pankajmore@gmail.com>
2017-03-27 21:25:19 +0530

    #leetcode #542

--- leetcode/src/ZeroOneMatrix.java
+++ leetcode/src/ZeroOneMatrix.java
@@ -1,6 +1,4 @@
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 /** https://leetcode.com/problems/01-matrix/ Created by pankaj on 25/03/17. */
 public class ZeroOneMatrix {
@@ -9,40 +7,33 @@ public class ZeroOneMatrix {
     int M = matrix.size(), N = matrix.get(0).size();
     for (int i = 0; i < M; i++) {
       for (int j = 0; j < N; j++) {
-        if (matrix.get(i).get(j) != 0) {
-          matrix.get(i).set(j, bfs(i, j, matrix));
-        }
+        if (matrix.get(i).get(j) != 0) matrix.get(i).set(j, M + N);
+      }
+    }
+    for (int i = 0; i < M; i++) {
+      for (int j = 0; j < N; j++) {
+        int v =
+            Math.min(
+                matrix.get(i).get(j),
+                Math.min(
+                        i == 0 ? M + N : matrix.get(i - 1).get(j),
+                        j == 0 ? M + N : matrix.get(i).get(j - 1))
+                    + 1);
+        matrix.get(i).set(j, v);
+      }
+    }
+    for (int i = M - 1; i >= 0; i--) {
+      for (int j = N - 1; j >= 0; j--) {
+        int v =
+            Math.min(
+                matrix.get(i).get(j),
+                Math.min(
+                        i == M - 1 ? M + N : matrix.get(i + 1).get(j),
+                        j == N - 1 ? M + N : matrix.get(i).get(j + 1))
+                    + 1);
+        matrix.get(i).set(j, v);
       }
     }
     return matrix;
-  }
-
-  private int bfs(int i, int j, List<List<Integer>> matrix) {
-    int l = 1;
-    int M = matrix.size(), N = matrix.get(0).size();
-    boolean[][] visited = new boolean[M][N];
-    int[][] dir = new int[][] {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
-    Queue<int[]> q = new LinkedList<>();
-    int[] s = new int[] {i, j};
-    q.add(s);
-    q.add(null);
-    visited[i][j] = true;
-    while (!q.isEmpty()) {
-      int[] u = q.remove();
-      if (u == null) {
-        l++;
-        q.add(null);
-        continue;
-      }
-      for (int[] d : dir) {
-        int[] v = new int[] {u[0] + d[0], u[1] + d[1]};
-        if (v[0] >= 0 && v[0] < M && v[1] >= 0 && v[1] < N && !visited[v[0]][v[1]]) {
-          if (matrix.get(v[0]).get(v[1]) == 0) return l;
-          visited[v[0]][v[1]] = true;
-          q.add(v);
-        }
-      }
-    }
-    return -1;
   }
 }
