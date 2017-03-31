6ddd56926e102801f91ba0a37a1f5afc205cb8f5
Pankaj More <pankajmore@gmail.com>
2017-03-31 20:32:29 +0530

    #leetcode #174

---
+++ leetcode/src/DungeonGame.java
@@ -0,0 +1,22 @@
+/** https://leetcode.com/problems/dungeon-game/ Created by pankaj on 31/03/17. */
+public class DungeonGame {
+  public int calculateMinimumHP(int[][] A) {
+    if (A.length == 0 || A[0].length == 0) return 0;
+    int M = A.length, N = A[0].length;
+    int[][] dp = new int[M][N];
+    for (int i = M - 1; i >= 0; i--) {
+      for (int j = N - 1; j >= 0; j--) {
+        dp[i][j] = Integer.MAX_VALUE;
+        if (i + 1 < M) {
+          dp[i][j] = Math.min(dp[i][j], dp[i + 1][j] - A[i][j]);
+        }
+        if (j + 1 < N) {
+          dp[i][j] = Math.min(dp[i][j], dp[i][j + 1] - A[i][j]);
+        }
+        if (dp[i][j] <= 0) dp[i][j] = 1;
+        if (dp[i][j] == Integer.MAX_VALUE) dp[i][j] = Math.max(1, 1 - A[i][j]);
+      }
+    }
+    return dp[0][0];
+  }
+}
