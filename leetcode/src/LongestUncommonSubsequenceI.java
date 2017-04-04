653b89c6806d22f0f1efa7d8e60b22ffcb385f3b
Pankaj More <pankajmore@gmail.com>
2017-04-04 22:00:07 +0530

    #leetcode #521 update url

--- leetcode/src/LongestUncommonSubsequenceI.java
+++ leetcode/src/LongestUncommonSubsequenceI.java
@@ -1,7 +1,4 @@
-/**
- * https://leetcode.com/contest/leetcode-weekly-contest-26/problems/longest-uncommon-subsequence-i/
- * Created by pankaj on 03/04/17.
- */
+/** https://leetcode.com/problems/longest-uncommon-subsequence-i/ Created by pankaj on 03/04/17. */
 public class LongestUncommonSubsequenceI {
   public int findLUSlength(String a, String b) {
     return a.equals(b) ? -1 : Math.max(a.length(), b.length());
