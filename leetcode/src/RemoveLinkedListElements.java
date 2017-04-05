28c168781cc6b30c68359932b4a8ab94961c247e
Pankaj More <pankajmore@gmail.com>
2017-04-05 23:08:01 +0530

    #leetcode #203

---
+++ leetcode/src/RemoveLinkedListElements.java
@@ -0,0 +1,26 @@
+/** https://leetcode.com/problems/remove-linked-list-elements/ Created by pankaj on 05/04/17. */
+public class RemoveLinkedListElements {
+  public ListNode removeElements(ListNode head, int val) {
+    ListNode dummyHead = new ListNode(0);
+    ListNode curr = dummyHead, next;
+    dummyHead.next = head;
+    while (curr != null) {
+      next = curr.next;
+      while (next != null && next.val == val) {
+        next = next.next;
+      }
+      curr.next = next;
+      curr = curr.next;
+    }
+    return dummyHead.next;
+  }
+
+  private static class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+      this.val = val;
+    }
+  }
+}
