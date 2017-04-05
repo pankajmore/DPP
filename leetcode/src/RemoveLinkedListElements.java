8c810dfcad0f4899b2e8477de2263f84bea8c27b
Pankaj More <pankajmore@gmail.com>
2017-04-05 23:11:35 +0530

    #leetcode #203 elegant recursion

--- leetcode/src/RemoveLinkedListElements.java
+++ leetcode/src/RemoveLinkedListElements.java
@@ -1,5 +1,11 @@
 /** https://leetcode.com/problems/remove-linked-list-elements/ Created by pankaj on 05/04/17. */
 public class RemoveLinkedListElements {
+  public ListNode removeElementsRecursive(ListNode head, int val) {
+    if (head == null) return null;
+    head.next = removeElementsRecursive(head.next, val);
+    return head.val == val ? head.next : head;
+  }
+
   public ListNode removeElements(ListNode head, int val) {
     ListNode dummyHead = new ListNode(0);
     ListNode curr = dummyHead, next;
