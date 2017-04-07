1bba3efd58fe43789f091d1cf23c8b0d9686afb1
Pankaj More <pankajmore@gmail.com>
2017-04-07 23:33:19 +0530

    #leetcode #144

---
+++ leetcode/src/BinaryTreePreOrderTraversal.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/** Created by pankaj on 07/04/17. */
+public class BinaryTreePreOrderTraversal {
+  public List<Integer> preorderTraversal(TreeNode root) {
+    List<Integer> preOrder = new ArrayList<>();
+    preOrderHelper(root, preOrder);
+    return preOrder;
+  }
+
+  private void preOrderHelper(TreeNode root, List<Integer> preOrder) {
+    if (root != null) {
+      preOrder.add(root.val);
+      preOrderHelper(root.left, preOrder);
+      preOrderHelper(root.right, preOrder);
+    }
+  }
+
+  private static class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+  }
+}
