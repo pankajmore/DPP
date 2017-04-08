a1ca6f2a3641aee7da7a45c7a7e752af5474de72
Pankaj More <pankajmore@gmail.com>
2017-04-08 17:05:15 +0530

    #leetcode #501

---
+++ leetcode/src/FindModeInABinarySearchTree.java
@@ -0,0 +1,45 @@
+/** https://leetcode.com/problems/find-mode-in-binary-search-tree/ Created by pankaj on 08/04/17. */
+public class FindModeInABinarySearchTree {
+  private int currCount = 0, maxCount = 0, modeCount = 0;
+  private int currVal;
+  private int[] modes = null;
+
+  public int[] findMode(TreeNode root) {
+    inOrder(root);
+    modes = new int[modeCount];
+    currCount = 0;
+    modeCount = 0;
+    inOrder(root);
+    return modes;
+  }
+
+  private void handleValue(int val) {
+    if (val != currVal) {
+      currVal = val;
+      currCount = 0;
+    }
+    currCount++;
+    if (currCount > maxCount) {
+      maxCount = currCount;
+      modeCount = 1;
+    } else if (currCount == maxCount) {
+      if (modes != null) {
+        modes[modeCount] = val;
+      }
+      modeCount++;
+    }
+  }
+
+  private void inOrder(TreeNode root) {
+    if (root != null) {
+      inOrder(root.left);
+      handleValue(root.val);
+      inOrder(root.right);
+    }
+  }
+
+  private static class TreeNode {
+    int val;
+    TreeNode left, right;
+  }
+}
