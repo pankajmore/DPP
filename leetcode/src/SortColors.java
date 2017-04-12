ad0abdaf42590462e738b3ec59d91417790d2788
Pankaj More <pankajmore@gmail.com>
2017-04-12 21:39:04 +0530

    #leetcode #75. Sort Colors one pass

--- leetcode/src/SortColors.java
+++ leetcode/src/SortColors.java
@@ -1,10 +1,23 @@
 /** https://leetcode.com/problems/sort-colors/ Created by pankaj on 11/04/17. */
 public class SortColors {
   public void sortColors(int[] nums) {
-    int[] cnt = new int[3];
-    for (int x : nums) cnt[x]++;
-    for (int idx = 0, i = 0; i < 3; i++) {
-      while (cnt[i]-- > 0) nums[idx++] = i;
+    int N = nums.length, i = 0, j = N - 1, k = N;
+    while (i <= j) {
+      if (nums[j] == 1) --j;
+      else if (nums[i] == 0) ++i;
+      else if (nums[j] == 0) swap(nums, i, j);
+      else if (nums[i] == 1) swap(nums, i, j);
+      else if (nums[j] == 2) {
+        --k;
+        swap(nums, j, k);
+        --j;
+      } else if (nums[i] == 2) swap(nums, i, j);
     }
+  }
+
+  private void swap(int[] A, int i, int j) {
+    int tmp = A[i];
+    A[i] = A[j];
+    A[j] = tmp;
   }
 }
