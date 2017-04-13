8c272e24015ee84a8237cd8e850121400a1809d2
Pankaj More <pankajmore@gmail.com>
2017-04-13 22:01:37 +0530

    #leetcode #162. Find Peak Element -> O(logN)

--- leetcode/src/FindPeakElement.java
+++ leetcode/src/FindPeakElement.java
@@ -1,12 +1,16 @@
 /** https://leetcode.com/problems/find-peak-element/ Created by pankaj on 13/04/17. */
 public class FindPeakElement {
   public int findPeakElement(int[] nums) {
-    int N = nums.length;
-    for (int i = 0; i < N; i++) {
-      long curr = nums[i];
-      long prev = i == 0 ? Long.MIN_VALUE : nums[i - 1];
-      long next = i == N - 1 ? Long.MIN_VALUE : nums[i + 1];
-      if (curr > prev && curr > next) return i;
+    int N = nums.length, lo = 0, hi = N - 1;
+    while (lo <= hi) {
+      int mid = lo + (hi - lo) / 2;
+      long curr = nums[mid];
+      long prev = mid == 0 ? Long.MIN_VALUE : nums[mid - 1];
+      long next = mid == N - 1 ? Long.MIN_VALUE : nums[mid + 1];
+      long max = Math.max(curr, Math.max(prev, next));
+      if (max == prev) hi = mid - 1;
+      else if (max == next) lo = mid + 1;
+      else return mid;
     }
     return -1;
   }
