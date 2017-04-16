ff2a1f366f4e3a9978a9442026638e7c99c2882f
Pankaj More <pankajmore@gmail.com>
2017-04-16 19:36:02 +0530

    #leetcode #551. Student Attendance Record I add link

--- leetcode/src/StudentAttendanceRecordI.java
+++ leetcode/src/StudentAttendanceRecordI.java
@@ -1,16 +1,14 @@
-/**
- * Created by pankaj on 16/04/17.
- */
+/** https://leetcode.com/problems/student-attendance-record-i/ Created by pankaj on 16/04/17. */
 public class StudentAttendanceRecordI {
-    public boolean checkRecord(String s) {
-        int N = s.length(), cntA = 0, cntL = 0;
-        for (int i = 0; i < N; i++) {
-            char c = s.charAt(i);
-            if (c == 'L') cntL++;
-            else cntL = 0;
-            if (c == 'A') cntA++;
-            if (cntA > 1 || cntL > 2) return false;
-        }
-        return true;
+  public boolean checkRecord(String s) {
+    int N = s.length(), cntA = 0, cntL = 0;
+    for (int i = 0; i < N; i++) {
+      char c = s.charAt(i);
+      if (c == 'L') cntL++;
+      else cntL = 0;
+      if (c == 'A') cntA++;
+      if (cntA > 1 || cntL > 2) return false;
     }
+    return true;
+  }
 }
