f2aec44463c0c0e391dc39d7c81c987ee7a2acda
Pankaj More <pankajmore@gmail.com>
2017-04-24 22:00:46 +0530

    #leetcode #552. Student Attendance Record II (alternative)

--- leetcode/src/StudentAttendanceRecordII.java
+++ leetcode/src/StudentAttendanceRecordII.java
@@ -16,4 +16,21 @@ public class StudentAttendanceRecordII {
     }
     return (int) cnt;
   }
+
+  public int checkRecord1(int n) {
+    final int m = 1000000007;
+    int[][][] dp = new int[n + 1][2][3];
+    dp[0] = new int[][] {{1, 1, 1}, {1, 1, 1}};
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j < 2; j++) {
+        for (int k = 0; k < 3; k++) {
+          int val = dp[i - 1][j][2]; // ...P
+          if (j > 0) val = (val + dp[i - 1][j - 1][2]) % m; // ...A
+          if (k > 0) val = (val + dp[i - 1][j][k - 1]) % m; // ...L
+          dp[i][j][k] = val;
+        }
+      }
+    }
+    return dp[n][1][2];
+  }
 }
