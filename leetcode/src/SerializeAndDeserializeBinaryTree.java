d489c2ee2a1898acf14b1e96eb1c96b17b786f07
Pankaj More <pankajmore@gmail.com>
2017-05-10 22:53:19 +0530

    #leetcode #297. Serialize and Deserialize Binary Tree (simpler preorder)

--- leetcode/src/SerializeAndDeserializeBinaryTree.java
+++ leetcode/src/SerializeAndDeserializeBinaryTree.java
@@ -1,7 +1,7 @@
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * https://leetcode.com/problems/serialize-and-deserialize-binary-tree/ Created by pankaj on
@@ -10,65 +10,39 @@ import java.util.List;
 class SerializeAndDeserializeBinaryTree {
   // Encodes a tree to a single string.
   String serialize(final TreeNode root) {
-    List<Integer> s = new ArrayList<>();
-    Deque<TreeNode> q = new LinkedList<>();
-    q.add(root);
-    while (!q.isEmpty()) {
-      TreeNode curr = q.remove();
-      if (curr == null) {
-        s.add(null);
-      } else {
-        s.add(curr.val);
-        q.add(curr.left);
-        q.add(curr.right);
-      }
+    StringBuilder sb = new StringBuilder();
+    serializeInternal(root, sb);
+    return sb.toString();
+  }
+
+  private void serializeInternal(final TreeNode root, final StringBuilder sb) {
+    if (root == null) {
+      sb.append("# ");
+    } else {
+      sb.append(root.val).append(' ');
+      serializeInternal(root.left, sb);
+      serializeInternal(root.right, sb);
     }
-    return s.toString();
   }
 
   // Decodes your encoded data to tree.
   TreeNode deserialize(final String data) {
-    String[] ls = data.substring(1, data.length() - 1).split(", ");
-    List<Integer> vals = new ArrayList<>();
-    for (String s : ls) {
-      if (s.equals("null")) {
-        vals.add(null);
-      } else {
-        vals.add(Integer.parseInt(s));
-      }
-    }
-    if (vals.isEmpty()) {
+    String[] ls = data.split(" ");
+    List<String> vals = Arrays.stream(ls).collect(Collectors.toList());
+    Iterator<String> iter = vals.iterator();
+    return deserializeInternal(iter);
+  }
+
+  private TreeNode deserializeInternal(final Iterator<String> iter) {
+    String next = iter.next();
+    if (next.equals("#")) {
       return null;
-    }
-    Deque<TreeNode> q = new LinkedList<>();
-    TreeNode root;
-    if (vals.get(0) == null) {
-      root = null;
     } else {
-      root = new TreeNode(vals.get(0));
+      TreeNode root = new TreeNode(Integer.parseInt(next));
+      root.left = deserializeInternal(iter);
+      root.right = deserializeInternal(iter);
+      return root;
     }
-    q.add(root);
-    int i = 0;
-    while (!q.isEmpty()) {
-      TreeNode curr = q.remove();
-      if (curr != null) {
-        Integer leftValue = vals.get(++i);
-        Integer rightValue = vals.get(++i);
-        if (leftValue == null) {
-          curr.left = null;
-        } else {
-          curr.left = new TreeNode(leftValue);
-        }
-        if (rightValue == null) {
-          curr.right = null;
-        } else {
-          curr.right = new TreeNode(rightValue);
-        }
-        q.add(curr.left);
-        q.add(curr.right);
-      }
-    }
-    return root;
   }
 
   class TreeNode {
