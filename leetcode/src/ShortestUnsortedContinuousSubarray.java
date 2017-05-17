c452789d49f3dbb0a0e7a5b7df2e7751f30ebdb5
Pankaj More <pankajmore@gmail.com>
2017-05-17 22:35:49 +0530

    #leetcode #581. Shortest Unsorted Continuous Subarray

--- leetcode/src/ShortestUnsortedContinuousSubarray.java
+++ leetcode/src/ShortestUnsortedContinuousSubarray.java
@@ -6,31 +6,18 @@ import java.util.Arrays;
  */
 class ShortestUnsortedContinuousSubarray {
   int findUnsortedSubarray(final int[] nums) {
-    int start = 0, n = nums.length;
-    while (start + 1 < n) {
-      if (nums[start] > nums[start + 1]) {
-        break;
-      }
-      start++;
-    }
-    if (start == n - 1) {
-      return 0;
-    }
-    int max = nums[start], min = nums[start];
-    int end = start + 1;
-    for (int i = start + 1; i < n; i++) {
+    int n = nums.length, beg = -1, end = -2, min = nums[n - 1], max = nums[0];
+    for (int i = 1; i < n; i++) {
+      max = Math.max(max, nums[i]);
+      min = Math.min(min, nums[n - 1 - i]);
       if (nums[i] < max) {
         end = i;
       }
-      max = Math.max(max, nums[i]);
-      min = Math.min(min, nums[i]);
-    }
-    for (int i = start - 1; i >= 0; i--) {
-      if (nums[i] > min) {
-        start = i;
+      if (nums[n - 1 - i] > min) {
+        beg = n - 1 - i;
       }
     }
-    return end - start + 1;
+    return end - beg + 1;
   }
 
   int findUnsortedSubarray1(final int[] nums) {
