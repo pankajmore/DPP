be2e728c141965b4343afadb91e6b86727f06296
Pankaj More <pankajmore@gmail.com>
2017-05-19 23:17:43 +0530

    #leetcode #516. Longest Palindromic Subsequence

--- leetcode/src/LongestPalindromicSubsequence.java
+++ leetcode/src/LongestPalindromicSubsequence.java
@@ -4,18 +4,19 @@ class LongestPalindromicSubsequence {
     if (s == null || s.isEmpty()) {
       return 0;
     }
-    String r = new StringBuilder().append(s).reverse().toString();
     int n = s.length();
-    int[][] dp = new int[n + 1][n + 1];
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n; j++) {
-        if (s.charAt(i - 1) == r.charAt(j - 1)) {
-          dp[i][j] = dp[i - 1][j - 1] + 1;
+    int[][] dp = new int[n][n];
+    for (int i = n - 1; i >= 0; i--) {
+      for (int j = i; j < n; j++) {
+        if (i == j) {
+          dp[i][i] = 1;
+        } else if (s.charAt(i) == s.charAt(j)) {
+          dp[i][j] = 2 + (i + 1 > j - 1 ? 0 : dp[i + 1][j - 1]);
         } else {
-          dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+          dp[i][j] = Math.max(i + 1 >= n ? 0 : dp[i + 1][j], j - 1 < 0 ? 0 : dp[i][j - 1]);
         }
       }
     }
-    return dp[n][n];
+    return dp[0][n - 1];
   }
 }
