6a466e4d08e22bca1308150cfc04b1c63ac2858b
Pankaj More <pankajmore@gmail.com>
2017-05-22 11:43:38 +0530

    #leetcode #594. Longest Harmonious Subsequence

--- leetcode/src/LongestHarmoniousSubsequence.java
+++ leetcode/src/LongestHarmoniousSubsequence.java
@@ -1,32 +1,19 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /** https://leetcode.com/problems/longest-harmonious-subsequence/ Created by pankaj on 21/05/17. */
 class LongestHarmoniousSubsequence {
   int findLHS(final int[] nums) {
-    if (nums == null || nums.length == 0) {
-      return 0;
-    }
-    Arrays.sort(nums);
-    int curr = nums[0];
-    int currCnt = 1;
-    Integer prev = null;
-    int prevCnt = 0;
     int max = 0;
-    for (int i = 1; i < nums.length; i++) {
-      if (nums[i] == nums[i - 1]) {
-        currCnt++;
-      } else {
-        if (prev != null && curr == prev + 1) {
-          max = Math.max(max, prevCnt + currCnt);
-        }
-        prev = curr;
-        prevCnt = currCnt;
-        curr = nums[i];
-        currCnt = 1;
+    Map<Integer, Integer> cnt = new HashMap<>();
+    for (int num : nums) {
+      cnt.put(num, cnt.getOrDefault(num, 0) + 1);
+      if (cnt.containsKey(num - 1)) {
+        max = Math.max(max, cnt.get(num) + cnt.get(num - 1));
       }
-    }
-    if (prev != null && curr == prev + 1) {
-      max = Math.max(max, prevCnt + currCnt);
+      if (cnt.containsKey(num + 1)) {
+        max = Math.max(max, cnt.get(num) + cnt.get(num + 1));
+      }
     }
     return max;
   }
