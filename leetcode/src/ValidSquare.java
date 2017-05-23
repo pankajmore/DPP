1ee624c9e6f7bf175b8c0a16e1fa6306c85df14e
Pankaj More <pankajmore@gmail.com>
2017-05-23 23:07:34 +0530

    #leetcode #593. Valid Square

--- leetcode/src/ValidSquare.java
+++ leetcode/src/ValidSquare.java
@@ -1,17 +1,14 @@
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /** https://leetcode.com/problems/valid-square/ Created by pankaj on 21/05/17. */
 class ValidSquare {
   boolean validSquare(final int[] p1, final int[] p2, final int[] p3, final int[] p4) {
-    List<Integer> ds =
-        Arrays.asList(d(p1, p2), d(p1, p3), d(p1, p4), d(p2, p3), d(p2, p4), d(p3, p4));
-    Collections.sort(ds);
-    return !ds.get(0).equals(0)
-        && ds.get(0).equals(ds.get(1))
-        && ds.get(1).equals(ds.get(2))
-        && ds.get(2).equals(ds.get(3));
+    Set<Integer> ds =
+        new HashSet<>(
+            Arrays.asList(d(p1, p2), d(p1, p3), d(p1, p4), d(p2, p3), d(p2, p4), d(p3, p4)));
+    return !ds.contains(0) && ds.size() == 2;
   }
 
   private int d(final int[] p, final int[] q) {
