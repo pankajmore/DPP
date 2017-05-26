ef67419514f3f2e1b1dcf84af27f59b04cf3fd67
Pankaj More <pankajmore@gmail.com>
2017-05-26 22:48:47 +0530

    #leetcode #449. Serialize and Deserialize BST

--- leetcode/src/SerializeAndDeserializeBST.java
+++ leetcode/src/SerializeAndDeserializeBST.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 import java.util.stream.Collectors;
 
 /** https://leetcode.com/problems/serialize-and-deserialize-bst/ Created by pankaj on 25/05/17. */
@@ -19,7 +20,8 @@ class SerializeAndDeserializeBST {
     }
     List<String> ls = Arrays.asList(data.substring(1, data.length() - 1).split(", "));
     List<Integer> vals = ls.stream().map(Integer::parseInt).collect(Collectors.toList());
-    return buildFromPreOrder(vals);
+    List<Integer> split = nextGreater(vals);
+    return buildFromPreOrder(0, vals.size() - 1, vals, split);
   }
 
   private void preOrder(final TreeNode root, final List<Integer> preOrder) {
@@ -30,19 +32,34 @@ class SerializeAndDeserializeBST {
     }
   }
 
-  private TreeNode buildFromPreOrder(final List<Integer> preOrder) {
-    if (preOrder.isEmpty()) {
+  private TreeNode buildFromPreOrder(
+      final int lo, final int hi, final List<Integer> preOrder, final List<Integer> split) {
+    if (lo > hi) {
       return null;
     } else {
-      TreeNode root = new TreeNode(preOrder.get(0));
-      int leftEnd = 1;
-      while (leftEnd < preOrder.size() && preOrder.get(leftEnd) <= root.val) {
-        leftEnd++;
-      }
-      root.left = buildFromPreOrder(preOrder.subList(1, leftEnd));
-      root.right = buildFromPreOrder(preOrder.subList(leftEnd, preOrder.size()));
+      TreeNode root = new TreeNode(preOrder.get(lo));
+      int idx = split.get(lo);
+      root.left = buildFromPreOrder(lo + 1, idx - 1, preOrder, split);
+      root.right = buildFromPreOrder(idx, hi, preOrder, split);
       return root;
     }
+  }
+
+  private List<Integer> nextGreater(final List<Integer> ls) {
+    List<Integer> nextGreater = new ArrayList<>();
+    for (int i = 0; i < ls.size(); i++) {
+      nextGreater.add(ls.size());
+    }
+    Stack<Integer> stack = new Stack<>();
+    stack.push(0);
+    for (int i = 1; i < ls.size(); i++) {
+      while (!stack.isEmpty() && ls.get(stack.peek()) < ls.get(i)) {
+        nextGreater.set(stack.peek(), i);
+        stack.pop();
+      }
+      stack.push(i);
+    }
+    return nextGreater;
   }
 
   class TreeNode {
