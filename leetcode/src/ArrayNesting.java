e8a914985af3bd0d47db8a7515c0dba0d94875c6
Pankaj More <pankajmore@gmail.com>
2017-05-29 21:11:50 +0530

    #leetcode #565. Array Nesting

--- leetcode/src/ArrayNesting.java
+++ leetcode/src/ArrayNesting.java
@@ -1,18 +1,18 @@
-import java.util.HashSet;
-
 /** https://leetcode.com/problems/array-nesting/ Created by pankaj on 29/05/17. */
 class ArrayNesting {
   int arrayNesting(final int[] nums) {
     int max = 0;
-    HashSet<Integer> visited = new HashSet<>();
-    for (int num : nums) {
-      int curr = num, cnt = 0;
-      while (!visited.contains(curr)) {
-        visited.add(curr);
-        curr = nums[curr];
-        cnt++;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != Integer.MAX_VALUE) {
+        int curr = nums[i], cnt = 0;
+        while (nums[curr] != Integer.MAX_VALUE) {
+          int tmp = curr;
+          curr = nums[curr];
+          cnt++;
+          nums[tmp] = Integer.MAX_VALUE;
+        }
+        max = Math.max(max, cnt);
       }
-      max = Math.max(max, cnt);
     }
     return max;
   }
