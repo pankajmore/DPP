a9db3f9a6c839da8adbdb047ab016e3c11d33d12
Pankaj More <pankajmore@gmail.com>
2017-06-02 22:14:02 +0530

    #leetcode #419. Battleships in a Board

--- leetcode/src/BattleshipsInABoard.java
+++ leetcode/src/BattleshipsInABoard.java
@@ -5,8 +5,8 @@ class BattleshipsInABoard {
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[i].length; j++) {
         if (board[i][j] == 'X'
-            && (i == 0 || board[i - 1][j] != 'X')
-            && (j == 0 || board[i][j - 1] != 'X')) {
+            && (i == 0 || board[i - 1][j] == '.')
+            && (j == 0 || board[i][j - 1] == '.')) {
           cnt++;
         }
       }
