c3ac82dbc13df4a3bf9e0946096884d123daab42
Pankaj More <pankajmore@gmail.com>
2017-06-03 23:31:21 +0530

    #leetcode #463. Island Perimeter

--- leetcode/src/IslandPerimeter.java
+++ leetcode/src/IslandPerimeter.java
@@ -1,21 +1,25 @@
-/**
- * https://leetcode.com/problems/island-perimeter/
- * Created by pankaj on 20/11/16.
- */
-public class IslandPerimeter {
-    public int islandPerimeter(int[][] grid) {
-        int M = grid.length, N = grid[0].length;
-        int p = 0;
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                if (grid[i][j] == 1) {
-                    if (i == 0 || grid[i - 1][j] == 0) p += 1;
-                    if (j == 0 || grid[i][j - 1] == 0) p += 1;
-                    if (i == M - 1 || grid[i + 1][j] == 0) p += 1;
-                    if (j == N - 1 || grid[i][j + 1] == 0) p += 1;
-                }
-            }
+/** https://leetcode.com/problems/island-perimeter/ Created by pankaj on 20/11/16. */
+class IslandPerimeter {
+  int islandPerimeter(final int[][] grid) {
+    int p = 0;
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[i].length; j++) {
+        if (grid[i][j] == 1) {
+          if (i == 0 || grid[i - 1][j] == 0) {
+              p += 1;
+          }
+          if (j == 0 || grid[i][j - 1] == 0) {
+              p += 1;
+          }
+          if (i == grid.length - 1 || grid[i + 1][j] == 0) {
+              p += 1;
+          }
+          if (j == grid[i].length - 1 || grid[i][j + 1] == 0) {
+              p += 1;
+          }
         }
-        return p;
+      }
     }
+    return p;
+  }
 }
