949f66ad7a71d96bc2990ba7fbbf170d91a5fd83
Pankaj More <pankajmore@gmail.com>
2017-07-07 21:55:40 +0530

    #leetcode #406. Queue Reconstruction by Height

--- geeksforgeeks/src/com/dpp/geeksforgeeks/QueueReconstructionByHeight.java
+++ geeksforgeeks/src/com/dpp/geeksforgeeks/QueueReconstructionByHeight.java
@@ -1,6 +1,8 @@
 package com.dpp.geeksforgeeks;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /** https://leetcode.com/problems/queue-reconstruction-by-height/ Created by pankaj on 07/07/17. */
 class QueueReconstructionByHeight {
@@ -34,5 +36,27 @@ class QueueReconstructionByHeight {
       actualOrder[idx] = p;
     }
     return actualOrder;
+  }
+
+  int[][] reconstructQueue1(final int[][] people) {
+    int n = people.length;
+    Arrays.sort(
+        people,
+        (x, y) -> {
+          if (x[0] == y[0]) {
+            return x[1] - y[1];
+          } else {
+            return y[0] - x[0];
+          }
+        });
+    List<int[]> order = new ArrayList<>();
+    for (int[] p : people) {
+      order.add(p[1], p);
+    }
+    int[][] res = new int[n][2];
+    for (int i = 0; i < n; i++) {
+      res[i] = order.get(i);
+    }
+    return res;
   }
 }
