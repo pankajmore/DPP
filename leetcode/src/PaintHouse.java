3df7d4f7186e7af7ea8b95dd9de0f658532d1ac9
Pankaj More <pankajmore@gmail.com>
2017-07-11 12:17:05 +0530

    #leetcode #http://www.lintcode.com/en/problem/paint-house/

---
+++ leetcode/src/PaintHouse.java
@@ -0,0 +1,25 @@
+/** http://www.lintcode.com/en/problem/paint-house/ Created by pankaj on 11/07/17. */
+class PaintHouse {
+  /**
+   * @param costs n x 3 cost matrix
+   * @return an integer, the minimum cost to paint all houses
+   */
+  int minCost(final int[][] costs) {
+    if (costs == null || costs.length == 0) {
+      return 0;
+    }
+    int n = costs.length;
+    int[] dp = new int[3];
+    dp[0] = costs[0][0];
+    dp[1] = costs[0][1];
+    dp[2] = costs[0][2];
+    for (int i = 1; i < n; i++) {
+      int[] next = new int[3];
+      next[0] = costs[i][0] + Math.min(dp[1], dp[2]);
+      next[1] = costs[i][1] + Math.min(dp[0], dp[2]);
+      next[2] = costs[i][2] + Math.min(dp[0], dp[1]);
+      dp = next;
+    }
+    return Math.min(dp[0], Math.min(dp[1], dp[2]));
+  }
+}
