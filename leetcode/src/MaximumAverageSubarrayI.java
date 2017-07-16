d1e8b097d16f2b3dead624f10875117dadbe364a
Pankaj More <pankajmore@gmail.com>
2017-07-16 23:55:02 +0530

    #leetcode #643. Maximum Average Subarray I

--- leetcode/src/MaximumAverageSubarrayI.java
+++ leetcode/src/MaximumAverageSubarrayI.java
@@ -1,16 +1,16 @@
 /** https://leetcode.com/problems/maximum-average-subarray-i/ Created by pankaj on 16/07/17. */
 class MaximumAverageSubarrayI {
-  double findMaxAverage(final int[] nums, final int k) {
+  public double findMaxAverage(final int[] nums, final int k) {
     double max = Double.NEGATIVE_INFINITY;
     double sum = 0;
     for (int i = 0; i < k; i++) {
-      sum += nums[i];
+      sum += (double) nums[i] / k;
     }
     max = Math.max(max, sum);
     for (int i = k; i < nums.length; i++) {
-      sum = sum - nums[i - k] + nums[i];
+      sum = sum - (double) nums[i - k] / k + (double) nums[i] / k;
       max = Math.max(max, sum);
     }
-    return max / k;
+    return max;
   }
 }
