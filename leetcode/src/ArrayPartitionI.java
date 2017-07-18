03b24c633740a360130c44d71fbe1a7c2e42e1a9
Pankaj More <pankajmore@gmail.com>
2017-07-18 23:35:55 +0530

    format

--- leetcode/src/ArrayPartitionI.java
+++ leetcode/src/ArrayPartitionI.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
 
 /** https://leetcode.com/problems/array-partition-i/ Created by pankaj on 23/04/17. */
-public class ArrayPartitionI {
-  public int arrayPairSum(int[] nums) {
+class ArrayPartitionI {
+  int arrayPairSum(final int[] nums) {
     int sum = 0;
     Arrays.sort(nums);
     for (int i = 0; i < nums.length; i = i + 2) {
