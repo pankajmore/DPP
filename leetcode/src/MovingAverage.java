1bc1d246a55915036742f481ae6119080cb7f40d
Pankaj More <pankajmore@gmail.com>
2017-07-28 22:48:51 +0530

    #leetcode #Moving Average from Data Stream

---
+++ leetcode/src/MovingAverage.java
@@ -0,0 +1,24 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/** https://leetcode.com/problems/moving-average-from-data-stream Created by pankaj on 28/07/17. */
+class MovingAverage {
+  private Deque<Integer> queue;
+  private int size;
+  private double sum;
+
+  MovingAverage(final int n) {
+    this.size = n;
+    sum = 0.0;
+    queue = new ArrayDeque<>();
+  }
+
+  double next(final int val) {
+    if (queue.size() == size) {
+      sum -= queue.remove();
+    }
+    queue.add(val);
+    sum += val;
+    return sum / queue.size();
+  }
+}
