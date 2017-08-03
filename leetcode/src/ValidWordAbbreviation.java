71453f8f4775013762c12b6688a38d59177a8b16
Pankaj More <pankajmore@gmail.com>
2017-08-03 23:53:50 +0530

    #leetcode #Valid Word Abbreviation

---
+++ leetcode/src/ValidWordAbbreviation.java
@@ -0,0 +1,25 @@
+/** https://leetcode.com/problems/valid-word-abbreviation Created by pankaj on 03/08/17. */
+class ValidWordAbbreviation {
+  boolean isValidAbbreviation(final String word, final String abbr) {
+    int idx = 0;
+    int num = 0;
+    for (int i = 0; i < abbr.length(); i++) {
+      char c = abbr.charAt(i);
+      if (Character.isDigit(c)) {
+        num = num * 10 + (c - '0');
+        if (num < 0) {
+          return false;
+        }
+      } else {
+        idx = idx + num;
+        num = 0;
+        if (idx >= word.length() || c != word.charAt(idx)) {
+          return false;
+        }
+        idx++;
+      }
+    }
+    idx = idx + num;
+    return idx == word.length();
+  }
+}
