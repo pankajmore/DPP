48e9d7eed8af869802226f312bab221bb602acf7
Pankaj More <pankajmore@gmail.com>
2017-08-27 20:54:58 +0530

    style-guide

--- leetcode/src/PlusOne.java
+++ leetcode/src/PlusOne.java
@@ -1,8 +1,8 @@
 /** https://leetcode.com/problems/plus-one/ Created by pankaj on 26/12/16. */
 class PlusOne {
   int[] plusOne(final int[] digits) {
-    int N = digits.length;
-    for (int i = N - 1; i >= 0; i--) {
+    int len = digits.length;
+    for (int i = len - 1; i >= 0; i--) {
       if (digits[i] < 9) {
         digits[i]++;
         return digits;
@@ -10,7 +10,7 @@ class PlusOne {
         digits[i] = 0;
       }
     }
-    int[] newNumber = new int[N + 1];
+    int[] newNumber = new int[len + 1];
     newNumber[0] = 1;
     return newNumber;
   }
