579a6a2702051871e58c202ad9ea042b1e1b98a3
Pankaj More <pankajmore@gmail.com>
2017-09-06 23:42:48 +0530

    < 80 chars

--- leetcode/src/ArithmeticSlices.java
+++ leetcode/src/ArithmeticSlices.java
@@ -1,13 +1,16 @@
-/** https://leetcode.com/problems/arithmetic-slices/ Created by pankaj on 20/02/17. */
+/**
+ * https://leetcode.com/problems/arithmetic-slices/
+ * Created by pankaj on 20/02/17.
+ */
 class ArithmeticSlices {
   int numberOfArithmeticSlices(final int[] nums) {
     int len = nums.length;
     int cnt = 0, x = 0;
     for (int i = 2; i < len; i++) {
       if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
-          x++;
+        x++;
       } else {
-          x = 0;
+        x = 0;
       }
       cnt += x;
     }
