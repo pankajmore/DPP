6c40c71201aa1fe95254b9025177591568b2afe8
Pankaj <pankajmore@gmail.com>
2018-01-07 21:05:59 -0500

    #dailycodingproblem #20180107

---
+++ dailycodingproblem/src/DCP20180107.java
@@ -0,0 +1,40 @@
+// Given a stack of N elements, interleave the first half of the stack with the second half reversed using only one other queue. This should be done in-place.
+
+// Recall that you can only push or pop from a stack, and enqueue or dequeue from a queue.
+
+// For example, if the stack is [1, 2, 3, 4, 5], it should become [1, 5, 2, 4, 3]. If the stack is [1, 2, 3, 4], it should become [1, 4, 2, 3].
+
+// [1, 2, 3, 4, 5, 6] -> [1, 6, 2, 5, 3, 4]
+
+// [1, 6, 2, 5, 3, 4]
+// [1, 6, 2, 5, 4, 3]
+// [1, 6, 2, 3, 4, 5]
+// [1, 6, 5, 4, 3, 2] 
+// [1, 2, 3, 4, 5, 6]
+
+import java.util.*;
+public class DCP20180107 {
+    public static void interleave(Stack<Integer> s, Queue<Integer> q) {
+        int len = s.size();
+        for (int i = len - 1; i >= 2; i--) {
+            rotate(s, q, i);
+        }
+    }
+
+    private static void rotate(Stack<Integer> s, Queue<Integer> q, int k) {
+        for (int i = 0; i < k; i++) {
+            q.add(s.pop());
+        }
+        for (int i = 0; i < k; i++) {
+            s.push(q.remove());
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.addAll(Arrays.asList(1, 2, 3, 4, 5));
+        Queue<Integer> q = new LinkedList<>();
+        interleave(s, q);
+        System.out.println(s.toString());
+    }
+}
