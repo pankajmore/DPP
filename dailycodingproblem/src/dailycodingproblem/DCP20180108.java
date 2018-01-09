63dc225a08c92d15d175cc5b532b1f8ebfdcb89e
Pankaj <pankajmore@gmail.com>
2018-01-08 22:26:38 -0500

    #dailycodingproblem #20180108

---
+++ dailycodingproblem/src/dailycodingproblem/DCP20180108.java
@@ -0,0 +1,25 @@
+package dailycodingproblem;
+
+/**
+ * Given an array of integers, return a new array such that each element at index i of the new array is the product of
+ * all the numbers in the original array except the one at i. Solve it without using division and in O(n) time.
+ * <p>
+ * For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24].
+ * If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+ */
+class DCP20180108 {
+    static int[] productExceptSelf(int[] in) {
+        int[] out = new int[in.length];
+        int left = 1;
+        for (int i = 0; i < in.length; i++) {
+            out[i] = left;
+            left *= in[i];
+        }
+        int right = 1;
+        for (int i = in.length - 1; i >= 0; i--) {
+            out[i] *= right;
+            right *= in[i];
+        }
+        return out;
+    }
+}
---
+++ dailycodingproblem/test/dailycodingproblem/DCP20180108Test.java
@@ -0,0 +1,20 @@
+package dailycodingproblem;
+
+import org.junit.jupiter.api.Test;
+
+import static dailycodingproblem.DCP20180108.productExceptSelf;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class DCP20180108Test {
+
+    @Test
+    void productExceptSelfTest() {
+        assertArrayEquals(new int[]{}, productExceptSelf(new int[]{}));
+        assertArrayEquals(new int[]{1}, productExceptSelf(new int[]{0}));
+        assertArrayEquals(new int[]{-2, -3}, productExceptSelf(new int[]{-3, -2}));
+        assertArrayEquals(new int[]{0, 1}, productExceptSelf(new int[]{1, 0}));
+        assertArrayEquals(new int[]{0, 6, 0}, productExceptSelf(new int[]{2, 0, 3}));
+        assertArrayEquals(new int[]{2, 3, 6}, productExceptSelf(new int[]{3, 2, 1}));
+        assertArrayEquals(new int[]{120, 60, 40, 30, 24}, productExceptSelf(new int[]{1, 2, 3, 4, 5}));
+    }
+}
