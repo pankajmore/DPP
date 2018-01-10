1c9d0684d81fde54fe6b07ec5e04a0493cb0da7f
Pankaj <pankajmore@gmail.com>
2018-01-09 22:51:27 -0500

    a test and re-format

---
+++ dailycodingproblem/test/dailycodingproblem/DCP20180107Test.java
@@ -0,0 +1,31 @@
+package dailycodingproblem;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static dailycodingproblem.DCP20180107.interleave;
+
+class DCP20180107Test {
+
+    @Test
+    void interleaveTest1() {
+        Stack<Integer> s = new Stack<>();
+        s.addAll(Arrays.asList(1, 2, 3, 4));
+        interleave(s, new LinkedList<>());
+        List<Integer> actual = new ArrayList<>();
+        actual.addAll(s);
+        Assertions.assertEquals(Arrays.asList(1, 4, 2, 3), s);
+    }
+
+    @Test
+    void interleaveTest2() {
+        Stack<Integer> s = new Stack<>();
+        s.addAll(Arrays.asList(1, 2, 3, 4, 5));
+        interleave(s, new LinkedList<>());
+        List<Integer> actual = new ArrayList<>();
+        actual.addAll(s);
+        Assertions.assertEquals(Arrays.asList(1, 5, 2, 4, 3), s);
+    }
+}
