6f7ed18977b7044fbc6583762cd208dbd8f13eca
Pankaj <pankajmore@gmail.com>
2018-01-15 22:55:55 -0500

    #dailycodingproblem #20180115

---
+++ dailycodingproblem/src/dailycodingproblem/DCP20180115.java
@@ -0,0 +1,22 @@
+package dailycodingproblem;
+
+/**
+ * Given a list of integers, write a function that returns the largest sum of non-adjacent numbers.
+ * For example, [2, 4, 6, 8] should return 12, since we pick 4 and 8. [5, 1, 1, 5] should return 10,
+ * since we pick 5 and 5.
+ */
+class DCP20180115 {
+    static int largestSumNonAdjacent(int[] in) {
+        if (in == null || in.length == 0) {
+            return 0;
+        }
+        int a = 0;
+        int b = in[0];
+        for (int i = 1; i < in.length; i++) {
+            int tmp = Math.max(in[i] + a, b);
+            a = b;
+            b = tmp;
+        }
+        return b;
+    }
+}
---
+++ dailycodingproblem/test/dailycodingproblem/DCP20180115Test.java
@@ -0,0 +1,22 @@
+package dailycodingproblem;
+
+import org.junit.jupiter.api.Test;
+
+import static dailycodingproblem.DCP20180115.largestSumNonAdjacent;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DCP20180115Test {
+
+    @Test
+    void largestSumNonAdjacentTest() {
+        assertEquals(0, largestSumNonAdjacent(new int[]{}));
+        assertEquals(1, largestSumNonAdjacent(new int[]{1}));
+        assertEquals(2, largestSumNonAdjacent(new int[]{1, 2}));
+        assertEquals(2, largestSumNonAdjacent(new int[]{2, 1}));
+        assertEquals(4, largestSumNonAdjacent(new int[]{1, 2, 3}));
+        assertEquals(3, largestSumNonAdjacent(new int[]{1, 3, 1}));
+        assertEquals(6, largestSumNonAdjacent(new int[]{4, 2, 1, 2}));
+        assertEquals(12, largestSumNonAdjacent(new int[]{2, 4, 6, 8}));
+        assertEquals(10, largestSumNonAdjacent(new int[]{5, 1, 1, 5}));
+    }
+}
