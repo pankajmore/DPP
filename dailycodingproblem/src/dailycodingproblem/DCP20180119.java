1f62af95f0dc3d9819f3d37d38a08b81427f6a17
Pankaj <pankajmore@gmail.com>
2018-01-19 23:51:57 -0500

    #dailycodingproblem #20180119

---
+++ dailycodingproblem/src/dailycodingproblem/DCP20180119.java
@@ -0,0 +1,30 @@
+package dailycodingproblem;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given an integer k and a string s, find the length of the longest substring that contains at most
+ * k distinct characters. For example, given s = "abcba" and k = 2, the longest substring with k
+ * distinct characters is "bcb", so your function should return 3.
+ */
+class DCP20180119 {
+    static int longestSubstringKDistinct(String s, int k) {
+        if (s.length() <= k) {
+            return s.length();
+        }
+        int best = k;
+        for (int i = 0; i < s.length(); i++) {
+            Set<Character> set = new HashSet<>();
+            for (int j = i; j < s.length(); j++) {
+                set.add(s.charAt(j));
+                if (set.size() > k) {
+                    break;
+                } else {
+                    best = Math.max(best, j - i + 1);
+                }
+            }
+        }
+        return best;
+    }
+}
---
+++ dailycodingproblem/test/dailycodingproblem/DCP20180119Test.java
@@ -0,0 +1,19 @@
+package dailycodingproblem;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static dailycodingproblem.DCP20180119.longestSubstringKDistinct;
+
+class DCP20180119Test {
+
+    @Test
+    void longestSubstringKDistinctTest() {
+        Assertions.assertEquals(1, longestSubstringKDistinct("abc", 1));
+        Assertions.assertEquals(2, longestSubstringKDistinct("aac", 1));
+        Assertions.assertEquals(2, longestSubstringKDistinct("caa", 1));
+        Assertions.assertEquals(3, longestSubstringKDistinct("aba", 2));
+        Assertions.assertEquals(4, longestSubstringKDistinct("aabbc", 2));
+        Assertions.assertEquals(4, longestSubstringKDistinct("ababc", 2));
+    }
+}
