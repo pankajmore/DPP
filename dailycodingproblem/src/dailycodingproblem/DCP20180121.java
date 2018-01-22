e14afa62d9960132a00d43ed8979193b1be72bce
Pankaj <pankajmore@gmail.com>
2018-01-21 22:37:54 -0500

    #dailycodingproblem #20180121

---
+++ dailycodingproblem/src/dailycodingproblem/DCP20180121.java
@@ -0,0 +1,24 @@
+package dailycodingproblem;
+
+import java.util.Iterator;
+import java.util.Random;
+
+/**
+ * Given a stream of elements too large to store in memory, pick a random element from the stream
+ * with uniform probability.
+ */
+class DCP20180121 {
+    static Integer reservoirSampling(Iterator<Integer> it) {
+        Integer e = null;
+        int n = 0;
+        Random rng = new Random();
+        while (it.hasNext()) {
+            Integer i = it.next();
+            n++;
+            if (rng.nextInt(n) == 0) {
+                e = i;
+            }
+        }
+        return e;
+    }
+}
