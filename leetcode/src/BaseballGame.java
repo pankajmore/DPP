54ee9c87377150c9bf01722fe4ec16c971446be0
Pankaj <pankajmore@gmail.com>
2018-02-16 23:57:46 -0500

    reformat

--- leetcode/src/BaseballGame.java
+++ leetcode/src/BaseballGame.java
@@ -1,30 +1,28 @@
 import java.util.Stack;
 
-/**
- * https://leetcode.com/problems/baseball-game/
- */
+/** https://leetcode.com/problems/baseball-game/ */
 class BaseballGame {
-    int calPoints(String[] ops) {
-        int sum = 0;
-        Stack<Integer> scores = new Stack<>();
-        for (String op : ops) {
-            if (op.equals("+")) {
-                int a = scores.pop();
-                int b = scores.peek();
-                scores.push(a);
-                sum += a + b;
-                scores.push(a + b);
-            } else if (op.equals("C")) {
-                sum -= scores.pop();
-            } else if (op.equals("D")) {
-                sum += scores.peek() * 2;
-                scores.push(scores.peek() * 2);
-            } else {
-                int score = Integer.parseInt(op);
-                scores.push(score);
-                sum += score;
-            }
-        }
-        return sum;
+  int calPoints(String[] ops) {
+    int sum = 0;
+    Stack<Integer> scores = new Stack<>();
+    for (String op : ops) {
+      if (op.equals("+")) {
+        int a = scores.pop();
+        int b = scores.peek();
+        scores.push(a);
+        sum += a + b;
+        scores.push(a + b);
+      } else if (op.equals("C")) {
+        sum -= scores.pop();
+      } else if (op.equals("D")) {
+        sum += scores.peek() * 2;
+        scores.push(scores.peek() * 2);
+      } else {
+        int score = Integer.parseInt(op);
+        scores.push(score);
+        sum += score;
+      }
     }
+    return sum;
+  }
 }
