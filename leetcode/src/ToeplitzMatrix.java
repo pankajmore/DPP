552410e296c44d80b033795c5268c4d9e41461fb
Pankaj <pankajmore@gmail.com>
2018-02-28 23:17:46 -0500

    reformat

--- leetcode/src/ToeplitzMatrix.java
+++ leetcode/src/ToeplitzMatrix.java
@@ -1,20 +1,18 @@
-/**
- * https://leetcode.com/problems/toeplitz-matrix/
- */
+/** https://leetcode.com/problems/toeplitz-matrix/ */
 class ToeplitzMatrix {
-    boolean isToeplitzMatrix(int[][] matrix) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return true;
+  boolean isToeplitzMatrix(int[][] matrix) {
+    if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+      return true;
+    }
+    int m = matrix.length;
+    int n = matrix[0].length;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i > 0 && j > 0 && matrix[i - 1][j - 1] != matrix[i][j]) {
+          return false;
         }
-        int m = matrix.length;
-        int n = matrix[0].length;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i > 0 && j > 0 && matrix[i - 1][j - 1] != matrix[i][j]) {
-                    return false;
-                }
-            }
-        }
-        return true;
+      }
+    }
+    return true;
   }
 }
