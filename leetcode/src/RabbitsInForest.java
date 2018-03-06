244679e169317de69f5064d939b59668f0356df4
Pankaj <pankajmore@gmail.com>
2018-03-05 23:18:28 -0500

    format

--- leetcode/src/RabbitsInForest.java
+++ leetcode/src/RabbitsInForest.java
@@ -7,6 +7,9 @@ public class RabbitsInForest {
   public int numRabbits(int[] answers) {
     Map<Integer, Integer> cnt = new HashMap<>();
     Arrays.stream(answers).forEach(ans -> cnt.put(ans, cnt.getOrDefault(ans, 0) + 1));
-    return cnt.keySet().stream().mapToInt(k -> Math.floorMod(-cnt.get(k), k + 1) + cnt.get(k)).sum();
+    return cnt.keySet()
+        .stream()
+        .mapToInt(k -> Math.floorMod(-cnt.get(k), k + 1) + cnt.get(k))
+        .sum();
   }
 }
