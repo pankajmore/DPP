c4c0ecd84f5ee714db0b8c0ce4f875c52d35c962
Pankaj <pankajmore@gmail.com>
2018-03-09 23:59:24 -0500

    format

--- dailycodingproblem/src/dailycodingproblem/DCP42.java
+++ dailycodingproblem/src/dailycodingproblem/DCP42.java
@@ -12,35 +12,35 @@ import java.util.List;
  * positive.
  */
 class DCP42 {
-    static List<Integer> subsetSum(List<Integer> list, int k) {
-        int len = list.size();
-        boolean[][] subsetSum = new boolean[len + 1][k + 1];
-        subsetSum[0][0] = true;
-        for(int i = 1; i <= len; i++) {
-            subsetSum[i][0] = true;
-            for(int j = 1; j <= k; j++) {
-                subsetSum[i][j] = subsetSum[i - 1][j];
-                if(list.get(i - 1) <= j) {
-                    subsetSum[i][j] |= subsetSum[i - 1][j - list.get(i - 1)];
-                }
-            }
+  static List<Integer> subsetSum(List<Integer> list, int k) {
+    int len = list.size();
+    boolean[][] subsetSum = new boolean[len + 1][k + 1];
+    subsetSum[0][0] = true;
+    for (int i = 1; i <= len; i++) {
+      subsetSum[i][0] = true;
+      for (int j = 1; j <= k; j++) {
+        subsetSum[i][j] = subsetSum[i - 1][j];
+        if (list.get(i - 1) <= j) {
+          subsetSum[i][j] |= subsetSum[i - 1][j - list.get(i - 1)];
         }
-        if(subsetSum[len][k]) {
-            List<Integer> subset = new ArrayList<>();
-            for(int i = len, j = k; i > 0 && j > 0;) {
-                if(subsetSum[i - 1][j]) {
-                    i--;
-                } else {
-                    assert subsetSum[i - 1][j - list.get(i - 1)];
-                    subset.add(list.get(i - 1));
-                    j -= list.get(i - 1);
-                    i--;
-                }
-            }
-            Collections.reverse(subset);
-            return subset;
-        } else {
-            return null;
-        }
+      }
     }
+    if (subsetSum[len][k]) {
+      List<Integer> subset = new ArrayList<>();
+      for (int i = len, j = k; i > 0 && j > 0; ) {
+        if (subsetSum[i - 1][j]) {
+          i--;
+        } else {
+          assert subsetSum[i - 1][j - list.get(i - 1)];
+          subset.add(list.get(i - 1));
+          j -= list.get(i - 1);
+          i--;
+        }
+      }
+      Collections.reverse(subset);
+      return subset;
+    } else {
+      return null;
+    }
+  }
 }
