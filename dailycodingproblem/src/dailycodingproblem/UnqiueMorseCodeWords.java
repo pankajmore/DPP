9e41ccd570e7f92fb6555814800ea13d7a995c8f
Pankaj <pankajmore@gmail.com>
2018-03-25 21:58:20 -0400

    #leetcode #https://leetcode.com/problems/unique-morse-code-words/description/

---
+++ dailycodingproblem/src/dailycodingproblem/UnqiueMorseCodeWords.java
@@ -0,0 +1,25 @@
+package dailycodingproblem;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/** https://leetcode.com/problems/unique-morse-code-words/description/ */
+class UnqiueMorseCodeWords {
+  private static String[] MORSE =
+      new String[] {
+        ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
+        "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
+      };
+
+  int uniqueMorseRepresentations(String[] words) {
+    Set<String> morses = new HashSet<>();
+    for (String word : words) {
+      StringBuilder sb = new StringBuilder();
+      for (char c : word.toCharArray()) {
+        sb.append(MORSE[c - 'a']);
+      }
+      morses.add(sb.toString());
+    }
+    return morses.size();
+  }
+}
