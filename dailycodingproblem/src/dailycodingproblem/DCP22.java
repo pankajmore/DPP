6e8afb9456abb89fe87f659a4df9e8a9b7cf713f
Pankaj <pankajmore@gmail.com>
2018-03-28 22:05:23 -0400

    reformat

--- dailycodingproblem/src/dailycodingproblem/DCP22.java
+++ dailycodingproblem/src/dailycodingproblem/DCP22.java
@@ -8,30 +8,30 @@ import java.util.*;
  * there is no possible reconstruction, then return null.
  */
 class DCP22 {
-    static List<String> wordBreak(String s, List<String> wordDict) {
-        Set<String> set = new HashSet<>(wordDict);
-        int len = s.length();
-        boolean[] valid = new boolean[len + 1];
-        int[] prev = new int[len + 1];
-        valid[0] = true;
-        prev[0] = -1;
-        for (int i = 1; i <= len; i++) {
-            for (int j = 0; j < i; j++) {
-                if (valid[j] && set.contains(s.substring(j, i))) {
-                    valid[i] = true;
-                    prev[i] = j;
-                    break;
-                }
-            }
+  static List<String> wordBreak(String s, List<String> wordDict) {
+    Set<String> set = new HashSet<>(wordDict);
+    int len = s.length();
+    boolean[] valid = new boolean[len + 1];
+    int[] prev = new int[len + 1];
+    valid[0] = true;
+    prev[0] = -1;
+    for (int i = 1; i <= len; i++) {
+      for (int j = 0; j < i; j++) {
+        if (valid[j] && set.contains(s.substring(j, i))) {
+          valid[i] = true;
+          prev[i] = j;
+          break;
         }
-        if (!valid[len]) {
-            return null;
-        }
-        List<String> words = new ArrayList<>();
-        for (int i = prev[len], j = len; i != -1; j = i, i = prev[i]) {
-            words.add(s.substring(i, j));
-        }
-        Collections.reverse(words);
-        return words;
+      }
     }
+    if (!valid[len]) {
+      return null;
+    }
+    List<String> words = new ArrayList<>();
+    for (int i = prev[len], j = len; i != -1; j = i, i = prev[i]) {
+      words.add(s.substring(i, j));
+    }
+    Collections.reverse(words);
+    return words;
+  }
 }
