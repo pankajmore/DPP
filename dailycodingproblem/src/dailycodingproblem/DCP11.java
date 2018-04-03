b10cf840b517f5795eb8747b8b969c3d13d598fb
Pankaj <pankajmore@gmail.com>
2018-04-02 21:34:13 -0400

    reformat

--- dailycodingproblem/src/dailycodingproblem/DCP11.java
+++ dailycodingproblem/src/dailycodingproblem/DCP11.java
@@ -10,64 +10,64 @@ import java.util.Set;
  * possible query strings, return all strings in the dictionary that have s as a prefix.
  */
 class DCP11 {
-    static Trie triefy(Set<String> dict) {
-        Trie t = new Trie();
-        for (String s : dict) {
-            t.add(s);
-        }
-        return t;
+  static Trie triefy(Set<String> dict) {
+    Trie t = new Trie();
+    for (String s : dict) {
+      t.add(s);
+    }
+    return t;
+  }
+
+  static Set<String> prefixSearch(String s, Trie dict) {
+    return dict.prefix(s).toList(s);
+  }
+
+  static class Trie {
+    Map<Character, Trie> children;
+
+    Trie() {
+      children = new HashMap<>();
     }
 
-    static Set<String> prefixSearch(String s, Trie dict) {
-        return dict.prefix(s).toList(s);
+    void add(String s) {
+      char[] chars = s.toCharArray();
+      Trie curr = this;
+      for (char c : chars) {
+        curr.children.putIfAbsent(c, new Trie());
+        curr = curr.children.get(c);
+      }
+      curr.children.put('\n', new Trie());
     }
 
-    static class Trie {
-        Map<Character, Trie> children;
-
-        Trie() {
-            children = new HashMap<>();
+    Trie prefix(String s) {
+      char[] chars = s.toCharArray();
+      Trie curr = this;
+      for (char c : chars) {
+        if (curr == null) {
+          return null;
+        } else {
+          curr = curr.children.get(c);
         }
-
-        void add(String s) {
-            char[] chars = s.toCharArray();
-            Trie curr = this;
-            for (char c : chars) {
-                curr.children.putIfAbsent(c, new Trie());
-                curr = curr.children.get(c);
-            }
-            curr.children.put('\n', new Trie());
-        }
-
-        Trie prefix(String s) {
-            char[] chars = s.toCharArray();
-            Trie curr = this;
-            for (char c : chars) {
-                if (curr == null) {
-                    return null;
-                } else {
-                    curr = curr.children.get(c);
-                }
-            }
-            return curr;
-        }
-
-        Set<String> toList(String prefix) {
-            Set<String> set = new HashSet<>();
-            StringBuilder sb = new StringBuilder().append(prefix);
-            toListInternal(set, sb);
-            return set;
-        }
-
-        void toListInternal(Set<String> list, StringBuilder sb) {
-            if (children.isEmpty()) {
-                list.add(sb.toString());
-            }
-            for (char c : children.keySet()) {
-                if (c != '\n') sb.append(c);
-                children.get(c).toListInternal(list, sb);
-                if (c != '\n') sb.deleteCharAt(sb.length() - 1);
-            }
-        }
+      }
+      return curr;
     }
+
+    Set<String> toList(String prefix) {
+      Set<String> set = new HashSet<>();
+      StringBuilder sb = new StringBuilder().append(prefix);
+      toListInternal(set, sb);
+      return set;
+    }
+
+    void toListInternal(Set<String> list, StringBuilder sb) {
+      if (children.isEmpty()) {
+        list.add(sb.toString());
+      }
+      for (char c : children.keySet()) {
+        if (c != '\n') sb.append(c);
+        children.get(c).toListInternal(list, sb);
+        if (c != '\n') sb.deleteCharAt(sb.length() - 1);
+      }
+    }
+  }
 }
