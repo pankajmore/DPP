45544adacfd38ea01d6835faf243f0c002844afd
Pankaj <pankajmore@gmail.com>
2018-04-05 22:43:22 -0400

    reformat

--- dailycodingproblem/src/dailycodingproblem/DCP02.java
+++ dailycodingproblem/src/dailycodingproblem/DCP02.java
@@ -9,18 +9,18 @@ package dailycodingproblem;
  * 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
  */
 class DCP02 {
-    static int[] productExceptSelf(int[] in) {
-        int[] out = new int[in.length];
-        int left = 1;
-        for (int i = 0; i < in.length; i++) {
-            out[i] = left;
-            left *= in[i];
-        }
-        int right = 1;
-        for (int i = in.length - 1; i >= 0; i--) {
-            out[i] *= right;
-            right *= in[i];
-        }
-        return out;
+  static int[] productExceptSelf(int[] in) {
+    int[] out = new int[in.length];
+    int left = 1;
+    for (int i = 0; i < in.length; i++) {
+      out[i] = left;
+      left *= in[i];
     }
+    int right = 1;
+    for (int i = in.length - 1; i >= 0; i--) {
+      out[i] *= right;
+      right *= in[i];
+    }
+    return out;
+  }
 }
