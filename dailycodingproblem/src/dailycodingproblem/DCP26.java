5634849ad3a778b4c8b02ef0fb08e349aa0bd260
Pankaj <pankajmore@gmail.com>
2018-04-07 22:40:21 -0400

    reformat

--- dailycodingproblem/src/dailycodingproblem/DCP26.java
+++ dailycodingproblem/src/dailycodingproblem/DCP26.java
@@ -8,53 +8,53 @@ import java.util.List;
  * guaranteed to be smaller than the length of the list.
  */
 class DCP26 {
-    /**
-     * @param head head of the linked list
-     * @param k    1 indexed,
-     */
-    static Node removeKthLast(Node head, int k) {
-        assert k > 0;
-        Node fakeHead = new Node(0);
-        fakeHead.next = head;
-        Node slow = fakeHead, fast = fakeHead;
-        for (int i = 0; i <= k; i++) {
-            fast = fast.next;
-        }
-        while (fast != null) {
-            fast = fast.next;
-            slow = slow.next;
-        }
-        slow.next = slow.next.next;
-        return fakeHead.next;
+  /**
+   * @param head head of the linked list
+   * @param k 1 indexed,
+   */
+  static Node removeKthLast(Node head, int k) {
+    assert k > 0;
+    Node fakeHead = new Node(0);
+    fakeHead.next = head;
+    Node slow = fakeHead, fast = fakeHead;
+    for (int i = 0; i <= k; i++) {
+      fast = fast.next;
+    }
+    while (fast != null) {
+      fast = fast.next;
+      slow = slow.next;
+    }
+    slow.next = slow.next.next;
+    return fakeHead.next;
+  }
+
+  static class Node {
+    int val;
+    Node next;
+
+    Node(int v) {
+      val = v;
+      next = null;
     }
 
-    static class Node {
-        int val;
-        Node next;
-
-        Node(int v) {
-            val = v;
-            next = null;
-        }
-
-        static List<Integer> toList(Node head) {
-            Node curr = head;
-            List<Integer> list = new ArrayList<>();
-            while (curr != null) {
-                list.add(curr.val);
-                curr = curr.next;
-            }
-            return list;
-        }
-
-        static Node fromList(List<Integer> values) {
-            if (values == null || values.isEmpty()) {
-                return null;
-            } else {
-                Node head = new Node(values.get(0));
-                head.next = fromList(values.subList(1, values.size()));
-                return head;
-            }
-        }
+    static List<Integer> toList(Node head) {
+      Node curr = head;
+      List<Integer> list = new ArrayList<>();
+      while (curr != null) {
+        list.add(curr.val);
+        curr = curr.next;
+      }
+      return list;
     }
+
+    static Node fromList(List<Integer> values) {
+      if (values == null || values.isEmpty()) {
+        return null;
+      } else {
+        Node head = new Node(values.get(0));
+        head.next = fromList(values.subList(1, values.size()));
+        return head;
+      }
+    }
+  }
 }
