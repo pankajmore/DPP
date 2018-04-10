52374e23d0bf476af3589a048f01984ef48596fd
Pankaj <pankajmore@gmail.com>
2018-04-09 22:50:30 -0400

    reformat

--- leetcode/src/ArrangingCoins.java
+++ leetcode/src/ArrangingCoins.java
@@ -1,18 +1,15 @@
-/**
- * https://leetcode.com/contest/11/problems/arranging-coins/
- * Created by pankaj on 10/31/16.
- */
+/** https://leetcode.com/contest/11/problems/arranging-coins/ Created by pankaj on 10/31/16. */
 public class ArrangingCoins {
-    public int arrangeCoins(int n) {
-        int i = 0;
-        while (n >= 0) {
-            i += 1;
-            n -= i;
-        }
-        return i - 1;
+  public int arrangeCoins(int n) {
+    int i = 0;
+    while (n >= 0) {
+      i += 1;
+      n -= i;
     }
+    return i - 1;
+  }
 
-    public int arrangeCoins1(int n) {
-        return (int) ((Math.sqrt(1 + 8 * (long) n) - 1.0) / 2.0);
-    }
+  public int arrangeCoins1(int n) {
+    return (int) ((Math.sqrt(1 + 8 * (long) n) - 1.0) / 2.0);
+  }
 }
