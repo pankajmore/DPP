2c6b80a025016be4ef73ef99c387ad3f8e46d6b2
Pankaj <pankajmore@gmail.com>
2018-04-09 22:49:50 -0400

    reformat

--- leetcode/src/AddDigits.java
+++ leetcode/src/AddDigits.java
@@ -1,9 +1,6 @@
-/**
- * https://leetcode.com/problems/add-digits/
- * Created by pankaj on 09/12/16.
- */
+/** https://leetcode.com/problems/add-digits/ Created by pankaj on 09/12/16. */
 public class AddDigits {
-    public int addDigits(int num) {
-        return num == 0 ? 0 : 1 + (num - 1) % 9;
-    }
+  public int addDigits(int num) {
+    return num == 0 ? 0 : 1 + (num - 1) % 9;
+  }
 }
