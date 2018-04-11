8dac9ec636d8e4ec9442f7636fede2e75a4d7073
Pankaj <pankajmore@gmail.com>
2018-04-10 22:51:58 -0400

    reformat

--- leetcode/src/AssignCookies.java
+++ leetcode/src/AssignCookies.java
@@ -1,19 +1,16 @@
 import java.util.Arrays;
 
-/**
- * https://leetcode.com/problems/assign-cookies/
- * Created by pankaj on 14/11/16.
- */
+/** https://leetcode.com/problems/assign-cookies/ Created by pankaj on 14/11/16. */
 public class AssignCookies {
-    public int findContentChildren(int[] g, int[] s) {
-        Arrays.sort(g);
-        Arrays.sort(s);
-        int i = 0;
-        for (int j = 0; j < s.length; j++) {
-            if (g[i] <= s[j]) {
-                i++;
-            }
-        }
-        return i;
+  public int findContentChildren(int[] g, int[] s) {
+    Arrays.sort(g);
+    Arrays.sort(s);
+    int i = 0;
+    for (int j = 0; j < s.length; j++) {
+      if (g[i] <= s[j]) {
+        i++;
+      }
     }
+    return i;
+  }
 }
