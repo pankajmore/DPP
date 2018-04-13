59481ffe18edf86104235be05acf837ce53c2118
Pankaj <pankajmore@gmail.com>
2018-04-12 22:01:37 -0400

    package private

--- leetcode/src/BalancedBinaryTree.java
+++ leetcode/src/BalancedBinaryTree.java
@@ -1,18 +1,18 @@
 /** https://leetcode.com/problems/balanced-binary-tree/ Created by pankaj on 8/17/16. */
-public class BalancedBinaryTree {
-  public boolean isBalanced(TreeNode root) {
+class BalancedBinaryTree {
+  boolean isBalanced(TreeNode root) {
     if (root == null) return true;
     return Math.abs(depth(root.left) - depth(root.right)) <= 1
         && isBalanced(root.left)
         && isBalanced(root.right);
   }
 
-  public int depth(TreeNode root) {
+  int depth(TreeNode root) {
     if (root == null) return 0;
     return 1 + Math.max(depth(root.left), depth(root.right));
   }
 
-  public class TreeNode {
+  class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
