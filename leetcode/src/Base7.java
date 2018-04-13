1770b61b6fbe77d814b025a5326c2434e69d412c
Pankaj <pankajmore@gmail.com>
2018-04-12 22:01:51 -0400

    reformat

--- leetcode/src/Base7.java
+++ leetcode/src/Base7.java
@@ -1,17 +1,17 @@
 /**
- * https://leetcode.com/contest/leetcode-weekly-contest-19/problems/base-7/
- * Created by pankaj on 12/02/17.
+ * https://leetcode.com/contest/leetcode-weekly-contest-19/problems/base-7/ Created by pankaj on
+ * 12/02/17.
  */
 public class Base7 {
-    public String convertTo7(int num) {
-        if (num == 0) return "0";
-        boolean neg = num < 0;
-        if (num < 0) num = -num;
-        StringBuilder sb = new StringBuilder();
-        while (num > 0) {
-            sb.append(num % 7);
-            num /= 7;
-        }
-        return neg ? "-" + sb.reverse().toString() : sb.reverse().toString();
+  public String convertTo7(int num) {
+    if (num == 0) return "0";
+    boolean neg = num < 0;
+    if (num < 0) num = -num;
+    StringBuilder sb = new StringBuilder();
+    while (num > 0) {
+      sb.append(num % 7);
+      num /= 7;
     }
+    return neg ? "-" + sb.reverse().toString() : sb.reverse().toString();
+  }
 }
