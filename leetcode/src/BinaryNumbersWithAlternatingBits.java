3e576acf8f2db1f7a9cc0d9fa2a6da06b96928b0
Pankaj <pankajmore@gmail.com>
2018-04-14 23:55:51 -0400

    reformat

--- leetcode/src/BinaryNumbersWithAlternatingBits.java
+++ leetcode/src/BinaryNumbersWithAlternatingBits.java
@@ -1,17 +1,15 @@
-/**
- * https://leetcode.com/problems/binary-number-with-alternating-bits/description/
- */
+/** https://leetcode.com/problems/binary-number-with-alternating-bits/description/ */
 public class BinaryNumbersWithAlternatingBits {
-    public boolean hasAlternatingBits(int n) {
-        int last = n & 1;
-        n >>= 1;
-        while (n > 0) {
-            if (last == (n & 1)) {
-                return false;
-            }
-            last = n & 1;
-            n >>= 1;
-        }
-        return true;
+  public boolean hasAlternatingBits(int n) {
+    int last = n & 1;
+    n >>= 1;
+    while (n > 0) {
+      if (last == (n & 1)) {
+        return false;
+      }
+      last = n & 1;
+      n >>= 1;
     }
+    return true;
+  }
 }
