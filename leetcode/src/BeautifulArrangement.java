761b95974e0af68d595d36b9191c5edf4a469f5a
Pankaj <pankajmore@gmail.com>
2018-04-14 23:55:34 -0400

    reformat

--- leetcode/src/BeautifulArrangement.java
+++ leetcode/src/BeautifulArrangement.java
@@ -1,30 +1,27 @@
-/**
- * https://leetcode.com/problems/beautiful-arrangement/
- * Created by pankaj on 22/02/17.
- */
+/** https://leetcode.com/problems/beautiful-arrangement/ Created by pankaj on 22/02/17. */
 public class BeautifulArrangement {
-    public int countArrangement(int N) {
-        int[] A = new int[N];
-        for (int i = 0; i < N; i++) A[i] = i + 1;
-        return count(N, A);
-    }
+  public int countArrangement(int N) {
+    int[] A = new int[N];
+    for (int i = 0; i < N; i++) A[i] = i + 1;
+    return count(N, A);
+  }
 
-    private int count(int N, int[] A) {
-        if (N <= 0) return 1;
-        int cnt = 0;
-        for (int i = 0; i < N; i++) {
-            if (A[i] % N == 0 || N % A[i] == 0) {
-                swap(A, i, N - 1);
-                cnt += count(N - 1, A);
-                swap(A, i, N - 1);
-            }
-        }
-        return cnt;
+  private int count(int N, int[] A) {
+    if (N <= 0) return 1;
+    int cnt = 0;
+    for (int i = 0; i < N; i++) {
+      if (A[i] % N == 0 || N % A[i] == 0) {
+        swap(A, i, N - 1);
+        cnt += count(N - 1, A);
+        swap(A, i, N - 1);
+      }
     }
+    return cnt;
+  }
 
-    private void swap(int[] A, int i, int j) {
-        int tmp = A[i];
-        A[i] = A[j];
-        A[j] = tmp;
-    }
+  private void swap(int[] A, int i, int j) {
+    int tmp = A[i];
+    A[i] = A[j];
+    A[j] = tmp;
+  }
 }
