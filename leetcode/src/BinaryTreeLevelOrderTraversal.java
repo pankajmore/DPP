b87ff91563a2040fc521da2bbd2d3143745bc390
Pankaj <pankajmore@gmail.com>
2018-04-15 22:58:16 -0400

    reformat

--- leetcode/src/BinaryTreeLevelOrderTraversal.java
+++ leetcode/src/BinaryTreeLevelOrderTraversal.java
@@ -4,36 +4,35 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * https://leetcode.com/problems/binary-tree-level-order-traversal/
- * Created by pankaj on 29/12/16.
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/ Created by pankaj on 29/12/16.
  */
 public class BinaryTreeLevelOrderTraversal {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        LinkedList<List<Integer>> levels = new LinkedList<>();
-        if (root == null) return levels;
-        Deque<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while (!q.isEmpty()) {
-            List<Integer> level = new ArrayList<>();
-            int size = q.size();
-            while (size-- > 0) {
-                TreeNode n = q.remove();
-                level.add(n.val);
-                if (n.left != null) q.add(n.left);
-                if (n.right != null) q.add(n.right);
-            }
-            levels.addLast(level);
-        }
-        return levels;
+  public List<List<Integer>> levelOrder(TreeNode root) {
+    LinkedList<List<Integer>> levels = new LinkedList<>();
+    if (root == null) return levels;
+    Deque<TreeNode> q = new LinkedList<>();
+    q.add(root);
+    while (!q.isEmpty()) {
+      List<Integer> level = new ArrayList<>();
+      int size = q.size();
+      while (size-- > 0) {
+        TreeNode n = q.remove();
+        level.add(n.val);
+        if (n.left != null) q.add(n.left);
+        if (n.right != null) q.add(n.right);
+      }
+      levels.addLast(level);
     }
+    return levels;
+  }
 
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-        TreeNode(int x) {
-            val = x;
-        }
+    TreeNode(int x) {
+      val = x;
     }
+  }
 }
