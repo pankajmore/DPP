54be06543fd1cb90522f4d629f5c01e015096b3f
Pankaj <pankajmore@gmail.com>
2018-04-16 20:01:36 -0400

    reformat

--- leetcode/src/BinaryTreePaths.java
+++ leetcode/src/BinaryTreePaths.java
@@ -1,40 +1,37 @@
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * https://leetcode.com/problems/binary-tree-paths/
- * Created by pankaj on 11/01/17.
- */
+/** https://leetcode.com/problems/binary-tree-paths/ Created by pankaj on 11/01/17. */
 public class BinaryTreePaths {
-    public List<String> binaryTreePaths(TreeNode root) {
-        List<String> paths = new ArrayList<>();
-        List<Integer> path = new ArrayList<>();
-        binaryTreePathsHelper(root, path, paths);
-        return paths;
-    }
+  public List<String> binaryTreePaths(TreeNode root) {
+    List<String> paths = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    binaryTreePathsHelper(root, path, paths);
+    return paths;
+  }
 
-    private void binaryTreePathsHelper(TreeNode root, List<Integer> path, List<String> paths) {
-        if (root == null) return;
-        path.add(root.val);
-        if (root.left == null && root.right == null) {
-            paths.add(pathToString(path));
-        }
-        binaryTreePathsHelper(root.left, path, paths);
-        binaryTreePathsHelper(root.right, path, paths);
-        path.remove(path.size() - 1);
+  private void binaryTreePathsHelper(TreeNode root, List<Integer> path, List<String> paths) {
+    if (root == null) return;
+    path.add(root.val);
+    if (root.left == null && root.right == null) {
+      paths.add(pathToString(path));
     }
+    binaryTreePathsHelper(root.left, path, paths);
+    binaryTreePathsHelper(root.right, path, paths);
+    path.remove(path.size() - 1);
+  }
 
-    private String pathToString(List<Integer> path) {
-        StringBuilder sb = new StringBuilder();
-        for (int v : path) {
-            if (sb.length() != 0) sb.append("->");
-            sb.append(v);
-        }
-        return sb.toString();
+  private String pathToString(List<Integer> path) {
+    StringBuilder sb = new StringBuilder();
+    for (int v : path) {
+      if (sb.length() != 0) sb.append("->");
+      sb.append(v);
     }
+    return sb.toString();
+  }
 
-    private class TreeNode {
-        int val;
-        TreeNode left, right;
-    }
+  private class TreeNode {
+    int val;
+    TreeNode left, right;
+  }
 }
