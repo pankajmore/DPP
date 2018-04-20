626f46cf69cfc0bc194af7a6f2720042fc0c8f4e
Pankaj <pankajmore@gmail.com>
2018-04-19 23:35:34 -0400

    reformat

--- leetcode/src/ConstructTheRectangle.java
+++ leetcode/src/ConstructTheRectangle.java
@@ -1,11 +1,8 @@
-/**
- * https://leetcode.com/problems/construct-the-rectangle/
- * Created by pankaj on 22/01/17.
- */
+/** https://leetcode.com/problems/construct-the-rectangle/ Created by pankaj on 22/01/17. */
 public class ConstructTheRectangle {
-    public int[] constructRectangle(int area) {
-        int w = (int) Math.sqrt(area);
-        while (area % w != 0) w--;
-        return new int[]{area / w, w};
-    }
+  public int[] constructRectangle(int area) {
+    int w = (int) Math.sqrt(area);
+    while (area % w != 0) w--;
+    return new int[] {area / w, w};
+  }
 }
