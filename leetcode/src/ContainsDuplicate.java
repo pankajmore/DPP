08977b47927d961a82db69bc5ca362da86cf6dd2
Pankaj <pankajmore@gmail.com>
2018-04-20 23:30:21 -0400

    reformat

--- leetcode/src/ContainsDuplicate.java
+++ leetcode/src/ContainsDuplicate.java
@@ -1,16 +1,13 @@
 import java.util.HashSet;
 
-/**
- * https://leetcode.com/problems/contains-duplicate/
- * Created by pankaj on 15/12/16.
- */
+/** https://leetcode.com/problems/contains-duplicate/ Created by pankaj on 15/12/16. */
 public class ContainsDuplicate {
-    public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int x : nums) {
-            if (set.contains(x)) return true;
-            set.add(x);
-        }
-        return false;
+  public boolean containsDuplicate(int[] nums) {
+    HashSet<Integer> set = new HashSet<>();
+    for (int x : nums) {
+      if (set.contains(x)) return true;
+      set.add(x);
     }
+    return false;
+  }
 }
