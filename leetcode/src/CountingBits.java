7159606d77e60d6c989a5f320eb9a375ff5c6202
Pankaj <pankajmore@gmail.com>
2018-04-28 22:39:39 -0400

    reformat

--- leetcode/src/CountingBits.java
+++ leetcode/src/CountingBits.java
@@ -1,13 +1,9 @@
-/**
- * https://leetcode.com/problems/counting-bits/
- * Created by pankaj on 9/30/16.
- */
+/** https://leetcode.com/problems/counting-bits/ Created by pankaj on 9/30/16. */
 public class CountingBits {
-    public int[] countBits(int num) {
-        int[] f = new int[num + 1];
-        f[0] = 0;
-        for (int i = 1; i <= num; i++)
-            f[i] = f[i >>> 1] + (i & 1);
-        return f;
-    }
+  public int[] countBits(int num) {
+    int[] f = new int[num + 1];
+    f[0] = 0;
+    for (int i = 1; i <= num; i++) f[i] = f[i >>> 1] + (i & 1);
+    return f;
+  }
 }
