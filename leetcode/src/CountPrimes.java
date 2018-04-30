c4ce7182521610edf7c9afea639fca5bc0963e7f
Pankaj <pankajmore@gmail.com>
2018-04-29 21:19:06 -0400

    reformat

--- leetcode/src/CountPrimes.java
+++ leetcode/src/CountPrimes.java
@@ -1,20 +1,17 @@
-/**
- * https://leetcode.com/problems/count-primes/
- * Created by pankaj on 10/02/17.
- */
+/** https://leetcode.com/problems/count-primes/ Created by pankaj on 10/02/17. */
 public class CountPrimes {
-    public int countPrimes(int N) {
-        int cnt = 0;
-        boolean[] A = new boolean[N];
-        for (int i = 2; i * i < N; i++) {
-            if (A[i]) continue;
-            for (int j = i * i; j < N; j += i) {
-                A[j] = true;
-            }
-        }
-        for (int i = 2; i < N; i++) {
-            if (!A[i]) cnt++;
-        }
-        return cnt;
+  public int countPrimes(int N) {
+    int cnt = 0;
+    boolean[] A = new boolean[N];
+    for (int i = 2; i * i < N; i++) {
+      if (A[i]) continue;
+      for (int j = i * i; j < N; j += i) {
+        A[j] = true;
+      }
     }
+    for (int i = 2; i < N; i++) {
+      if (!A[i]) cnt++;
+    }
+    return cnt;
+  }
 }
