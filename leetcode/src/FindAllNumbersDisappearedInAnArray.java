8d4068613362ddf0c982942ff2e71a185121589d
Pankaj <pankajmore@gmail.com>
2018-05-05 23:49:16 -0400

    reformat

--- leetcode/src/FindAllNumbersDisappearedInAnArray.java
+++ leetcode/src/FindAllNumbersDisappearedInAnArray.java
@@ -2,22 +2,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
- * Created by pankaj on 22/12/16.
+ * https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/ Created by pankaj on
+ * 22/12/16.
  */
 public class FindAllNumbersDisappearedInAnArray {
-    public List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer> disappeared = new ArrayList<>();
-        int N = nums.length;
-        for (int i = 0; i < N; i++) {
-            int idx = Math.abs(nums[i]) - 1;
-            if (nums[idx] > 0) {
-                nums[idx] = -nums[idx];
-            }
-        }
-        for (int i = 0; i < N; i++) {
-            if (nums[i] > 0) disappeared.add(i + 1);
-        }
-        return disappeared;
+  public List<Integer> findDisappearedNumbers(int[] nums) {
+    List<Integer> disappeared = new ArrayList<>();
+    int N = nums.length;
+    for (int i = 0; i < N; i++) {
+      int idx = Math.abs(nums[i]) - 1;
+      if (nums[idx] > 0) {
+        nums[idx] = -nums[idx];
+      }
     }
+    for (int i = 0; i < N; i++) {
+      if (nums[i] > 0) disappeared.add(i + 1);
+    }
+    return disappeared;
+  }
 }
