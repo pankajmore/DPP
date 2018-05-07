39b5c0eed08fd20e8946d45a41e3c7ff2ffc8525
Pankaj <pankajmore@gmail.com>
2018-05-06 22:38:31 -0400

    reformat

--- leetcode/src/FindModeInBinaryTree.java
+++ leetcode/src/FindModeInBinaryTree.java
@@ -5,35 +5,35 @@ import java.util.*;
  * Created by pankaj on 29/01/17.
  */
 public class FindModeInBinaryTree {
-    public int[] findMode(TreeNode root) {
-        List<Integer> A = new ArrayList<>();
-        inorder(root, A);
-        Map<Integer, Integer> cnt = new HashMap<>();
-        for (int x : A) {
-            cnt.put(x, cnt.getOrDefault(x, 0) + 1);
-        }
-        List<Integer> l = new ArrayList<>();
-        int max = Collections.max(cnt.values());
-        for (int x : cnt.keySet()) {
-            int c = cnt.get(x);
-            if (c == max) l.add(x);
-        }
-        int[] res = new int[l.size()];
-        for (int i = 0; i < res.length; i++) {
-            res[i] = l.get(i);
-        }
-        return res;
+  public int[] findMode(TreeNode root) {
+    List<Integer> A = new ArrayList<>();
+    inorder(root, A);
+    Map<Integer, Integer> cnt = new HashMap<>();
+    for (int x : A) {
+      cnt.put(x, cnt.getOrDefault(x, 0) + 1);
     }
+    List<Integer> l = new ArrayList<>();
+    int max = Collections.max(cnt.values());
+    for (int x : cnt.keySet()) {
+      int c = cnt.get(x);
+      if (c == max) l.add(x);
+    }
+    int[] res = new int[l.size()];
+    for (int i = 0; i < res.length; i++) {
+      res[i] = l.get(i);
+    }
+    return res;
+  }
 
-    private void inorder(TreeNode root, List<Integer> A) {
-        if (root == null) return;
-        inorder(root.left, A);
-        A.add(root.val);
-        inorder(root.right, A);
-    }
+  private void inorder(TreeNode root, List<Integer> A) {
+    if (root == null) return;
+    inorder(root.left, A);
+    A.add(root.val);
+    inorder(root.right, A);
+  }
 
-    private static class TreeNode {
-        int val;
-        TreeNode left, right;
-    }
+  private static class TreeNode {
+    int val;
+    TreeNode left, right;
+  }
 }
