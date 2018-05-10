c09222cc7aa5b1ead34dcf9c1da4f1651d19e262
Pankaj <pankajmore@gmail.com>
2018-05-09 21:44:11 -0400

    reformat

--- leetcode/src/FindTheDuplicateNumber.java
+++ leetcode/src/FindTheDuplicateNumber.java
@@ -1,27 +1,21 @@
-/**
- * https://leetcode.com/problems/find-the-duplicate-number/
- * Created by pankaj on 10/11/16.
- */
+/** https://leetcode.com/problems/find-the-duplicate-number/ Created by pankaj on 10/11/16. */
 public class FindTheDuplicateNumber {
-    /**
-     * Time: O(NlogN)
-     * Space: O(1)
-     */
-    public int findDuplicate(int[] nums) {
-        int lo = 1, hi = nums.length - 1;
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            int cnt = 0;
-            for (int num : nums) {
-                if (num <= mid) cnt++;
-            }
-            if (cnt > mid) {
-                hi = mid;
-            } else {
-                lo = mid + 1;
-            }
-        }
-        assert lo == hi;
-        return lo;
+  /** Time: O(NlogN) Space: O(1) */
+  public int findDuplicate(int[] nums) {
+    int lo = 1, hi = nums.length - 1;
+    while (lo < hi) {
+      int mid = lo + (hi - lo) / 2;
+      int cnt = 0;
+      for (int num : nums) {
+        if (num <= mid) cnt++;
+      }
+      if (cnt > mid) {
+        hi = mid;
+      } else {
+        lo = mid + 1;
+      }
     }
+    assert lo == hi;
+    return lo;
+  }
 }
