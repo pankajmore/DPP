6daddf68e2495df03d369bdffc0b64bd28d89a56
Pankaj <pankajmore@gmail.com>
2018-05-10 20:52:13 -0400

    reformat

--- leetcode/src/FirstBadVersion.java
+++ leetcode/src/FirstBadVersion.java
@@ -1,23 +1,20 @@
-/**
- * https://leetcode.com/problems/first-bad-version/
- * Created by pankaj on 17/02/17.
- */
+/** https://leetcode.com/problems/first-bad-version/ Created by pankaj on 17/02/17. */
 public class FirstBadVersion {
-    class Solution extends VersionControl {
-        public int firstBadVersion(int n) {
-            int lo = 1, hi = n;
-            while (lo < hi) {
-                int mid = lo + (hi - lo) / 2;
-                if (!isBadVersion(mid)) lo = mid + 1;
-                else hi = mid;
-            }
-            return lo;
-        }
+  class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+      int lo = 1, hi = n;
+      while (lo < hi) {
+        int mid = lo + (hi - lo) / 2;
+        if (!isBadVersion(mid)) lo = mid + 1;
+        else hi = mid;
+      }
+      return lo;
     }
+  }
 
-    class VersionControl {
-        boolean isBadVersion(int version) {
-            throw new UnsupportedOperationException();
-        }
+  class VersionControl {
+    boolean isBadVersion(int version) {
+      throw new UnsupportedOperationException();
     }
+  }
 }
