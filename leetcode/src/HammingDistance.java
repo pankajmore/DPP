fbe88d09ddd7406fc645fd7dba45995851f5fdd3
Pankaj <pankajmore@gmail.com>
2018-05-12 22:26:46 -0400

    reformat

--- leetcode/src/HammingDistance.java
+++ leetcode/src/HammingDistance.java
@@ -1,14 +1,14 @@
 /**
- * https://leetcode.com/contest/leetcode-weekly-contest-13/problems/hamming-distance/
- * Created by pankaj on 18/12/16.
+ * https://leetcode.com/contest/leetcode-weekly-contest-13/problems/hamming-distance/ Created by
+ * pankaj on 18/12/16.
  */
 public class HammingDistance {
-    public int hammingDistance(int x, int y) {
-        int cnt = 0, z = x ^ y;
-        while (z > 0) {
-            z = z & (z - 1);
-            cnt++;
-        }
-        return cnt;
+  public int hammingDistance(int x, int y) {
+    int cnt = 0, z = x ^ y;
+    while (z > 0) {
+      z = z & (z - 1);
+      cnt++;
     }
+    return cnt;
+  }
 }
