1c04b16ae6648009213683e81cdab8efb8aafbea
Pankaj <pankajmore@gmail.com>
2018-05-12 22:26:39 -0400

    reformat

--- leetcode/src/GuessNumberHigherOrLower.java
+++ leetcode/src/GuessNumberHigherOrLower.java
@@ -1,23 +1,20 @@
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-/**
- * https://leetcode.com/problems/guess-number-higher-or-lower/
- * Created by pankaj on 27/12/16.
- */
+/** https://leetcode.com/problems/guess-number-higher-or-lower/ Created by pankaj on 27/12/16. */
 public class GuessNumberHigherOrLower {
-    public int guessNumber(int n) {
-        int lo = 1, hi = n;
-        while (lo <= hi) {
-            int mid = (lo + hi) >>> 1;
-            int x = guess(mid);
-            if (x < 0) hi = mid - 1;
-            else if (x > 0) lo = mid + 1;
-            else return mid;
-        }
-        return -1;
+  public int guessNumber(int n) {
+    int lo = 1, hi = n;
+    while (lo <= hi) {
+      int mid = (lo + hi) >>> 1;
+      int x = guess(mid);
+      if (x < 0) hi = mid - 1;
+      else if (x > 0) lo = mid + 1;
+      else return mid;
     }
+    return -1;
+  }
 
-    private int guess(int x) {
-        throw new NotImplementedException();
-    }
+  private int guess(int x) {
+    throw new NotImplementedException();
+  }
 }
