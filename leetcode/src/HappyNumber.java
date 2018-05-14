2aebd07a66d9e2639d92c24426915a13e295546e
Pankaj <pankajmore@gmail.com>
2018-05-13 22:19:55 -0400

    reformat

--- leetcode/src/HappyNumber.java
+++ leetcode/src/HappyNumber.java
@@ -1,24 +1,21 @@
 import java.util.HashSet;
 
-/**
- * https://leetcode.com/problems/happy-number/
- * Created by pankaj on 21/12/16.
- */
+/** https://leetcode.com/problems/happy-number/ Created by pankaj on 21/12/16. */
 public class HappyNumber {
-    public boolean isHappy(int n) {
-        HashSet<Integer> visited = new HashSet<>();
-        if (n <= 0) return false;
-        while (n > 1) {
-            if (visited.contains(n)) return false;
-            visited.add(n);
-            int s = 0;
-            while (n > 0) {
-                int d = n % 10;
-                s += d * d;
-                n = n / 10;
-            }
-            n = s;
-        }
-        return true;
+  public boolean isHappy(int n) {
+    HashSet<Integer> visited = new HashSet<>();
+    if (n <= 0) return false;
+    while (n > 1) {
+      if (visited.contains(n)) return false;
+      visited.add(n);
+      int s = 0;
+      while (n > 0) {
+        int d = n % 10;
+        s += d * d;
+        n = n / 10;
+      }
+      n = s;
     }
+    return true;
+  }
 }
