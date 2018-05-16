56de58a00d27be591f3d6e301a55f6e78c98a139
Pankaj <pankajmore@gmail.com>
2018-05-15 22:51:50 -0400

    reformat

--- leetcode/src/HouseRobber.java
+++ leetcode/src/HouseRobber.java
@@ -1,16 +1,13 @@
-/**
- * https://leetcode.com/problems/house-robber/
- * Created by pankaj on 25/12/16.
- */
+/** https://leetcode.com/problems/house-robber/ Created by pankaj on 25/12/16. */
 public class HouseRobber {
-    public int rob(int[] nums) {
-        int N = nums.length;
-        int a = 0, b = 0;
-        for (int i = 0; i < N; i++) {
-            int tmp = Math.max(b, a + nums[i]);
-            a = b;
-            b = tmp;
-        }
-        return b;
+  public int rob(int[] nums) {
+    int N = nums.length;
+    int a = 0, b = 0;
+    for (int i = 0; i < N; i++) {
+      int tmp = Math.max(b, a + nums[i]);
+      a = b;
+      b = tmp;
     }
+    return b;
+  }
 }
