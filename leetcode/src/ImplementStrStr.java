68a655a6270f73ab23368688c61148be1b416776
Pankaj <pankajmore@gmail.com>
2018-05-16 21:37:19 -0400

    reformat

--- leetcode/src/ImplementStrStr.java
+++ leetcode/src/ImplementStrStr.java
@@ -1,12 +1,9 @@
-/**
- * https://leetcode.com/problems/implement-strstr/
- * Created by pankaj on 08/02/17.
- */
+/** https://leetcode.com/problems/implement-strstr/ Created by pankaj on 08/02/17. */
 public class ImplementStrStr {
-    public int strStr(String haystack, String needle) {
-        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
-            if (haystack.substring(i, i + needle.length()).equals(needle)) return i;
-        }
-        return -1;
+  public int strStr(String haystack, String needle) {
+    for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+      if (haystack.substring(i, i + needle.length()).equals(needle)) return i;
     }
+    return -1;
+  }
 }
