eeec611c55a37f65ff476f9fd234dc2b77aa3e4a
Pankaj <pankajmore@gmail.com>
2018-05-23 22:04:17 -0400

    reformat

--- leetcode/src/LengthOfLastWord.java
+++ leetcode/src/LengthOfLastWord.java
@@ -1,16 +1,13 @@
-/**
- * https://leetcode.com/problems/length-of-last-word/
- * Created by pankaj on 8/24/16.
- */
+/** https://leetcode.com/problems/length-of-last-word/ Created by pankaj on 8/24/16. */
 public class LengthOfLastWord {
-    public int lengthOfLastWord(String s) {
-        if (s == null || s.length() == 0) return 0;
-        int len = 0, i = s.length() - 1;
-        while (i >= 0 && s.charAt(i) == ' ') i--;
-        while (i >= 0 && s.charAt(i) != ' ') {
-            len++;
-            i--;
-        }
-        return len;
+  public int lengthOfLastWord(String s) {
+    if (s == null || s.length() == 0) return 0;
+    int len = 0, i = s.length() - 1;
+    while (i >= 0 && s.charAt(i) == ' ') i--;
+    while (i >= 0 && s.charAt(i) != ' ') {
+      len++;
+      i--;
     }
+    return len;
+  }
 }
