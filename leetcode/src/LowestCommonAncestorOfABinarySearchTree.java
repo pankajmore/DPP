aa77f48277eefd5a530a3f335a8141fe47371fee
Pankaj <pankajmore@gmail.com>
2018-06-06 22:51:05 -0400

    reformat

--- leetcode/src/LowestCommonAncestorOfABinarySearchTree.java
+++ leetcode/src/LowestCommonAncestorOfABinarySearchTree.java
@@ -1,27 +1,27 @@
 /**
- * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
- * Created by pankaj on 28/12/16.
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/ Created by pankaj
+ * on 28/12/16.
  */
 public class LowestCommonAncestorOfABinarySearchTree {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        while (root != null) {
-            if (root.val > p.val && root.val > q.val) {
-                root = root.left;
-            } else if (root.val < p.val && root.val < q.val) {
-                root = root.right;
-            } else {
-                return root;
-            }
-        }
-        return null;
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    while (root != null) {
+      if (root.val > p.val && root.val > q.val) {
+        root = root.left;
+      } else if (root.val < p.val && root.val < q.val) {
+        root = root.right;
+      } else {
+        return root;
+      }
     }
+    return null;
+  }
 
-    private static class TreeNode {
-        int val;
-        TreeNode left, right;
+  private static class TreeNode {
+    int val;
+    TreeNode left, right;
 
-        TreeNode(int x) {
-            val = x;
-        }
+    TreeNode(int x) {
+      val = x;
     }
+  }
 }
