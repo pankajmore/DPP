d756deb046aed8339cf5fab8d07a9e5f12cfe47e
Pankaj <pankajmore@gmail.com>
2018-06-15 23:39:35 -0400

    reformat

--- leetcode/src/MinimumMovesToEqualArrayElements.java
+++ leetcode/src/MinimumMovesToEqualArrayElements.java
@@ -1,15 +1,15 @@
 /**
- * https://leetcode.com/contest/12/problems/minimum-moves-to-equal-array-elements/
- * Created by pankaj on 11/6/16.
+ * https://leetcode.com/contest/12/problems/minimum-moves-to-equal-array-elements/ Created by pankaj
+ * on 11/6/16.
  */
 public class MinimumMovesToEqualArrayElements {
-    public int minMoves(int[] nums) {
-        int N = nums.length;
-        if (N == 0) return 0;
-        int min = Integer.MAX_VALUE;
-        for (int n : nums) min = Math.min(min, n);
-        int sum = 0;
-        for (int n : nums) sum += n - min;
-        return sum;
-    }
+  public int minMoves(int[] nums) {
+    int N = nums.length;
+    if (N == 0) return 0;
+    int min = Integer.MAX_VALUE;
+    for (int n : nums) min = Math.min(min, n);
+    int sum = 0;
+    for (int n : nums) sum += n - min;
+    return sum;
+  }
 }
