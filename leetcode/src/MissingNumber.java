57cb3e5b5e219faaf343e8ba28c02cf60a697352
Pankaj <pankajmore@gmail.com>
2018-06-17 22:24:16 -0400

    reformat

--- leetcode/src/MissingNumber.java
+++ leetcode/src/MissingNumber.java
@@ -1,13 +1,10 @@
-/**
- * https://leetcode.com/problems/missing-number/
- * Created by pankaj on 30/01/17.
- */
+/** https://leetcode.com/problems/missing-number/ Created by pankaj on 30/01/17. */
 public class MissingNumber {
-    public int missingNumber(int[] nums) {
-        int i = 0, missing = nums.length;
-        for (int x : nums) {
-            missing ^= x ^ i++;
-        }
-        return missing;
+  public int missingNumber(int[] nums) {
+    int i = 0, missing = nums.length;
+    for (int x : nums) {
+      missing ^= x ^ i++;
     }
+    return missing;
+  }
 }
