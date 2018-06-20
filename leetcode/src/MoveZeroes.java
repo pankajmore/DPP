ae0d3330683861625b5845711602d47f70c0e3be
Pankaj <pankajmore@gmail.com>
2018-06-19 21:17:17 -0400

    reformat

--- leetcode/src/MoveZeroes.java
+++ leetcode/src/MoveZeroes.java
@@ -1,16 +1,13 @@
-/**
- * https://leetcode.com/problems/move-zeroes/
- * Created by pankaj on 10/26/16.
- */
+/** https://leetcode.com/problems/move-zeroes/ Created by pankaj on 10/26/16. */
 public class MoveZeroes {
-    public void moveZeroes(int[] nums) {
-        int i, idx, N = nums.length;
-        for (i = 0, idx = 0; i < N; i++) {
-            if (nums[i] != 0) {
-                int tmp = nums[idx];
-                nums[idx++] = nums[i];
-                nums[i] = tmp;
-            }
-        }
+  public void moveZeroes(int[] nums) {
+    int i, idx, N = nums.length;
+    for (i = 0, idx = 0; i < N; i++) {
+      if (nums[i] != 0) {
+        int tmp = nums[idx];
+        nums[idx++] = nums[i];
+        nums[i] = tmp;
+      }
     }
+  }
 }
