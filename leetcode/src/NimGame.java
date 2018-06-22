3347ee6b8abb0d619d8e09e234c2c70e738a29f0
Pankaj <pankajmore@gmail.com>
2018-06-21 21:34:52 -0400

    reformat

--- leetcode/src/NimGame.java
+++ leetcode/src/NimGame.java
@@ -1,9 +1,6 @@
-/**
- * https://leetcode.com/problems/nim-game/
- * Created by pankaj on 10/4/16.
- */
+/** https://leetcode.com/problems/nim-game/ Created by pankaj on 10/4/16. */
 public class NimGame {
-    public boolean canWinNim(int n) {
-        return (n & 0b11) != 0;
-    }
+  public boolean canWinNim(int n) {
+    return (n & 0b11) != 0;
+  }
 }
