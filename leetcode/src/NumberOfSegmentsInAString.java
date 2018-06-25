06d1c3829a3b7be04a7d19aaaba4fb5fa90c1dfd
Pankaj <pankajmore@gmail.com>
2018-06-24 22:59:16 -0400

    reformat

--- leetcode/src/NumberOfSegmentsInAString.java
+++ leetcode/src/NumberOfSegmentsInAString.java
@@ -1,14 +1,10 @@
-/**
- * https://leetcode.com/problems/number-of-segments-in-a-string/
- * Created by pankaj on 04/12/16.
- */
+/** https://leetcode.com/problems/number-of-segments-in-a-string/ Created by pankaj on 04/12/16. */
 public class NumberOfSegmentsInAString {
-    public int countSegments(String s) {
-        int cnt = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' '))
-                cnt++;
-        }
-        return cnt;
+  public int countSegments(String s) {
+    int cnt = 0;
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' ')) cnt++;
     }
+    return cnt;
+  }
 }
