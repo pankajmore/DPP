fcedc9273e8db460c52be8852137153a2bc4c66d
Pankaj <pankajmore@gmail.com>
2018-06-27 22:37:42 -0400

    reformat

--- leetcode/src/PascalsTriangle.java
+++ leetcode/src/PascalsTriangle.java
@@ -2,22 +2,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * https://leetcode.com/problems/pascals-triangle/
- * Created by pankaj on 30/12/16.
- */
+/** https://leetcode.com/problems/pascals-triangle/ Created by pankaj on 30/12/16. */
 public class PascalsTriangle {
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> rows = new ArrayList<>();
-        if (numRows == 0) return rows;
-        rows.add(Collections.singletonList(1));
-        for (int i = 1; i < numRows; i++) {
-            List<Integer> row = new ArrayList<>();
-            for (int j = 0; j <= i; j++) {
-                row.add((j == 0 ? 0 : rows.get(i - 1).get(j - 1)) + (j == i ? 0 : rows.get(i - 1).get(j)));
-            }
-            rows.add(row);
-        }
-        return rows;
+  public List<List<Integer>> generate(int numRows) {
+    List<List<Integer>> rows = new ArrayList<>();
+    if (numRows == 0) return rows;
+    rows.add(Collections.singletonList(1));
+    for (int i = 1; i < numRows; i++) {
+      List<Integer> row = new ArrayList<>();
+      for (int j = 0; j <= i; j++) {
+        row.add((j == 0 ? 0 : rows.get(i - 1).get(j - 1)) + (j == i ? 0 : rows.get(i - 1).get(j)));
+      }
+      rows.add(row);
     }
+    return rows;
+  }
 }
