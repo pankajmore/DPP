f96f5c7387bc499f656a9fdccda13bb1f0d1e221
Pankaj <pankajmore@gmail.com>
2018-06-29 22:37:49 -0400

    reformat

--- leetcode/src/PathSumIII.java
+++ leetcode/src/PathSumIII.java
@@ -1,25 +1,24 @@
-/**
- * https://leetcode.com/problems/path-sum-iii/
- * Created by pankaj on 10/24/16.
- */
+/** https://leetcode.com/problems/path-sum-iii/ Created by pankaj on 10/24/16. */
 public class PathSumIII {
-    public int pathSum(TreeNode root, int sum) {
-        if (root == null) return 0;
-        return fromRootSum(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
-    }
+  public int pathSum(TreeNode root, int sum) {
+    if (root == null) return 0;
+    return fromRootSum(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+  }
 
-    public int fromRootSum(TreeNode root, int sum) {
-        if (root == null) return 0;
-        return (root.val == sum ? 1 : 0) + fromRootSum(root.left, sum - root.val) + fromRootSum(root.right, sum - root.val);
-    }
+  public int fromRootSum(TreeNode root, int sum) {
+    if (root == null) return 0;
+    return (root.val == sum ? 1 : 0)
+        + fromRootSum(root.left, sum - root.val)
+        + fromRootSum(root.right, sum - root.val);
+  }
 
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-        TreeNode(int x) {
-            val = x;
-        }
+    TreeNode(int x) {
+      val = x;
     }
+  }
 }
