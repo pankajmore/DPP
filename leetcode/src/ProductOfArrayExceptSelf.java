f43b183f8233d8bb4f0a73637e1f9fcd2e0f3959
Pankaj <pankajmore@gmail.com>
2018-07-01 21:50:07 -0400

    reformat

--- leetcode/src/ProductOfArrayExceptSelf.java
+++ leetcode/src/ProductOfArrayExceptSelf.java
@@ -1,19 +1,16 @@
-/**
- * https://leetcode.com/problems/product-of-array-except-self/
- * Created by pankaj on 24/02/17.
- */
+/** https://leetcode.com/problems/product-of-array-except-self/ Created by pankaj on 24/02/17. */
 public class ProductOfArrayExceptSelf {
-    public int[] productExceptSelf(int[] nums) {
-        int N = nums.length;
-        int[] res = new int[N];
-        for (int i = 0, left = 1; i < N; i++) {
-            res[i] = left;
-            left *= nums[i];
-        }
-        for (int i = N - 1, right = 1; i >= 0; i--) {
-            res[i] *= right;
-            right *= nums[i];
-        }
-        return res;
+  public int[] productExceptSelf(int[] nums) {
+    int N = nums.length;
+    int[] res = new int[N];
+    for (int i = 0, left = 1; i < N; i++) {
+      res[i] = left;
+      left *= nums[i];
     }
+    for (int i = N - 1, right = 1; i >= 0; i--) {
+      res[i] *= right;
+      right *= nums[i];
+    }
+    return res;
+  }
 }
