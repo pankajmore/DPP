4fe8efa3f960874708a76edba916fc99da0be5e0
Pankaj <pankajmore@gmail.com>
2018-07-02 21:34:36 -0400

    reformat

--- leetcode/src/RangeSumQueryImmutable.java
+++ leetcode/src/RangeSumQueryImmutable.java
@@ -1,20 +1,17 @@
-/**
- * https://leetcode.com/problems/range-sum-query-immutable/
- * Created by pankaj on 17/02/17.
- */
+/** https://leetcode.com/problems/range-sum-query-immutable/ Created by pankaj on 17/02/17. */
 public class RangeSumQueryImmutable {
-    public class NumArray {
-        private final int[] prefixSum;
+  public class NumArray {
+    private final int[] prefixSum;
 
-        public NumArray(int[] nums) {
-            prefixSum = new int[nums.length];
-            for (int i = 0; i < nums.length; i++) {
-                prefixSum[i] = (i == 0 ? 0 : prefixSum[i - 1]) + nums[i];
-            }
-        }
-
-        public int sumRange(int i, int j) {
-            return prefixSum[j] - (i == 0 ? 0 : prefixSum[i - 1]);
-        }
+    public NumArray(int[] nums) {
+      prefixSum = new int[nums.length];
+      for (int i = 0; i < nums.length; i++) {
+        prefixSum[i] = (i == 0 ? 0 : prefixSum[i - 1]) + nums[i];
+      }
     }
+
+    public int sumRange(int i, int j) {
+      return prefixSum[j] - (i == 0 ? 0 : prefixSum[i - 1]);
+    }
+  }
 }
