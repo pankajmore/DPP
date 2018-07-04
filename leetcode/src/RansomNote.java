fc65e5e1f34e1068bc4faab270016c5aefac006c
Pankaj <pankajmore@gmail.com>
2018-07-03 22:17:26 -0400

    reformat

--- leetcode/src/RansomNote.java
+++ leetcode/src/RansomNote.java
@@ -1,13 +1,10 @@
-/**
- * https://leetcode.com/problems/ransom-note/
- * Created by pankaj on 9/10/16.
- */
+/** https://leetcode.com/problems/ransom-note/ Created by pankaj on 9/10/16. */
 public class RansomNote {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        int a[] = new int[26];
-        for (char c : magazine.toCharArray()) a[c - 'a']++;
-        for (char c : ransomNote.toCharArray()) a[c - 'a']--;
-        for (int i = 0; i < 26; i++) if (a[i] < 0) return false;
-        return true;
-    }
+  public boolean canConstruct(String ransomNote, String magazine) {
+    int a[] = new int[26];
+    for (char c : magazine.toCharArray()) a[c - 'a']++;
+    for (char c : ransomNote.toCharArray()) a[c - 'a']--;
+    for (int i = 0; i < 26; i++) if (a[i] < 0) return false;
+    return true;
+  }
 }
