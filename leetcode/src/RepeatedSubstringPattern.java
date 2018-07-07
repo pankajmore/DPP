bc0d2045c48f55b3560327033a3343925b88a0ae
Pankaj <pankajmore@gmail.com>
2018-07-06 23:24:09 -0400

    reformat

--- leetcode/src/RepeatedSubstringPattern.java
+++ leetcode/src/RepeatedSubstringPattern.java
@@ -1,38 +1,33 @@
-/**
- * https://leetcode.com/problems/repeated-substring-pattern/
- * Created by pankaj on 17/11/16.
- */
+/** https://leetcode.com/problems/repeated-substring-pattern/ Created by pankaj on 17/11/16. */
 public class RepeatedSubstringPattern {
-    public boolean repeatedSubstringPattern(String str) {
-        int N = str.length();
-        for (int d = 1; d <= N / 2; d++) { // logN divisors?
-            if (N % d == 0 && check(str, d)) return true;
-        }
-        return false;
+  public boolean repeatedSubstringPattern(String str) {
+    int N = str.length();
+    for (int d = 1; d <= N / 2; d++) { // logN divisors?
+      if (N % d == 0 && check(str, d)) return true;
     }
+    return false;
+  }
 
-    private boolean check(String str, int d) {
-        for (int i = 0, j = 0; i < str.length(); i++, j = (j + 1) % d) {
-            if (str.charAt(i) != str.charAt(j))
-                return false;
-        }
-        return true;
+  private boolean check(String str, int d) {
+    for (int i = 0, j = 0; i < str.length(); i++, j = (j + 1) % d) {
+      if (str.charAt(i) != str.charAt(j)) return false;
     }
+    return true;
+  }
 
-    public boolean repeatedSubstringPattern1(String str) {
-        return (str + str).substring(1, 2 * str.length() - 1).contains(str);
-    }
+  public boolean repeatedSubstringPattern1(String str) {
+    return (str + str).substring(1, 2 * str.length() - 1).contains(str);
+  }
 
-    public boolean repeatedSubstringPattern2(String str) {
-        int N = str.length();
-        for (int d = 1; d <= N / 2; d++) {
-            if (N % d == 0 && str.startsWith(str.substring(d)))
-                return true;
-        }
-        return false;
+  public boolean repeatedSubstringPattern2(String str) {
+    int N = str.length();
+    for (int d = 1; d <= N / 2; d++) {
+      if (N % d == 0 && str.startsWith(str.substring(d))) return true;
     }
+    return false;
+  }
 
-    public boolean repeatedSubstringPatternRegEX(String str) {
-        return str.matches("(.+)\\1+");
-    }
+  public boolean repeatedSubstringPatternRegEX(String str) {
+    return str.matches("(.+)\\1+");
+  }
 }
