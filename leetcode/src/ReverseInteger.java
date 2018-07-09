f712a8dc00cb47f8f5bfdac1151d17105ed5442c
Pankaj <pankajmore@gmail.com>
2018-07-08 22:38:10 -0400

    reformat

--- leetcode/src/ReverseInteger.java
+++ leetcode/src/ReverseInteger.java
@@ -1,16 +1,13 @@
-/**
- * https://leetcode.com/problems/reverse-integer/
- * Created by pankaj on 9/23/16.
- */
+/** https://leetcode.com/problems/reverse-integer/ Created by pankaj on 9/23/16. */
 public class ReverseInteger {
-    public int reverse(int x) {
-        int r = 0;
-        while (x != 0) {
-            int temp = (r * 10) + (x % 10);
-            if (temp / 10 != r) return 0; // handle overflow
-            r = temp;
-            x /= 10;
-        }
-        return r;
+  public int reverse(int x) {
+    int r = 0;
+    while (x != 0) {
+      int temp = (r * 10) + (x % 10);
+      if (temp / 10 != r) return 0; // handle overflow
+      r = temp;
+      x /= 10;
     }
+    return r;
+  }
 }
