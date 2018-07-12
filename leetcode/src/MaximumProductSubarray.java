43e9bccdcf8856fde9f145cdaac58c0e29705d4f
Pankaj <pankajmore@gmail.com>
2018-07-11 21:15:48 -0400

    reformat

--- leetcode/src/MaximumProductSubarray.java
+++ leetcode/src/MaximumProductSubarray.java
@@ -1,19 +1,16 @@
-/**
- * https://leetcode.com/problems/maximum-product-subarray/
- * Created by pankaj on 9/15/16.
- */
+/** https://leetcode.com/problems/maximum-product-subarray/ Created by pankaj on 9/15/16. */
 public class MaximumProductSubarray {
-    public int maxProduct(int[] nums) {
-        if (nums == null || nums.length == 0) return 1;
-        int N = nums.length;
-        int max = nums[0], maxSoFar = nums[0], minSoFar = nums[0];
-        for (int i = 1; i < N; i++) {
-            int x = nums[i];
-            int temp = Math.max(x, Math.max(maxSoFar * x, minSoFar * x));
-            minSoFar = Math.min(x, Math.min(maxSoFar * x, minSoFar * x));
-            maxSoFar = temp;
-            max = Math.max(max, maxSoFar);
-        }
-        return max;
+  public int maxProduct(int[] nums) {
+    if (nums == null || nums.length == 0) return 1;
+    int N = nums.length;
+    int max = nums[0], maxSoFar = nums[0], minSoFar = nums[0];
+    for (int i = 1; i < N; i++) {
+      int x = nums[i];
+      int temp = Math.max(x, Math.max(maxSoFar * x, minSoFar * x));
+      minSoFar = Math.min(x, Math.min(maxSoFar * x, minSoFar * x));
+      maxSoFar = temp;
+      max = Math.max(max, maxSoFar);
     }
+    return max;
+  }
 }
