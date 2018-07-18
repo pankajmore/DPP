bc5af880035f826b7271232243810b439a257fc5
Pankaj <pankajmore@gmail.com>
2018-07-17 22:02:40 -0400

    reformat

--- leetcode/src/SymmetricTree.java
+++ leetcode/src/SymmetricTree.java
@@ -1,19 +1,18 @@
-/**
- * https://leetcode.com/problems/symmetric-tree/
- * Created by pankaj on 25/12/16.
- */
+/** https://leetcode.com/problems/symmetric-tree/ Created by pankaj on 25/12/16. */
 public class SymmetricTree {
-    public boolean isSymmetric(TreeNode root) {
-        return isSymmetricHelper(root.left, root.right);
-    }
+  public boolean isSymmetric(TreeNode root) {
+    return isSymmetricHelper(root.left, root.right);
+  }
 
-    private boolean isSymmetricHelper(TreeNode left, TreeNode right) {
-        if (left == null || right == null) return left == right;
-        return left.val == right.val && isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
-    }
+  private boolean isSymmetricHelper(TreeNode left, TreeNode right) {
+    if (left == null || right == null) return left == right;
+    return left.val == right.val
+        && isSymmetricHelper(left.left, right.right)
+        && isSymmetricHelper(left.right, right.left);
+  }
 
-    public class TreeNode {
-        int val;
-        TreeNode left, right;
-    }
+  public class TreeNode {
+    int val;
+    TreeNode left, right;
+  }
 }
