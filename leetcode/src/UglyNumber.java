067a378ac2ffc5f10bf22ae58ce20ec96ac55b6e
Pankaj <pankajmore@gmail.com>
2018-07-23 22:04:50 -0400

    reformat

--- leetcode/src/UglyNumber.java
+++ leetcode/src/UglyNumber.java
@@ -1,13 +1,10 @@
-/**
- * https://leetcode.com/problems/ugly-number/
- * Created by pankaj on 24/12/16.
- */
+/** https://leetcode.com/problems/ugly-number/ Created by pankaj on 24/12/16. */
 public class UglyNumber {
-    public boolean isUgly(int num) {
-        if (num < 1) return false;
-        while (num % 2 == 0) num /= 2;
-        while (num % 3 == 0) num /= 3;
-        while (num % 5 == 0) num /= 5;
-        return num == 1;
-    }
+  public boolean isUgly(int num) {
+    if (num < 1) return false;
+    while (num % 2 == 0) num /= 2;
+    while (num % 3 == 0) num /= 3;
+    while (num % 5 == 0) num /= 5;
+    return num == 1;
+  }
 }
