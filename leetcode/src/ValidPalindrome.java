3f05286c4236c12a8c93b562a7ecb11076bd333e
Pankaj <pankajmore@gmail.com>
2018-07-25 22:13:35 -0400

    reformat

--- leetcode/src/ValidPalindrome.java
+++ leetcode/src/ValidPalindrome.java
@@ -1,17 +1,15 @@
-/**
- * https://leetcode.com/problems/valid-palindrome/
- * Created by pankaj on 09/02/17.
- */
+/** https://leetcode.com/problems/valid-palindrome/ Created by pankaj on 09/02/17. */
 public class ValidPalindrome {
-    public boolean isPalindrome(String s) {
-        int i = 0, j = s.length() - 1;
-        while (i < j) {
-            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
-            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
-            if (i < j && Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) return false;
-            i++;
-            j--;
-        }
-        return true;
+  public boolean isPalindrome(String s) {
+    int i = 0, j = s.length() - 1;
+    while (i < j) {
+      while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+      while (i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
+      if (i < j && Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+        return false;
+      i++;
+      j--;
     }
+    return true;
+  }
 }
