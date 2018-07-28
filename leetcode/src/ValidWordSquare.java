e546298350e0693f157364f4dfef9b7b5afe0803
Pankaj <pankajmore@gmail.com>
2018-07-27 22:01:24 -0400

    reformat

--- leetcode/src/ValidWordSquare.java
+++ leetcode/src/ValidWordSquare.java
@@ -1,17 +1,15 @@
 import java.util.List;
 
-/**
- * https://leetcode.com/problems/valid-word-square
- * Created by pankaj on 10/16/16.
- */
+/** https://leetcode.com/problems/valid-word-square Created by pankaj on 10/16/16. */
 public class ValidWordSquare {
-    public boolean validWordSquare(List<String> words) {
-        for (int i = 0; i < words.size(); i++) {
-            for (int j = 0; j < words.get(i).length(); j++) {
-                if (j >= words.size() || i >= words.get(j).length() || words.get(i).charAt(j) != words.get(j).charAt(i))
-                    return false;
-            }
-        }
-        return true;
+  public boolean validWordSquare(List<String> words) {
+    for (int i = 0; i < words.size(); i++) {
+      for (int j = 0; j < words.get(i).length(); j++) {
+        if (j >= words.size()
+            || i >= words.get(j).length()
+            || words.get(i).charAt(j) != words.get(j).charAt(i)) return false;
+      }
     }
+    return true;
+  }
 }
