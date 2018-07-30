6ddf8c50d8bb15af6edee514d4163f18516fcf0e
Pankaj <pankajmore@gmail.com>
2018-07-29 22:11:30 -0400

    reformat

--- leetcode/src/MinimumDepthOfBinaryTree.java
+++ leetcode/src/MinimumDepthOfBinaryTree.java
@@ -1,6 +1,6 @@
 /** https://leetcode.com/problems/minimum-depth-of-binary-tree/ Created by pankaj on 17/01/17. */
-public class MinimumDepthOfBinaryTree {
-  public int minDepth(TreeNode root) {
+class MinimumDepthOfBinaryTree {
+  int minDepth(TreeNode root) {
     if (root == null) return 0;
     if (root.left == null) return 1 + minDepth(root.right);
     if (root.right == null) return 1 + minDepth(root.left);
