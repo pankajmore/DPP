acb5f1e25623547ec2a3b19d2dcde2e69ff6b480
Pankaj <pankajmore@gmail.com>
2018-07-29 22:07:21 -0400

    reformat

--- leetcode/src/MaxConsecutiveOnes.java
+++ leetcode/src/MaxConsecutiveOnes.java
@@ -1,13 +1,10 @@
-/**
- * https://leetcode.com/problems/max-consecutive-ones/
- * Created by pankaj on 15/01/17.
- */
+/** https://leetcode.com/problems/max-consecutive-ones/ Created by pankaj on 15/01/17. */
 public class MaxConsecutiveOnes {
-    public int findMaxConsecutiveOnes(int[] nums) {
-        int max = 0, curr = 0;
-        for (int n : nums) {
-            max = Math.max(max, curr = n == 0 ? 0 : curr + 1);
-        }
-        return max;
+  public int findMaxConsecutiveOnes(int[] nums) {
+    int max = 0, curr = 0;
+    for (int n : nums) {
+      max = Math.max(max, curr = n == 0 ? 0 : curr + 1);
     }
+    return max;
+  }
 }
