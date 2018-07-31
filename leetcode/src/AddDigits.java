2bc7aca73e8b86eb911221e745dbe9c1fa9dd28e
Pankaj <pankajmore@gmail.com>
2018-07-30 22:49:30 -0400

    make package private

--- leetcode/src/AddDigits.java
+++ leetcode/src/AddDigits.java
@@ -1,6 +1,6 @@
 /** https://leetcode.com/problems/add-digits/ Created by pankaj on 09/12/16. */
-public class AddDigits {
-  public int addDigits(int num) {
+class AddDigits {
+  int addDigits(int num) {
     return num == 0 ? 0 : 1 + (num - 1) % 9;
   }
 }
