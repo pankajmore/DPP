cf217f543a1807b56fe59cbc81ae10543b4de1fa
Pankaj <pankajmore@gmail.com>
2018-08-01 21:44:48 -0400

    make package private

--- leetcode/src/AssignCookies.java
+++ leetcode/src/AssignCookies.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
 
 /** https://leetcode.com/problems/assign-cookies/ Created by pankaj on 14/11/16. */
-public class AssignCookies {
-  public int findContentChildren(int[] g, int[] s) {
+class AssignCookies {
+  int findContentChildren(int[] g, int[] s) {
     Arrays.sort(g);
     Arrays.sort(s);
     int i = 0;
