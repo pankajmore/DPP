59251ed0e10c570ec0be316dea421e9db60465c8
Pankaj <pankajmore@gmail.com>
2018-08-04 22:41:31 -0400

    package-private

--- leetcode/src/BeautifulArrangement.java
+++ leetcode/src/BeautifulArrangement.java
@@ -1,6 +1,6 @@
 /** https://leetcode.com/problems/beautiful-arrangement/ Created by pankaj on 22/02/17. */
-public class BeautifulArrangement {
-  public int countArrangement(int N) {
+class BeautifulArrangement {
+  int countArrangement(int N) {
     int[] A = new int[N];
     for (int i = 0; i < N; i++) A[i] = i + 1;
     return count(N, A);
