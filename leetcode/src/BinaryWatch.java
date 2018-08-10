b1d72fac8526d00ed56cd7a3d678e2a088d39820
Pankaj <pankajmore@gmail.com>
2018-08-09 22:38:59 -0400

    package private

--- leetcode/src/BinaryWatch.java
+++ leetcode/src/BinaryWatch.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /** https://leetcode.com/problems/binary-watch/ Created by pankaj on 9/23/16. */
-public class BinaryWatch {
-  public List<String> readBinaryWatch(int num) {
+class BinaryWatch {
+  List<String> readBinaryWatch(int num) {
     List<String> l = new ArrayList<>();
     for (int h = 0; h < 12; h++) {
       for (int m = 0; m < 60; m++) {
