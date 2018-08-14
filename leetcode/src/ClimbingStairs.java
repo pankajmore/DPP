e4333ee0cf675253792a6bf66e1bdc778b744038
Pankaj <pankajmore@gmail.com>
2018-08-13 22:10:57 -0400

    package private

--- leetcode/src/ClimbingStairs.java
+++ leetcode/src/ClimbingStairs.java
@@ -1,6 +1,6 @@
 /** https://leetcode.com/problems/climbing-stairs/ Created by pankaj on 9/26/16. */
-public class ClimbingStairs {
-  public int climbStairs(int n) {
+class ClimbingStairs {
+  int climbStairs(int n) {
     if (n < 2) return n;
     int a = 1, b = 2;
     for (int i = 3; i <= n; i++) {
