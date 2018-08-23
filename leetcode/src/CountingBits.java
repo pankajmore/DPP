7612b4bf107916def2ae1c0f9a73e59692ffa4ab
Pankaj <pankajmore@gmail.com>
2018-08-22 22:02:01 -0400

    package private

--- leetcode/src/CountingBits.java
+++ leetcode/src/CountingBits.java
@@ -1,6 +1,6 @@
 /** https://leetcode.com/problems/counting-bits/ Created by pankaj on 9/30/16. */
-public class CountingBits {
-  public int[] countBits(int num) {
+class CountingBits {
+  int[] countBits(int num) {
     int[] f = new int[num + 1];
     f[0] = 0;
     for (int i = 1; i <= num; i++) f[i] = f[i >>> 1] + (i & 1);
