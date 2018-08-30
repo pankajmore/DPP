adc36f708de6cf9783fb330acac985d3188ebfbd
Pankaj <pankajmore@gmail.com>
2018-08-29 19:45:58 -0700

    package private

--- leetcode/src/EliminationGame.java
+++ leetcode/src/EliminationGame.java
@@ -1,16 +1,16 @@
 /** https://leetcode.com/contest/2/problems/elimination-game/ Created by pankaj on 8/28/16. */
-public class EliminationGame {
-  public int lastRemaining(int n) {
+class EliminationGame {
+  int lastRemaining(int n) {
     return f(n, true);
   }
 
-  public int f(int n, boolean left) {
+  private int f(int n, boolean left) {
     if (n == 1) return 1;
     if (left) return 2 * f(n / 2, false);
     else return n + 1 - f(n, true);
   }
 
-  public int lastRemaining1(int n) {
+  int lastRemaining1(int n) {
     int head = 1, remaining = n, step = 1;
     boolean left = true;
     while (remaining > 1) {
@@ -24,7 +24,7 @@ public class EliminationGame {
     return head;
   }
 
-  public int lastRemaining2(int n) {
+  int lastRemaining2(int n) {
     return 1 + ((Integer.highestOneBit(n) - 1) & (0x55555555 | n));
   }
 }
