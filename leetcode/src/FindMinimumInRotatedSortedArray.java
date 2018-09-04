d90ac91c93ac942683a120e01cc612f3178473b3
Pankaj <pankajmore@gmail.com>
2018-09-03 20:12:04 -0400

    package private

--- leetcode/src/FindMinimumInRotatedSortedArray.java
+++ leetcode/src/FindMinimumInRotatedSortedArray.java
@@ -2,8 +2,8 @@
  * https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/ Created by pankaj on
  * 24/03/17.
  */
-public class FindMinimumInRotatedSortedArray {
-  public int findMin(int[] nums) {
+class FindMinimumInRotatedSortedArray {
+  int findMin(int[] nums) {
     int lo = 0, hi = nums.length - 1;
     while (lo < hi) {
       int mid = lo + (hi - lo) / 2;
