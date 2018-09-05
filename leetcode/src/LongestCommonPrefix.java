c9219f970945ce61699a324c4b0be2c025e96742
Pankaj <pankajmore@gmail.com>
2018-09-05 19:32:49 -0400

    package private

--- leetcode/src/LongestCommonPrefix.java
+++ leetcode/src/LongestCommonPrefix.java
@@ -1,6 +1,6 @@
 /** https://leetcode.com/problems/longest-common-prefix/ Created by pankaj on 02/02/17. */
-public class LongestCommonPrefix {
-  public String longestCommonPrefix(String[] strs) {
+class LongestCommonPrefix {
+  String longestCommonPrefix(String[] strs) {
     if (strs.length == 0) return "";
     StringBuilder sb = new StringBuilder();
     for (int idx = 0; ; idx++) {
