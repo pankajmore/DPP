d37f9e22e36c448797e042a986aa63d35e58d962
Pankaj <pankajmore@gmail.com>
2018-09-08 22:25:35 -0400

    package private

--- leetcode/src/LongestUncommonSubsequenceI.java
+++ leetcode/src/LongestUncommonSubsequenceI.java
@@ -1,6 +1,6 @@
 /** https://leetcode.com/problems/longest-uncommon-subsequence-i/ Created by pankaj on 03/04/17. */
-public class LongestUncommonSubsequenceI {
-  public int findLUSlength(String a, String b) {
+class LongestUncommonSubsequenceI {
+  int findLUSlength(String a, String b) {
     return a.equals(b) ? -1 : Math.max(a.length(), b.length());
   }
 }
