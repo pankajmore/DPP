b2a108179f67e1b9570c30a42930c5492e8b78e4
Pankaj <pankajmore@gmail.com>
2018-09-14 21:25:08 -0400

    package private

--- leetcode/src/MergeSortedArray.java
+++ leetcode/src/MergeSortedArray.java
@@ -1,6 +1,6 @@
 /** https://leetcode.com/problems/merge-sorted-array/ Created by pankaj on 19/01/17. */
-public class MergeSortedArray {
-  public void merge(int[] nums1, int m, int[] nums2, int n) {
+class MergeSortedArray {
+  void merge(int[] nums1, int m, int[] nums2, int n) {
     for (int i = m - 1, j = n - 1; j >= 0; ) {
       nums1[i + j + 1] = i >= 0 && nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
     }
