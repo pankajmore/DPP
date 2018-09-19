9d2b9c745478cb63981b89ef514ca0374179c11a
Pankaj <pankajmore@gmail.com>
2018-09-18 23:11:14 -0400

    package private

--- leetcode/src/ExcelSheetColumnNumber.java
+++ leetcode/src/ExcelSheetColumnNumber.java
@@ -1,6 +1,6 @@
 /** https://leetcode.com/problems/excel-sheet-column-number/ Created by pankaj on 14/12/16. */
-public class ExcelSheetColumnNumber {
-  public int titleToNumber(String s) {
+class ExcelSheetColumnNumber {
+  int titleToNumber(String s) {
     int x = 0;
     for (char c : s.toCharArray()) x = x * 26 + (c - 'A' + 1);
     return x;
