aeabde687e5336a2c9065af25b00c2c844c75d74
Pankaj <pankajmore@gmail.com>
2018-09-21 22:02:34 -0400

    package private

--- leetcode/src/FactorialTrailingZeroes.java
+++ leetcode/src/FactorialTrailingZeroes.java
@@ -1,6 +1,6 @@
 /** https://leetcode.com/problems/factorial-trailing-zeroes/ Created by pankaj on 27/12/16. */
-public class FactorialTrailingZeroes {
-  public int trailingZeroes(int n) {
+class FactorialTrailingZeroes {
+  int trailingZeroes(int n) {
     int numFives = 0;
     while (n > 0) {
       n /= 5;
