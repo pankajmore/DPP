d4c593532821730dfc3aa44e9c66eedb4bd15465
Pankaj <pankajmore@gmail.com>
2018-09-29 23:13:52 -0400

    package private

--- leetcode/src/FindSmallestLetterGreaterThanTarget.java
+++ leetcode/src/FindSmallestLetterGreaterThanTarget.java
@@ -1,6 +1,6 @@
 /** https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/ */
-public class FindSmallestLetterGreaterThanTarget {
-  public char nextGreatestLetter(char[] letters, char target) {
+class FindSmallestLetterGreaterThanTarget {
+  char nextGreatestLetter(char[] letters, char target) {
     int lo = 0, hi = letters.length - 1;
     if (target < letters[0] || target >= letters[hi]) return letters[0];
     while (lo < hi) {
