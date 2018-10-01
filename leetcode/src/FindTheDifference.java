d9ee7f31b95c408c89c518baa7953a709445e2ae
Pankaj <pankajmore@gmail.com>
2018-09-30 21:50:53 -0400

    package private

--- leetcode/src/FindTheDifference.java
+++ leetcode/src/FindTheDifference.java
@@ -2,8 +2,8 @@
  * https://leetcode.com/problems/find-the-difference/
  * https://leetcode.com/contest/2/problems/find-the-difference/ Created by pankaj on 8/28/16.
  */
-public class FindTheDifference {
-  public char findTheDifference(String s, String t) {
+class FindTheDifference {
+  char findTheDifference(String s, String t) {
     char d = 0;
     for (int i = 0; i < s.length(); i++) {
       d ^= s.charAt(i) ^ t.charAt(i);
