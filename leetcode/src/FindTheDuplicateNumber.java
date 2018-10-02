69c89543fe17221e1c3aaa6ad5ec7ea7eb271e2a
Pankaj <pankajmore@gmail.com>
2018-10-01 21:35:22 -0400

    package private

--- leetcode/src/FindTheDuplicateNumber.java
+++ leetcode/src/FindTheDuplicateNumber.java
@@ -1,7 +1,7 @@
 /** https://leetcode.com/problems/find-the-duplicate-number/ Created by pankaj on 10/11/16. */
-public class FindTheDuplicateNumber {
+class FindTheDuplicateNumber {
   /** Time: O(NlogN) Space: O(1) */
-  public int findDuplicate(int[] nums) {
+  int findDuplicate(int[] nums) {
     int lo = 1, hi = nums.length - 1;
     while (lo < hi) {
       int mid = lo + (hi - lo) / 2;
