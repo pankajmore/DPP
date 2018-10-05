c696ded4fbed1275d7a7d31a5bd99159997148de
Pankaj <pankajmore@gmail.com>
2018-10-04 22:46:59 -0400

    package private

--- leetcode/src/GuessNumberHigherOrLower.java
+++ leetcode/src/GuessNumberHigherOrLower.java
@@ -1,8 +1,8 @@
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /** https://leetcode.com/problems/guess-number-higher-or-lower/ Created by pankaj on 27/12/16. */
-public class GuessNumberHigherOrLower {
-  public int guessNumber(int n) {
+class GuessNumberHigherOrLower {
+  int guessNumber(int n) {
     int lo = 1, hi = n;
     while (lo <= hi) {
       int mid = (lo + hi) >>> 1;
