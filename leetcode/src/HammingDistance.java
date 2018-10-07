9395894433213a42de07389d4a7740d096da2efb
Pankaj <pankajmore@gmail.com>
2018-10-06 22:50:07 -0400

    package private

--- leetcode/src/HammingDistance.java
+++ leetcode/src/HammingDistance.java
@@ -2,8 +2,8 @@
  * https://leetcode.com/contest/leetcode-weekly-contest-13/problems/hamming-distance/ Created by
  * pankaj on 18/12/16.
  */
-public class HammingDistance {
-  public int hammingDistance(int x, int y) {
+class HammingDistance {
+  int hammingDistance(int x, int y) {
     int cnt = 0, z = x ^ y;
     while (z > 0) {
       z = z & (z - 1);
