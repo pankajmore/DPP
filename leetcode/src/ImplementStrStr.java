10115514678d56cd24b471717ee3522892388abb
Pankaj <pankajmore@gmail.com>
2018-10-09 22:03:16 -0400

    package private

--- leetcode/src/ImplementStrStr.java
+++ leetcode/src/ImplementStrStr.java
@@ -1,6 +1,6 @@
 /** https://leetcode.com/problems/implement-strstr/ Created by pankaj on 08/02/17. */
-public class ImplementStrStr {
-  public int strStr(String haystack, String needle) {
+class ImplementStrStr {
+  int strStr(String haystack, String needle) {
     for (int i = 0; i <= haystack.length() - needle.length(); i++) {
       if (haystack.substring(i, i + needle.length()).equals(needle)) return i;
     }
